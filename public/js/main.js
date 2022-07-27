43ae4f65ea00e133e4c2b6a6e96259dcca0d4036
JonTheAlex <jon.the.alex@gmail.com>
2022-07-26 20:13:52 -0500

    first commit

--- public/js/main.js
+++ public/js/main.js
@@ -1,19 +1,32 @@
+//query all buttons with class .fa-trash
 const deleteBtn = document.querySelectorAll('.fa-trash')
+//query all items with class .item of span
 const item = document.querySelectorAll('.item span')
+//query all spans with class complete that are inside .item
 const itemCompleted = document.querySelectorAll('.item span.completed')
 
+//loop through delete button and add evenet listener
 Array.from(deleteBtn).forEach((element)=>{
     element.addEventListener('click', deleteItem)
 })
 
+//loop through items button and add evenet listener
 Array.from(item).forEach((element)=>{
     element.addEventListener('click', markComplete)
 })
 
+
+//loop through item complete and add evenet listener
 Array.from(itemCompleted).forEach((element)=>{
     element.addEventListener('click', markUnComplete)
 })
 
+//fetch method to deleteItem
+//pulls text from item in dom
+//makes fetch request to deleteitem route with method delete
+//sends json object with itemText
+//waits for reqsponse from server and logs the message. reloads page
+//if promise fails logs error message
 async function deleteItem(){
     const itemText = this.parentNode.childNodes[1].innerText
     try{
@@ -33,6 +46,13 @@ async function deleteItem(){
     }
 }
 
+//async function mark todo item completed
+//pulls item text from dom on todoitem
+//makes fetch request to markcomplete route
+//method: put
+//forms a json body with itemtext with key 'itemsfromjs'
+//awaits response from server and logs and reloads page
+//logs error message
 async function markComplete(){
     const itemText = this.parentNode.childNodes[1].innerText
     try{
@@ -52,6 +72,13 @@ async function markComplete(){
     }
 }
 
+//async function mark todo item uncompleted
+//pulls item text from dom on todoitem
+//makes fetch request to markuncomplete route
+//method: put
+//forms a json body with itemtext with key 'itemsfromjs'
+//awaits response from server and logs and reloads page
+//logs error message
 async function markUnComplete(){
     const itemText = this.parentNode.childNodes[1].innerText
     try{
--- server.js
+++ server.js
@@ -1,27 +1,43 @@
+//imports the express object
 const express = require('express')
+//uses the express app constructor and creates the 'app' object exports
 const app = express()
+//imports the mongodb file and exports the mongo object
 const MongoClient = require('mongodb').MongoClient
+//sets default port to 2121
 const PORT = 2121
+//import the .dotenv and sets it up for the project
 require('dotenv').config()
 
-
+//creates empty db variable, sets the db_string to dbConnectionStr, and sets dbName to 'todo'
 let db,
     dbConnectionStr = process.env.DB_STRING,
     dbName = 'todo'
 
+//uses the mongoclient object, and calls the connect object.  sets the dbconnecton str, and sets the useunifiedtopology to true
+//returns a promise that waits to resolve to the client return object.  Then prints connects to the ${dbname}
+//then sets the client db to the dbName ('todo') and sets that to db
 MongoClient.connect(dbConnectionStr, { useUnifiedTopology: true })
     .then(client => {
         console.log(`Connected to ${dbName} Database`)
         db = client.db(dbName)
     })
     
+//sets the express object view engine to 'ejs'
+//express object uses the express static to public, gives access to files in public folder across project
+//express object uses the express.urlencoded method and passes an object with key/value extended:true.  simplifies the url parsing
+//express object uses the express.json() method so we can use the json to render/parse
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 
 
-app.get('/',async (request, response)=>{
+//calls the get method on the express object, passes in the root directory, then make an anonymous async function that has parameters of request/response.
+//awaits call and collects all todoitems documents from todo collection and converts it to an array
+//awaits call and gives a number of documents in the collection
+//response from the server, renders the index.ejs template, and passes in an object with two key/values pairs that populated the ejs file.
+app.get('/', async (request, response)=>{
     const todoItems = await db.collection('todos').find().toArray()
     const itemsLeft = await db.collection('todos').countDocuments({completed: false})
     response.render('index.ejs', { items: todoItems, left: itemsLeft })
@@ -35,6 +51,10 @@ app.get('/',async (request, response)=>{
     // .catch(error => console.error(error))
 })
 
+//express object post method is called, from the client to the addTodo route
+//the request parameter body is parsed and inserted into the db.colloection
+//once doc inserted and successful, then is called logging the todo message and redirected back to root route
+//if promis is not succesful, an error is logged
 app.post('/addTodo', (request, response) => {
     db.collection('todos').insertOne({thing: request.body.todoItem, completed: false})
     .then(result => {
@@ -44,6 +64,12 @@ app.post('/addTodo', (request, response) => {
     .catch(error => console.error(error))
 })
 
+//put requests from client to the markcomplete route
+//finds the item in the list updates the todo collection item once with with the parsed item from the request body.
+//sets the complete field to true of the found object.
+//sorts the documents by descending order and sets upsert to false
+//if promise resolves, console log marked complete
+//if promise fails, error is caught and logged.
 app.put('/markComplete', (request, response) => {
     db.collection('todos').updateOne({thing: request.body.itemFromJS},{
         $set: {
@@ -61,6 +87,13 @@ app.put('/markComplete', (request, response) => {
 
 })
 
+//put request is made to markuncomplete route
+//updatesone with the the requests item and set to thing keyword
+//set the parameter of the complete key to false
+//sorts the documents in descending order
+//forces the upsert:false --> false by default?
+//if promise resolves then keyword logs mark complete
+//promise fails, error caught printed
 app.put('/markUnComplete', (request, response) => {
     db.collection('todos').updateOne({thing: request.body.itemFromJS},{
         $set: {
@@ -78,6 +111,10 @@ app.put('/markUnComplete', (request, response) => {
 
 })
 
+//calls the express object delete method, routes to deleteitem route.  
+//deletes one item matching the request.body
+//once success logs and responses with json
+//fails it catches error and logs
 app.delete('/deleteItem', (request, response) => {
     db.collection('todos').deleteOne({thing: request.body.itemFromJS})
     .then(result => {
@@ -88,6 +125,8 @@ app.delete('/deleteItem', (request, response) => {
 
 })
 
+//express listens on the defined port or the port form the .env file. good for heroku hosting
+//logs the port to the console
 app.listen(process.env.PORT || PORT, ()=>{
     console.log(`Server running on port ${PORT}`)
 })
