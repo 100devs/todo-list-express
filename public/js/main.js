16c875a9f69c4f0b9d2b5784efc4a57099253dc9
unknown <yuvalbenkaplan@gmail.com>
2022-07-26 22:02:01 -0400

    added comments to main.js, server.js, and index.ejs

--- public/js/main.js
+++ public/js/main.js
@@ -1,72 +1,89 @@
 const deleteBtn = document.querySelectorAll('.fa-trash')
+//create deleteBtn variable that applies to every item with trash can icon
 const item = document.querySelectorAll('.item span')
-const itemCompleted = document.querySelectorAll('.item span.completed')
+//create item variable that applies to every item with class of item span
+const itemCompleted = document.querySelectorAll('.item span.completed') 
+//create item variable that stores all items with item span and completed class
 
 Array.from(deleteBtn).forEach((element)=>{
-    element.addEventListener('click', deleteItem)
+    //creates an array from the deleteBtn variable
+    element.addEventListener('click', deleteItem) 
+    //adds event listeners to each item
 })
 
 Array.from(item).forEach((element)=>{
+    //creates an array from the item variable
     element.addEventListener('click', markComplete)
+    //adds event listeners to each item
 })
 
 Array.from(itemCompleted).forEach((element)=>{
+    //creates an array from the itemCompleted variable
     element.addEventListener('click', markUnComplete)
+    //adds event listeners to each item
 })
 
-async function deleteItem(){
+async function deleteItem(){ //async function to delete an item
     const itemText = this.parentNode.childNodes[1].innerText
+    //get text from the span user clicks, store in variable
     try{
         const response = await fetch('deleteItem', {
-            method: 'delete',
-            headers: {'Content-Type': 'application/json'},
-            body: JSON.stringify({
-              'itemFromJS': itemText
+            //make fetch request and store response in variable
+            method: 'delete', //method type
+            headers: {'Content-Type': 'application/json'}, //headers
+            body: JSON.stringify({ //converts js value to json string
+              'itemFromJS': itemText //item being converted and sent to server
             })
           })
-        const data = await response.json()
-        console.log(data)
-        location.reload()
+        const data = await response.json() //gets response from server as json
+        console.log(data) //log data to console
+        location.reload() //reload window to re render items
 
-    }catch(err){
+    }catch(err){ //catch errors
         console.log(err)
     }
 }
 
-async function markComplete(){
+async function markComplete(){ //async function to mark complete an item
     const itemText = this.parentNode.childNodes[1].innerText
+    //get text from the span user clicks, store in variable
     try{
         const response = await fetch('markComplete', {
-            method: 'put',
-            headers: {'Content-Type': 'application/json'},
-            body: JSON.stringify({
-                'itemFromJS': itemText
+            //make fetch request and store response in variable
+            method: 'put', //method type
+            headers: {'Content-Type': 'application/json'}, //headers
+            body: JSON.stringify({ //converts js value to json string
+                'itemFromJS': itemText //item being converted and sent to server
             })
           })
         const data = await response.json()
-        console.log(data)
-        location.reload()
+        //gets response from server as json
+        console.log(data) //log data to console
+        location.reload() //reload window to re render items
 
-    }catch(err){
+    }catch(err){ //catch errors
         console.log(err)
     }
 }
 
-async function markUnComplete(){
+async function markUnComplete(){ //async function to mark uncomplete an item
     const itemText = this.parentNode.childNodes[1].innerText
+    //get text from the span user clicks, store in variable
     try{
         const response = await fetch('markUnComplete', {
-            method: 'put',
-            headers: {'Content-Type': 'application/json'},
-            body: JSON.stringify({
-                'itemFromJS': itemText
+            //make fetch request and store response in variable
+            method: 'put', //method type
+            headers: {'Content-Type': 'application/json'}, //headers
+            body: JSON.stringify({ //converts js value to json string
+                'itemFromJS': itemText //item being converted and sent to server
             })
           })
         const data = await response.json()
-        console.log(data)
-        location.reload()
+        //gets response from server as json
+        console.log(data) //log data to console
+        location.reload() //reload window to re render items
 
-    }catch(err){
+    }catch(err){ //catch errors
         console.log(err)
     }
 }
