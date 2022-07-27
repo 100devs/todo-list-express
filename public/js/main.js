0b06fffb7f43865e756437feaa4625fc72c60d2a
Ticia Francisco <ticiafrancisco@gmail.com>
2022-07-27 16:51:03 -0700

    Add comments to .js files

--- public/js/main.js
+++ public/js/main.js
@@ -1,42 +1,61 @@
-const deleteBtn = document.querySelectorAll('.fa-trash')
-const item = document.querySelectorAll('.item span')
-const itemCompleted = document.querySelectorAll('.item span.completed')
+const deleteBtn = document.querySelectorAll('.fa-trash')    //refer to .fa-trash selection as deleteBtn
+const item = document.querySelectorAll('.item span')    //group all items with .item class into item
+const itemCompleted = document.querySelectorAll('.item span.completed') //group all items with .item and .completed classes into itemCompleted
 
-Array.from(deleteBtn).forEach((element)=>{
-    element.addEventListener('click', deleteItem)
+Array.from(deleteBtn).forEach((element)=>{  //iterate through all deleteBtm elements
+    element.addEventListener('click', deleteItem)   //on click, run deleteItem function
 })
 
-Array.from(item).forEach((element)=>{
-    element.addEventListener('click', markComplete)
+Array.from(item).forEach((element)=>{   //iterate through all item elements
+    element.addEventListener('click', markComplete) //on click, run markComplete function
 })
 
-Array.from(itemCompleted).forEach((element)=>{
-    element.addEventListener('click', markUnComplete)
+Array.from(itemCompleted).forEach((element)=>{  //iterate through all itemCompleted
+    element.addEventListener('click', markUnComplete)   //on click, run markUnComplete function
 })
 
-async function deleteItem(){
-    const itemText = this.parentNode.childNodes[1].innerText
+async function deleteItem(){    //create async function to delete items
+    const itemText = this.parentNode.childNodes[1].innerText    //select item text in selected node
     try{
-        const response = await fetch('deleteItem', {
+        const response = await fetch('deleteItem', {    //delete request
             method: 'delete',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
               'itemFromJS': itemText
             })
           })
-        const data = await response.json()
-        console.log(data)
-        location.reload()
+        const data = await response.json()  //hold json response in data
+        console.log(data)   //print data to console
+        location.reload()   //reload element
 
     }catch(err){
-        console.log(err)
+        console.log(err)    //print error to console
     }
 }
 
-async function markComplete(){
-    const itemText = this.parentNode.childNodes[1].innerText
+async function markComplete(){  //create async function to mark items as complete
+    const itemText = this.parentNode.childNodes[1].innerText    //select item text in selected node
     try{
-        const response = await fetch('markComplete', {
+        const response = await fetch('markComplete', {  //put request for completed status
+            method: 'put',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({
+                'itemFromJS': itemText
+            })
+          })
+        const data = await response.json()  //hold json response in data
+        console.log(data)   //print data to console
+        location.reload()   //reload element
+
+    }catch(err){
+        console.log(err)    //print error to console
+    }
+}
+
+async function markUnComplete(){    //create async function to mark items as not complete
+    const itemText = this.parentNode.childNodes[1].innerText    //select item text in selected node
+    try{
+        const response = await fetch('markUnComplete', {    //put request for not completed status
             method: 'put',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
@@ -44,29 +63,10 @@ async function markComplete(){
             })
           })
         const data = await response.json()
-        console.log(data)
-        location.reload()
+        console.log(data)   //print data to console
+        location.reload()   //reload element
 
     }catch(err){
-        console.log(err)
-    }
-}
-
-async function markUnComplete(){
-    const itemText = this.parentNode.childNodes[1].innerText
-    try{
-        const response = await fetch('markUnComplete', {
-            method: 'put',
-            headers: {'Content-Type': 'application/json'},
-            body: JSON.stringify({
-                'itemFromJS': itemText
-            })
-          })
-        const data = await response.json()
-        console.log(data)
-        location.reload()
-
-    }catch(err){
-        console.log(err)
+        console.log(err)    //print error to console
     }
 }
