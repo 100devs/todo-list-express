589e225f807fd951771a94260878fe688bfefd68
SarahAlCodes <salsaid1016@gmail.com>
2022-07-26 21:13:42 -0400

    Added pseudocode

--- public/js/main.js
+++ public/js/main.js
@@ -1,72 +1,71 @@
-const deleteBtn = document.querySelectorAll('.fa-trash')
-const item = document.querySelectorAll('.item span')
-const itemCompleted = document.querySelectorAll('.item span.completed')
+const deleteBtn = document.querySelectorAll('.fa-trash') //Variable for fa-trash icon class
+const item = document.querySelectorAll('.item span') //Variable for item and span class
+const itemCompleted = document.querySelectorAll('.item span.completed') //Variable for item, span, completed class
 
 Array.from(deleteBtn).forEach((element)=>{
     element.addEventListener('click', deleteItem)
-})
-
-Array.from(item).forEach((element)=>{
+}) ///returns an new array minus deleted item
+Array.from(item).forEach((element)=>{ 
     element.addEventListener('click', markComplete)
-})
+}) //Marks selected item from array complete, and returns new array
 
 Array.from(itemCompleted).forEach((element)=>{
     element.addEventListener('click', markUnComplete)
-})
+}) //Marks selected item from array uncomplete, and returns new array
 
-async function deleteItem(){
-    const itemText = this.parentNode.childNodes[1].innerText
+async function deleteItem(){ //deletes todo list item 
+    const itemText = this.parentNode.childNodes[1].innerText //declaring itemText variable 
     try{
-        const response = await fetch('deleteItem', {
-            method: 'delete',
-            headers: {'Content-Type': 'application/json'},
-            body: JSON.stringify({
+        const response = await fetch('deleteItem', { //fetches deleted todo list item
+            method: 'delete', 
+            headers: {'Content-Type': 'application/json'}, 
+            body: JSON.stringify({ //converts body to JSON
               'itemFromJS': itemText
-            })
+            }) 
           })
-        const data = await response.json()
+        const data = await response.json() //responds with JSON data
         console.log(data)
-        location.reload()
+        location.reload() //reload page after todo list item is deleted
 
     }catch(err){
         console.log(err)
     }
 }
 
-async function markComplete(){
-    const itemText = this.parentNode.childNodes[1].innerText
+async function markComplete(){ //marks complete todo list item
+    const itemText = this.parentNode.childNodes[1].innerText //declaring itemText variable 
     try{
-        const response = await fetch('markComplete', {
+        const response = await fetch('markComplete', { //fetches completed todo item
             method: 'put',
             headers: {'Content-Type': 'application/json'},
-            body: JSON.stringify({
+            body: JSON.stringify({ //converts body to JSON
                 'itemFromJS': itemText
             })
           })
-        const data = await response.json()
+        const data = await response.json() //responds with JSON data
         console.log(data)
-        location.reload()
+        location.reload() //reload page after todo list item is marked completed
 
     }catch(err){
-        console.log(err)
+        console.log(err) //error if promise fails
     }
 }
 
-async function markUnComplete(){
-    const itemText = this.parentNode.childNodes[1].innerText
+async function markUnComplete(){ //marks todo list uncompleted
+    const itemText = this.parentNode.childNodes[1].innerText //declaring itemText variable 
     try{
-        const response = await fetch('markUnComplete', {
+        const response = await fetch('markUnComplete', { //fetches uncompleted todo item
             method: 'put',
             headers: {'Content-Type': 'application/json'},
-            body: JSON.stringify({
+            body: JSON.stringify({ //converts body to JSON
                 'itemFromJS': itemText
             })
           })
-        const data = await response.json()
+        const data = await response.json() //responds with JSON data
         console.log(data)
-        location.reload()
+        location.reload() //reload page after todo list item is marked uncompleted
 
     }catch(err){
-        console.log(err)
+        console.log(err) //error if promise fails
     }
 }
