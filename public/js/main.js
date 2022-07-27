9c9118ef2196b67a2e0e3599e036d71784dc0d01
Jason Cassella <jasoncassella1@gmail.com>
2022-07-27 19:23:32 -0400

    fixed typos and now i can see

--- public/js/main.js
+++ public/js/main.js
@@ -1,72 +1,72 @@
-const deleteBtn = document.querySelectorAll('.fa-trash')
-const item = document.querySelectorAll('.item span')
-const itemCompleted = document.querySelectorAll('.item span.completed')
+const deleteBtn = document.querySelectorAll('.fa-trash');
+const item = document.querySelectorAll('.item span');
+const itemCompleted = document.querySelectorAll('.item span.completed');
 
-Array.from(deleteBtn).forEach((element)=>{
-    element.addEventListener('click', deleteItem)
-})
+Array.from(deleteBtn).forEach((element) => {
+    element.addEventListener('click', deleteItem);
+});
 
-Array.from(item).forEach((element)=>{
-    element.addEventListener('click', markComplete)
-})
+Array.from(item).forEach((element) => {
+    element.addEventListener('click', markComplete);
+});
 
-Array.from(itemCompleted).forEach((element)=>{
-    element.addEventListener('click', markUnComplete)
-})
+Array.from(itemCompleted).forEach((element) => {
+    element.addEventListener('click', markIncomplete);
+});
 
-async function deleteItem(){
-    const itemText = this.parentNode.childNodes[1].innerText
-    try{
+async function deleteItem() {
+    const itemText = this.parentNode.childNodes[1].innerText;
+    try {
         const response = await fetch('deleteItem', {
             method: 'delete',
-            headers: {'Content-Type': 'application/json'},
+            headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify({
-              'itemFromJS': itemText
+                'itemFromJS': itemText
             })
-          })
-        const data = await response.json()
-        console.log(data)
-        location.reload()
+        });
+        const data = await response.json();
+        console.log(data);
+        location.reload();
 
-    }catch(err){
-        console.log(err)
+    } catch (err) {
+        console.log(err);
     }
 }
 
-async function markComplete(){
-    const itemText = this.parentNode.childNodes[1].innerText
-    try{
+async function markComplete() {
+    const itemText = this.parentNode.childNodes[1].innerText;
+    try {
         const response = await fetch('markComplete', {
             method: 'put',
-            headers: {'Content-Type': 'application/json'},
+            headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify({
                 'itemFromJS': itemText
             })
-          })
-        const data = await response.json()
-        console.log(data)
-        location.reload()
+        });
+        const data = await response.json();
+        console.log(data);
+        location.reload();
 
-    }catch(err){
-        console.log(err)
+    } catch (err) {
+        console.log(err);
     }
 }
 
-async function markUnComplete(){
-    const itemText = this.parentNode.childNodes[1].innerText
-    try{
-        const response = await fetch('markUnComplete', {
+async function markIncomplete() {
+    const itemText = this.parentNode.childNodes[1].innerText;
+    try {
+        const response = await fetch('markIncomplete', {
             method: 'put',
-            headers: {'Content-Type': 'application/json'},
+            headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify({
                 'itemFromJS': itemText
             })
-          })
-        const data = await response.json()
-        console.log(data)
-        location.reload()
+        });
+        const data = await response.json();
+        console.log(data);
+        location.reload();
 
-    }catch(err){
-        console.log(err)
+    } catch (err) {
+        console.log(err);
     }
 }
