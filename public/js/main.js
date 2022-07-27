4575f3a4d8024ad05a822cde3aa426c1731b1ad1
Yire Morlans <yire.m@icloud.com>
2022-07-26 21:42:23 -0400

    complete server.js comments

--- public/js/main.js
+++ public/js/main.js
@@ -1,19 +1,20 @@
+//learned that document.querySelectors are not js but web APIs that grab the element and assigns it to a variable
 const deleteBtn = document.querySelectorAll('.fa-trash')
 const item = document.querySelectorAll('.item span')
 const itemCompleted = document.querySelectorAll('.item span.completed')
-
+//creates an array from each item deleteBtn and it iterates through each item to add an event listener to each item with the function of deleteItem
 Array.from(deleteBtn).forEach((element)=>{
     element.addEventListener('click', deleteItem)
 })
-
+//creates an array from each list item span and adds event listener to that item so that when clicked, it triggers markComplete function
 Array.from(item).forEach((element)=>{
     element.addEventListener('click', markComplete)
 })
-
+//creates an array from each markCompleted item span and assigns eventlistener to it that runs markUncomplete func on click
 Array.from(itemCompleted).forEach((element)=>{
     element.addEventListener('click', markUnComplete)
 })
-
+//
 async function deleteItem(){
     const itemText = this.parentNode.childNodes[1].innerText
     try{
