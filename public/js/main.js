b92564786c8b52fbb51d1ccc506c7b5fac843fc9
MazeBuer <mazikeenbuer@gmail.com>
2022-07-26 23:34:23 -0700

    commented details

--- public/js/main.js
+++ public/js/main.js
@@ -1,34 +1,36 @@
-const deleteBtn = document.querySelectorAll('.fa-trash')
-const item = document.querySelectorAll('.item span')
-const itemCompleted = document.querySelectorAll('.item span.completed')
+const deleteBtn = document.querySelectorAll('.fa-trash')  //goest into the document and looks for selecors with class of fa-trash -> this will be named deleteBtn
+const item = document.querySelectorAll('.item span')  //select on all the .item in the spans
+const itemCompleted = document.querySelectorAll('.item span.completed') //any span with class of item AND completed
 
+
+//the arrays are constructed upon document load and waiting for something to happen. Once we click, whatever we clicked will run
 Array.from(deleteBtn).forEach((element)=>{
-    element.addEventListener('click', deleteItem)
+    element.addEventListener('click', deleteItem)  //makes an arraay of every fa-trash class. each element in that array takes the element and adds an event listener to it and on click runs the deleteItem functin (below)
 })
 
 Array.from(item).forEach((element)=>{
-    element.addEventListener('click', markComplete)
+    element.addEventListener('click', markComplete)  //makes an array from the item (above). each element in that array takes an element and adds an event listener to it, on click it runs the markComplete function (below)
 })
 
 Array.from(itemCompleted).forEach((element)=>{
-    element.addEventListener('click', markUnComplete)
+    element.addEventListener('click', markUnComplete) //makes an array from the itemCompleted (above). each element that
 })
 
 async function deleteItem(){
-    const itemText = this.parentNode.childNodes[1].innerText
-    try{
+    const itemText = this.parentNode.childNodes[1].innerText //parentNode would be ul and childNode would be li in a list; the innerText would be the text inside the list ; going into the ul and into the li and getting the text to change; childNodes[1] is the childNode located at index 1
+    try{ //what we will be doing once deleteItem is actived
         const response = await fetch('deleteItem', {
-            method: 'delete',
-            headers: {'Content-Type': 'application/json'},
-            body: JSON.stringify({
-              'itemFromJS': itemText
-            })
+            method: 'delete', //using the CRUD delete
+            headers: {'Content-Type': 'application/json'}, //explains what type of file it will get in return
+            body: JSON.stringify({  //converts an object to a JSON string
+              'itemFromJS': itemText  //this is what is being turned into JSON string ; itemFromJS is from serverJS
+            }) //itemFormJS is the name given to thte itemText ; it is the key and value is itemText; (we pick the name itemFromJS and the itemText is from above const) similar ex. 
           })
-        const data = await response.json()
-        console.log(data)
-        location.reload()
+        const data = await response.json() //console logs the information above (response)
+        console.log(data) //console logs the data
+        location.reload() //reloads the page so it shows your updates ; automatic refresh
 
-    }catch(err){
+    }catch(err){  //console logs any errors
         console.log(err)
     }
 }
@@ -37,7 +39,7 @@ async function markComplete(){
     const itemText = this.parentNode.childNodes[1].innerText
     try{
         const response = await fetch('markComplete', {
-            method: 'put',
+            method: 'put', //using the CRUD put
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
                 'itemFromJS': itemText
--- server.js
+++ server.js
@@ -3,28 +3,33 @@ const app = express()
 const MongoClient = require('mongodb').MongoClient
 const PORT = 2121
 require('dotenv').config()
+//dependencies needed 
 
-
+//mongoDB your ps and the name of the database
 let db,
     dbConnectionStr = process.env.DB_STRING,
     dbName = 'todo'
 
+
+// connects MongoDB with your ps and the database name
 MongoClient.connect(dbConnectionStr, { useUnifiedTopology: true })
     .then(client => {
         console.log(`Connected to ${dbName} Database`)
         db = client.db(dbName)
     })
     
-app.set('view engine', 'ejs')
+app.set('view engine', 'ejs') //the middleware ; a command for what you use to communicate with the database
 app.use(express.static('public'))
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 
 
+//on page load request we will response with todoItems and itemsLeft (explained in 2 lines below)
 app.get('/',async (request, response)=>{
-    const todoItems = await db.collection('todos').find().toArray()
-    const itemsLeft = await db.collection('todos').countDocuments({completed: false})
-    response.render('index.ejs', { items: todoItems, left: itemsLeft })
+    const todoItems = await db.collection('todos').find().toArray() //goes into database of named 'todo' and finds it and turns that into an array
+    const itemsLeft = await db.collection('todos').countDocuments({completed: false}) //goes into database of 'todo' (i named it in Mongo) and counts how many documents are in the collections array. if there are any still present then it is false ; (because it is not completed); database - room ; collection - box ; document - item inside the box
+    response.render('index.ejs', { items: todoItems, left: itemsLeft }) //the response would be at index.ejs we will get the above to show on the DOM (todoItems and itemsLeft) (under the appropriate name in index.ejs in this case items and left)
+
     // db.collection('todos').find().toArray()
     // .then(data => {
     //     db.collection('todos').countDocuments({completed: false})
@@ -35,8 +40,10 @@ app.get('/',async (request, response)=>{
     // .catch(error => console.error(error))
 })
 
-app.post('/addTodo', (request, response) => {
-    db.collection('todos').insertOne({thing: request.body.todoItem, completed: false})
+
+//create (post)
+app.post('/addTodo', (request, response) => { //on page addTodo
+    db.collection('todos').insertOne({thing: request.body.todoItem, completed: false}) //we will insert one 'thing' which is an todoItem. and if we add a thing then the completed will be false
     .then(result => {
         console.log('Todo Added')
         response.redirect('/')
@@ -44,14 +51,15 @@ app.post('/addTodo', (request, response) => {
     .catch(error => console.error(error))
 })
 
-app.put('/markComplete', (request, response) => {
+//update (put)
+app.put('/markComplete', (request, response) => {  //on page markComplete
     db.collection('todos').updateOne({thing: request.body.itemFromJS},{
         $set: {
-            completed: true
+            completed: true //when complete is true and no more on left list
           }
     },{
-        sort: {_id: -1},
-        upsert: false
+        sort: {_id: -1}, //sorts in decesding order based on id number (MongoDB assigns id on the database so once it goes to completed it will be on the descending order based on the MongoDB id)
+        upsert: false //default is false ; when true creates a new document when no document does not match query
     })
     .then(result => {
         console.log('Marked Complete')
@@ -61,25 +69,27 @@ app.put('/markComplete', (request, response) => {
 
 })
 
+//on page markUncomplete
 app.put('/markUnComplete', (request, response) => {
-    db.collection('todos').updateOne({thing: request.body.itemFromJS},{
+    db.collection('todos').updateOne({thing: request.body.itemFromJS},{ //gets item from index.ejs form (where you add items) and puts it in the database collection
         $set: {
-            completed: false
+            completed: false //list not completed
           }
     },{
-        sort: {_id: -1},
-        upsert: false
+        sort: {_id: -1}, //sorts in decesding order based on id number (MongoDB assigns id on the database so once it goes to completed it will be on the descending order based on the MongoDB id)
+        upsert: false //default is false ; when true creates a new document when no document does not match query
     })
     .then(result => {
-        console.log('Marked Complete')
-        response.json('Marked Complete')
+        console.log('Marked unComplete')
+        response.json('Marked unComplete')
     })
     .catch(error => console.error(error))
 
 })
 
+//on page deleteItem
 app.delete('/deleteItem', (request, response) => {
-    db.collection('todos').deleteOne({thing: request.body.itemFromJS})
+    db.collection('todos').deleteOne({thing: request.body.itemFromJS}) //goes to the database of 'todo' and deletes what is on the form
     .then(result => {
         console.log('Todo Deleted')
         response.json('Todo Deleted')
@@ -88,6 +98,7 @@ app.delete('/deleteItem', (request, response) => {
 
 })
 
+//port we are using wither our choosing or Heroku choosing
 app.listen(process.env.PORT || PORT, ()=>{
     console.log(`Server running on port ${PORT}`)
 })
