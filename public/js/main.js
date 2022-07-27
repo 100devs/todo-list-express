1015f03b77d782525377ef3480e3efea1352bf45
Alyssha <90856589+AlysshaLewin@users.noreply.github.com>
2022-07-26 21:01:42 -0500

    commented out lines of code

--- public/js/main.js
+++ public/js/main.js
@@ -1,11 +1,14 @@
 const deleteBtn = document.querySelectorAll('.fa-trash')
+//selects each li with a class of .item
 const item = document.querySelectorAll('.item span')
+//selects each li with a class of .item
 const itemCompleted = document.querySelectorAll('.item span.completed')
 
 Array.from(deleteBtn).forEach((element)=>{
     element.addEventListener('click', deleteItem)
 })
 
+//adds a click event to every rendered element and will fire markComplete async function
 Array.from(item).forEach((element)=>{
     element.addEventListener('click', markComplete)
 })
@@ -33,10 +36,17 @@ async function deleteItem(){
     }
 }
 
+/*
+*
+*/
+
 async function markComplete(){
+    //will select the innerText of the span
     const itemText = this.parentNode.childNodes[1].innerText
     try{
+        //fetching markcomplete method with put method endpoint
         const response = await fetch('markComplete', {
+            //pass itemText into body of request
             method: 'put',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
@@ -45,6 +55,8 @@ async function markComplete(){
           })
         const data = await response.json()
         console.log(data)
+
+        //web api object, reloads our page (which will fire a new GET request)
         location.reload()
 
     }catch(err){
@@ -52,9 +64,15 @@ async function markComplete(){
     }
 }
 
+
+/*
+* Name: markComplete
+* Description: fires when an item from our todo is clicked and will make a PUT request in our server.
+*/
 async function markUnComplete(){
     const itemText = this.parentNode.childNodes[1].innerText
     try{
+        //will select the innerText
         const response = await fetch('markUnComplete', {
             method: 'put',
             headers: {'Content-Type': 'application/json'},
--- server.js
+++ server.js
@@ -1,29 +1,60 @@
+//defining variable to use express in server.js
 const express = require('express')
+
+//initiating express app, allows you to use variable name app
 const app = express()
+
+//importing mongodb in order to connect to a mongo database
 const MongoClient = require('mongodb').MongoClient
+
+//variable for port number
 const PORT = 2121
+
+//allows us to use .env file
 require('dotenv').config()
 
-
+/*
+* let db = database
+* dbConnectionStr = allows us to connect to mongo atlas db
+* dbName = database name
+*/
 let db,
     dbConnectionStr = process.env.DB_STRING,
     dbName = 'todo'
 
+/*
+* connecting app to mongoclient
+* useUnifiedTopology = allows us to 
+*/
 MongoClient.connect(dbConnectionStr, { useUnifiedTopology: true })
     .then(client => {
         console.log(`Connected to ${dbName} Database`)
+        // create a new name db with the name you pass to it, allows you to access mongoDB
         db = client.db(dbName)
     })
-    
+
+
+//sets our view engine to ejs, allows us to use ejs
 app.set('view engine', 'ejs')
+
+//sets up public folder that will serve files to client
 app.use(express.static('public'))
+
+//parses incoming request with urlencoded payloads and is based on body-parser -parser is deprecated.
 app.use(express.urlencoded({ extended: true }))
+
+//It parses incoming JSON requests and puts the parsed data in request.
 app.use(express.json())
 
-
-app.get('/',async (request, response)=>{
+// defines a GET request at default endpoint "/"
+app.get('/', async (request, response) => {
+    // accessing db collection called 'todos', .find returns documents and converts them into an array
     const todoItems = await db.collection('todos').find().toArray()
+
+    //count documents in database that have false as a value for key "completed"
     const itemsLeft = await db.collection('todos').countDocuments({completed: false})
+
+    //passes todoItems and itemLeft to index.ejs file in order to be rendered.
     response.render('index.ejs', { items: todoItems, left: itemsLeft })
     // db.collection('todos').find().toArray()
     // .then(data => {
@@ -35,26 +66,39 @@ app.get('/',async (request, response)=>{
     // .catch(error => console.error(error))
 })
 
+
+//defined a POST request at /addToDo endpoint, used to Create a new document
 app.post('/addTodo', (request, response) => {
+    // adds new document to do todos collection, todoItem property is pulled from our request body
     db.collection('todos').insertOne({thing: request.body.todoItem, completed: false})
     .then(result => {
         console.log('Todo Added')
+        // redirects to base endpoint after the document is added 
         response.redirect('/')
     })
+    // catches any errors from previous code
     .catch(error => console.error(error))
 })
 
+
+//defined a PUT request at /markComplete endpoint, used to update a document
 app.put('/markComplete', (request, response) => {
+
+    //updates a document from the todos collection
     db.collection('todos').updateOne({thing: request.body.itemFromJS},{
         $set: {
+            //sets the completed key to true (in our ejs, will add a class "completed" in the span)
             completed: true
           }
     },{
+        // method specifies the order in which the quert returns the match documents from the given collection
         sort: {_id: -1},
+        // makes sure a new document isn't created if the document isn't found in our DB
         upsert: false
     })
     .then(result => {
         console.log('Marked Complete')
+        // respond with json indicatin the marking is complete
         response.json('Marked Complete')
     })
     .catch(error => console.error(error))
@@ -64,6 +108,7 @@ app.put('/markComplete', (request, response) => {
 app.put('/markUnComplete', (request, response) => {
     db.collection('todos').updateOne({thing: request.body.itemFromJS},{
         $set: {
+            //sets the completed key to true (in our ejs, will add a class "completed" to the span) 
             completed: false
           }
     },{
@@ -71,14 +116,17 @@ app.put('/markUnComplete', (request, response) => {
         upsert: false
     })
     .then(result => {
-        console.log('Marked Complete')
-        response.json('Marked Complete')
+        console.log('Marked UnComplete')
+        response.json('Marked UnComplete')
     })
     .catch(error => console.error(error))
 
 })
 
+
+//adds a DELETE request to /deleteItem endpoint
 app.delete('/deleteItem', (request, response) => {
+    //delete document from todos collection that has the thing key's value as the document passed to the request.
     db.collection('todos').deleteOne({thing: request.body.itemFromJS})
     .then(result => {
         console.log('Todo Deleted')
