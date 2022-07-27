033f1d36b0a95a5ec68768dfa5c6ac9291e0a41b
valerievozza <valerievozza@gmail.com>
2022-07-27 11:42:25 -0700

    added comments to main.js file - incomplete

--- public/js/main.js
+++ public/js/main.js
@@ -1,19 +1,28 @@
+// get all the DOM elements with a class of `fa-trash` and assign the NodeList to deleteBtn
 const deleteBtn = document.querySelectorAll('.fa-trash')
+// get all the DOM elements that are spans and descendants of elements with a class of `item`, and assign to the item variable
 const item = document.querySelectorAll('.item span')
+// get all the DOM elements that are spans with a class of `completed` and are descendants of elements with a class of `item`, assigning them to the `itemCompleted` variable
 const itemCompleted = document.querySelectorAll('.item span.completed')
 
+// convert deleteBtn NodeList to an array and then call .forEach on the array
 Array.from(deleteBtn).forEach((element)=>{
+    // adding the deleteItem function as a click event listener for each item in deleteBtn
     element.addEventListener('click', deleteItem)
 })
 
+// creating an array from the nodelist assigned to the item variable, looping through it and adding an event listener to each element that will 'listen' for a click and passing the function 'markComplete'
 Array.from(item).forEach((element)=>{
     element.addEventListener('click', markComplete)
 })
 
+// creating an array from the nodelist assigned to itemCompleted. Looping through the array
 Array.from(itemCompleted).forEach((element)=>{
+    // add event listener to each element that will 'listen' for a click and passing the function 'markUnComplete' 
     element.addEventListener('click', markUnComplete)
 })
 
+// declare an async function called deleteItem
 async function deleteItem(){
     const itemText = this.parentNode.childNodes[1].innerText
     try{
