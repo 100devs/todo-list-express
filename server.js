85d463b66c4aa4273db08743d341a386c673312e
Andrew Arkwright <andrew.arkwright@hotmail.com>
2022-07-26 21:54:18 -0400

    Add comments to the code.

--- server.js
+++ server.js
@@ -1,31 +1,31 @@
-const express = require('express')
-const app = express()
-const MongoClient = require('mongodb').MongoClient
-const PORT = 2121
-require('dotenv').config()
+const express = require('express') //Says that it requires JS
+const app = express() //Allows you to use express
+const MongoClient = require('mongodb').MongoClient //says that it requires mongodb
+const PORT = 2121 //port your server will run on
+require('dotenv').config() //Lets you define hidden variables like the password for mangodb
 
 
-let db,
+let db, //defining variables that are used to grab the hidden login info for mongodb. Used to get info from .env files
     dbConnectionStr = process.env.DB_STRING,
     dbName = 'todo'
 
-MongoClient.connect(dbConnectionStr, { useUnifiedTopology: true })
+MongoClient.connect(dbConnectionStr, { useUnifiedTopology: true }) //Connects to mongodb database
     .then(client => {
         console.log(`Connected to ${dbName} Database`)
         db = client.db(dbName)
     })
     
-app.set('view engine', 'ejs')
-app.use(express.static('public'))
-app.use(express.urlencoded({ extended: true }))
-app.use(express.json())
+app.set('view engine', 'ejs') //Says to use the view folder for ejs and adjusts the root
+app.use(express.static('public')) //Says to look in public folder for static express files
+app.use(express.urlencoded({ extended: true })) //Parser for requests
+app.use(express.json()) //Parser for JSON responses
 
 
-app.get('/',async (request, response)=>{
-    const todoItems = await db.collection('todos').find().toArray()
-    const itemsLeft = await db.collection('todos').countDocuments({completed: false})
-    response.render('index.ejs', { items: todoItems, left: itemsLeft })
-    // db.collection('todos').find().toArray()
+app.get('/',async (request, response)=>{ //Request and respose for main page of the website
+    const todoItems = await db.collection('todos').find().toArray() //Changes it to an array
+    const itemsLeft = await db.collection('todos').countDocuments({completed: false}) //Items left that is not viewable
+    response.render('index.ejs', { items: todoItems, left: itemsLeft }) //Renders the items that are available
+    // db.collection('todos').find().toArray() //non async way to do the steps above
     // .then(data => {
     //     db.collection('todos').countDocuments({completed: false})
     //     .then(itemsLeft => {
@@ -35,7 +35,7 @@ app.get('/',async (request, response)=>{
     // .catch(error => console.error(error))
 })
 
-app.post('/addTodo', (request, response) => {
+app.post('/addTodo', (request, response) => { //Create a new item to add to the todo list and redirects back to the homepage
     db.collection('todos').insertOne({thing: request.body.todoItem, completed: false})
     .then(result => {
         console.log('Todo Added')
@@ -44,13 +44,13 @@ app.post('/addTodo', (request, response) => {
     .catch(error => console.error(error))
 })
 
-app.put('/markComplete', (request, response) => {
+app.put('/markComplete', (request, response) => {//Update existing todo to complete
     db.collection('todos').updateOne({thing: request.body.itemFromJS},{
         $set: {
             completed: true
           }
     },{
-        sort: {_id: -1},
+        sort: {_id: -1}, //sorts and adds to the bottom of the list
         upsert: false
     })
     .then(result => {
@@ -61,13 +61,13 @@ app.put('/markComplete', (request, response) => {
 
 })
 
-app.put('/markUnComplete', (request, response) => {
+app.put('/markUnComplete', (request, response) => {//Update existing todo to not completed
     db.collection('todos').updateOne({thing: request.body.itemFromJS},{
         $set: {
             completed: false
           }
     },{
-        sort: {_id: -1},
+        sort: {_id: -1}, //sorts and adds to the bottom of the list
         upsert: false
     })
     .then(result => {
@@ -78,7 +78,7 @@ app.put('/markUnComplete', (request, response) => {
 
 })
 
-app.delete('/deleteItem', (request, response) => {
+app.delete('/deleteItem', (request, response) => { //Delete an item from the todo list
     db.collection('todos').deleteOne({thing: request.body.itemFromJS})
     .then(result => {
         console.log('Todo Deleted')
@@ -88,6 +88,6 @@ app.delete('/deleteItem', (request, response) => {
 
 })
 
-app.listen(process.env.PORT || PORT, ()=>{
+app.listen(process.env.PORT || PORT, ()=>{ //assign the port by mongoDB or by the env file
     console.log(`Server running on port ${PORT}`)
 })
