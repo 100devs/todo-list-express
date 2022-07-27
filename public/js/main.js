7c91f14cdd92b3918115940b6f65d860322c172b
Ashley Christman <dev@ashleychristman.com>
2022-07-27 10:43:44 -0400

    Add comments

--- public/js/main.js
+++ public/js/main.js
@@ -1,19 +1,29 @@
+// Comments by Ashley Christman, Stacey Ali, Omar Hernandez, Mark Fehrenbach, Ryan Hardin, Kimberly Scranton
+
+// Selects all delete buttons
 const deleteBtn = document.querySelectorAll('.fa-trash')
+// Selects all spans within a class of 'item'
 const item = document.querySelectorAll('.item span')
+// Selects all COMPLETED spans within a class of 'item'
 const itemCompleted = document.querySelectorAll('.item span.completed')
 
+
+// Make an array out of all delete buttons, add click event listenener to each that calls back deleteItem function
 Array.from(deleteBtn).forEach((element)=>{
     element.addEventListener('click', deleteItem)
 })
 
+// Make an array out of all active items, add click event listenener to each that calls back markComplete function
 Array.from(item).forEach((element)=>{
     element.addEventListener('click', markComplete)
 })
 
+// Make an array out of all completed items, add click event listenener to each that calls back markUnComplete function
 Array.from(itemCompleted).forEach((element)=>{
     element.addEventListener('click', markUnComplete)
 })
 
+// Calls '/deleteItem' route, sends item to be deleted, console logs repsonse, and reloads page
 async function deleteItem(){
     const itemText = this.parentNode.childNodes[1].innerText
     try{
@@ -33,6 +43,7 @@ async function deleteItem(){
     }
 }
 
+// Calls '/markComplete' route, sends item to be marked complete, console logs repsonse, and reloads page
 async function markComplete(){
     const itemText = this.parentNode.childNodes[1].innerText
     try{
@@ -52,6 +63,7 @@ async function markComplete(){
     }
 }
 
+// Calls '/markUnComplete' route, sends item to be marked incomplete, console logs repsonse, and reloads page
 async function markUnComplete(){
     const itemText = this.parentNode.childNodes[1].innerText
     try{
--- server.js
+++ server.js
@@ -1,40 +1,57 @@
+// Comments by Ashley Christman, Stacey Ali, Omar Hernandez, Mark Fehrenbach, Ryan Hardin, Kimberly Scranton
+
+// Instantiate Express
 const express = require('express')
 const app = express()
+// Instantiate MongoDB
 const MongoClient = require('mongodb').MongoClient
+// Define Default Port
 const PORT = 2121
+// Instantiate dotenv environment variables
 require('dotenv').config()
 
-
+// Define MongoDB Variables
 let db,
     dbConnectionStr = process.env.DB_STRING,
-    dbName = 'todo'
+    dbName = 'todo-list-express'
 
+// Connect to MongoDB database
 MongoClient.connect(dbConnectionStr, { useUnifiedTopology: true })
     .then(client => {
         console.log(`Connected to ${dbName} Database`)
         db = client.db(dbName)
     })
     
+//Middleware
+// Set EJS    
 app.set('view engine', 'ejs')
+// Set Public folder
 app.use(express.static('public'))
+// Use URL Encoding
 app.use(express.urlencoded({ extended: true }))
+// Parse JSON
 app.use(express.json())
 
-
+// Home Route
 app.get('/',async (request, response)=>{
+    // Returns collection of todos as array
     const todoItems = await db.collection('todos').find().toArray()
+    // Returns count of items left that are incomplete
     const itemsLeft = await db.collection('todos').countDocuments({completed: false})
+    // Renders prior two variables into index.ejs
     response.render('index.ejs', { items: todoItems, left: itemsLeft })
-    // db.collection('todos').find().toArray()
-    // .then(data => {
-    //     db.collection('todos').countDocuments({completed: false})
-    //     .then(itemsLeft => {
-    //         response.render('index.ejs', { items: data, left: itemsLeft })
-    //     })
-    // })
-    // .catch(error => console.error(error))
+
+        // db.collection('todos').find().toArray()
+        // .then(data => {
+        //     db.collection('todos').countDocuments({completed: false})
+        //     .then(itemsLeft => {
+        //         response.render('index.ejs', { items: data, left: itemsLeft })
+        //     })
+        // })
+        // .catch(error => console.error(error))
 })
 
+// Add new list item - Add to database, console log, then redirect to home route
 app.post('/addTodo', (request, response) => {
     db.collection('todos').insertOne({thing: request.body.todoItem, completed: false})
     .then(result => {
@@ -44,6 +61,8 @@ app.post('/addTodo', (request, response) => {
     .catch(error => console.error(error))
 })
 
+
+// Mark database item as complete when clicked
 app.put('/markComplete', (request, response) => {
     db.collection('todos').updateOne({thing: request.body.itemFromJS},{
         $set: {
@@ -61,6 +80,8 @@ app.put('/markComplete', (request, response) => {
 
 })
 
+
+// Mark completed database item as incomplete when clicked
 app.put('/markUnComplete', (request, response) => {
     db.collection('todos').updateOne({thing: request.body.itemFromJS},{
         $set: {
@@ -71,13 +92,14 @@ app.put('/markUnComplete', (request, response) => {
         upsert: false
     })
     .then(result => {
-        console.log('Marked Complete')
-        response.json('Marked Complete')
+        console.log('Marked Inomplete')
+        response.json('Marked Incomplete')
     })
     .catch(error => console.error(error))
 
 })
 
+// Delete database item when delete button is clicked
 app.delete('/deleteItem', (request, response) => {
     db.collection('todos').deleteOne({thing: request.body.itemFromJS})
     .then(result => {
@@ -88,6 +110,9 @@ app.delete('/deleteItem', (request, response) => {
 
 })
 
+
+// Run server on port defined in env. variable OR default port defined above
 app.listen(process.env.PORT || PORT, ()=>{
     console.log(`Server running on port ${PORT}`)
 })
+
