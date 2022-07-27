709793277cb6063f0e1ef6468f53791e7e3dc584
Michael Joel <michaeljoelt@gmail.com>
2022-07-27 11:13:47 -0700

    comments

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
+Original README:
 npm install
 add DB_STRING to .env file
+
+Updated README:
+---------------------
+- Getting Started -
+---------------------
+1. Go to Leon's link above and click "Fork" on the top right. This will make your own copy of this repository to work on.
+
+2. In your forked repository, click Code -> HTTPS -> copy the link
+
+3. Choose a location on your local PC where you want to create copies of these files so you can work on them. Use the terminal to access that location, and type: git clone [copied link]
+
+4. Now you can work on the code. The assignment is to comment every line. Probably start with server.js, then move to index.ejs, main.js, and style.css if you want.
+
+----------------------
+- Running the App -
+(Note: This technically isn't part of the assignment, but I'd like to get it working if possible...)
+----------------------
+1. README says to: run "npm install" - run this command from the terminal, while in your repository folder
+
+2. README says to: add DB_STRING to .env file - create a file called ".env", where you need to add the DB_STRING from MongoDB. 
+Note: If you need help, follow along with this article until you get the "connection string": https://zellwk.com/blog/crud-express-mongodb/#setting-up-mongodb-atlas. Then, add that connection string in to the .env file like:
+DB_STRING=mongodb+srv://..... (continue with the full connection string. This is sensitive information, which is why it's being stored in the .env file which is set to be ignored in the .gitignore file)
+
+3. Start the node server by running this command in terminal within repository folder: node server.js
+Note: If running properly, the terminal will state: 
+"Server running on port 2121
+Connected to todo Database"
+
+4. Open the application by going to http://127.0.0.1:2121/ or http://localhost:2121/ (the server is running on port 2121 on your local machine, which has the address of 127.0.0.1 (or localhost) when referring to itself)
--- server.js
+++ server.js
@@ -1,30 +1,39 @@
-const express = require('express')
-const app = express()
-const MongoClient = require('mongodb').MongoClient
-const PORT = 2121
-require('dotenv').config()
+const express = require('express') //this uses node module "express" to help us handle API requests
+const app = express() //creates an instance of the express module?
+const MongoClient = require('mongodb').MongoClient //uses module "mongodb"
+const PORT = 2121 //assigning a port for our server to communicate through
+require('dotenv').config() //use node module dotenv; 
 
-
+/*initializing and assigning variable*/
 let db,
     dbConnectionStr = process.env.DB_STRING,
     dbName = 'todo'
 
-MongoClient.connect(dbConnectionStr, { useUnifiedTopology: true })
+MongoClient.connect(dbConnectionStr, {
+        useUnifiedTopology: true
+    })
     .then(client => {
         console.log(`Connected to ${dbName} Database`)
         db = client.db(dbName)
     })
-    
+
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
-app.use(express.urlencoded({ extended: true }))
+app.use(express.urlencoded({
+    extended: true
+}))
 app.use(express.json())
 
 
-app.get('/',async (request, response)=>{
+app.get('/', async (request, response) => {
     const todoItems = await db.collection('todos').find().toArray()
-    const itemsLeft = await db.collection('todos').countDocuments({completed: false})
-    response.render('index.ejs', { items: todoItems, left: itemsLeft })
+    const itemsLeft = await db.collection('todos').countDocuments({
+        completed: false
+    })
+    response.render('index.ejs', {
+        items: todoItems,
+        left: itemsLeft
+    })
     // db.collection('todos').find().toArray()
     // .then(data => {
     //     db.collection('todos').countDocuments({completed: false})
@@ -36,58 +45,71 @@ app.get('/',async (request, response)=>{
 })
 
 app.post('/addTodo', (request, response) => {
-    db.collection('todos').insertOne({thing: request.body.todoItem, completed: false})
-    .then(result => {
-        console.log('Todo Added')
-        response.redirect('/')
-    })
-    .catch(error => console.error(error))
+    db.collection('todos').insertOne({
+            thing: request.body.todoItem,
+            completed: false
+        })
+        .then(result => {
+            console.log('Todo Added')
+            response.redirect('/')
+        })
+        .catch(error => console.error(error))
 })
 
 app.put('/markComplete', (request, response) => {
-    db.collection('todos').updateOne({thing: request.body.itemFromJS},{
-        $set: {
-            completed: true
-          }
-    },{
-        sort: {_id: -1},
-        upsert: false
-    })
-    .then(result => {
-        console.log('Marked Complete')
-        response.json('Marked Complete')
-    })
-    .catch(error => console.error(error))
+    db.collection('todos').updateOne({
+            thing: request.body.itemFromJS
+        }, {
+            $set: {
+                completed: true
+            }
+        }, {
+            sort: {
+                _id: -1
+            },
+            upsert: false
+        })
+        .then(result => {
+            console.log('Marked Complete')
+            response.json('Marked Complete')
+        })
+        .catch(error => console.error(error))
 
 })
 
 app.put('/markUnComplete', (request, response) => {
-    db.collection('todos').updateOne({thing: request.body.itemFromJS},{
-        $set: {
-            completed: false
-          }
-    },{
-        sort: {_id: -1},
-        upsert: false
-    })
-    .then(result => {
-        console.log('Marked Complete')
-        response.json('Marked Complete')
-    })
-    .catch(error => console.error(error))
+    db.collection('todos').updateOne({
+            thing: request.body.itemFromJS
+        }, {
+            $set: {
+                completed: false
+            }
+        }, {
+            sort: {
+                _id: -1
+            },
+            upsert: false
+        })
+        .then(result => {
+            console.log('Marked Complete')
+            response.json('Marked Complete')
+        })
+        .catch(error => console.error(error))
 
 })
 
 app.delete('/deleteItem', (request, response) => {
-    db.collection('todos').deleteOne({thing: request.body.itemFromJS})
-    .then(result => {
-        console.log('Todo Deleted')
-        response.json('Todo Deleted')
-    })
-    .catch(error => console.error(error))
+    db.collection('todos').deleteOne({
+            thing: request.body.itemFromJS
+        })
+        .then(result => {
+            console.log('Todo Deleted')
+            response.json('Todo Deleted')
+        })
+        .catch(error => console.error(error))
 
 })
 
-app.listen(process.env.PORT || PORT, ()=>{
+app.listen(process.env.PORT || PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
