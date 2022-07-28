6299ef757708b752dfb197e14ef0a176f99b229b
Walt Lungan <97710646+ztlaw@users.noreply.github.com>
2022-07-28 16:14:36 -0700

    commenting onto forked branch main.js

--- public/js/main.js
+++ public/js/main.js
@@ -1,17 +1,17 @@
-const deleteBtn = document.querySelectorAll('.fa-trash')
-const item = document.querySelectorAll('.item span')
-const itemCompleted = document.querySelectorAll('.item span.completed')
+const deleteBtn = document.querySelectorAll('.fa-trash') //selecting ALL elements that have the .fa-trash class 
+const item = document.querySelectorAll('.item span') //selecting ALL elements that have the .item class and span elements
+const itemCompleted = document.querySelectorAll('.item span.completed') //selecting ALL elements that have the .item class and span elements with a .completed class
 
-Array.from(deleteBtn).forEach((element)=>{
-    element.addEventListener('click', deleteItem)
+Array.from(deleteBtn).forEach((element)=>{ //creating an array from the deleteBtn variable which is all trash-can symbols 
+    element.addEventListener('click', deleteItem) // within the array is a forEach method with a click event and async callback function 
 })
 
-Array.from(item).forEach((element)=>{
-    element.addEventListener('click', markComplete)
+Array.from(item).forEach((element)=>{ //creating an array from the item variable 
+    element.addEventListener('click', markComplete) // within the array is a forEach method with a click event and async callback function
 })
 
-Array.from(itemCompleted).forEach((element)=>{
-    element.addEventListener('click', markUnComplete)
+Array.from(itemCompleted).forEach((element)=>{ //creating an array from the itemCompleted variable 
+    element.addEventListener('click', markUnComplete) // within the array is a forEach method with a click event and async callback function
 })
 
 async function deleteItem(){
@@ -33,21 +33,22 @@ async function deleteItem(){
     }
 }
 
-async function markComplete(){
-    const itemText = this.parentNode.childNodes[1].innerText
-    try{
-        const response = await fetch('markComplete', {
+async function markComplete(){ //async function tied to clickevent 
+    const itemText = this.parentNode.childNodes[1].innerText // initializing variable to 'this' object, which is the item that was clicked on and its text, using object properties
+    try{ //try is similar to a promise, to attempt to do something
+        const response = await fetch('markComplete', { //fetching a response from the back-end server that listens to ping markComplete. 1st param - server ping
+            //2nd param = options of the object
             method: 'put',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
-                'itemFromJS': itemText
+                'itemFromJS': itemText //from the fetch, using this information that was given from the server 
             })
           })
-        const data = await response.json()
+        const data = await response.json() //waiting json response
         console.log(data)
-        location.reload()
+        location.reload() //reloading page
 
-    }catch(err){
+    }catch(err){ //catching error after the TRY promise
         console.log(err)
     }
 }
