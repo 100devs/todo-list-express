2e75897000d75e3f82c735fb60393139bbb20143
Walt Lungan <97710646+ztlaw@users.noreply.github.com>
2022-07-28 16:12:42 -0700

    Update server.js

--- server.js
+++ server.js
@@ -1,93 +1,91 @@
-const express = require('express')
+//server functionality: 
+//this server works as a saveable journal for exercises, their weight, with reps and sets
+//for example: a client can save their
+//      exercise: 'bench press'
+//      weight (in lb or kg): 150
+//      reps: 10
+//      sets: 4
+
+//the exercises will be saved to a MongoDB collection for clients to come back to 
+
+const express = require('express') //express module + methods
 const app = express()
-const MongoClient = require('mongodb').MongoClient
-const PORT = 2121
+const MongoClient = require('mongodb').MongoClient //database module + methods
+const PORT = 2001 
 require('dotenv').config()
 
 
 let db,
     dbConnectionStr = process.env.DB_STRING,
-    dbName = 'todo'
+    dbName = 'work-out'
 
-MongoClient.connect(dbConnectionStr, { useUnifiedTopology: true })
+MongoClient.connect(dbConnectionStr, { useUnifiedTopology: true})
     .then(client => {
-        console.log(`Connected to ${dbName} Database`)
+        console.log(`Now connected to the ${dbName} database.`)
         db = client.db(dbName)
     })
-    
+
+
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
-app.use(express.urlencoded({ extended: true }))
+app.use(express.urlencoded({extended: true}))
 app.use(express.json())
 
-
-app.get('/',async (request, response)=>{
-    const todoItems = await db.collection('todos').find().toArray()
-    const itemsLeft = await db.collection('todos').countDocuments({completed: false})
-    response.render('index.ejs', { items: todoItems, left: itemsLeft })
-    // db.collection('todos').find().toArray()
-    // .then(data => {
-    //     db.collection('todos').countDocuments({completed: false})
-    //     .then(itemsLeft => {
-    //         response.render('index.ejs', { items: data, left: itemsLeft })
-    //     })
-    // })
-    // .catch(error => console.error(error))
+app.get('/', (req, res) => {
+    db.collection('exercises').find().toArray() //cannot read collection -- need to fix this issue. can it be network issues? WiFi whitelisting?
+        .then( data => {
+            
+            res.render('index.ejs', {info: data})
+        }).catch(error => console.error(error))
 })
 
-app.post('/addTodo', (request, response) => {
-    db.collection('todos').insertOne({thing: request.body.todoItem, completed: false})
-    .then(result => {
-        console.log('Todo Added')
-        response.redirect('/')
-    })
-    .catch(error => console.error(error))
+app.post('/addExercise', (req, res) => {
+    console.log(req)
+    db.collection('exercises').insertOne(
+    req.body 
+        )
+        .then(result => {
+           
+            console.log('Exercise added successfully.')
+            res.redirect('/')
+        }).catch(error => console.error(error))
 })
 
-app.put('/markComplete', (request, response) => {
-    db.collection('todos').updateOne({thing: request.body.itemFromJS},{
-        $set: {
-            completed: true
-          }
-    },{
-        sort: {_id: -1},
-        upsert: false
-    })
-    .then(result => {
-        console.log('Marked Complete')
-        response.json('Marked Complete')
-    })
-    .catch(error => console.error(error))
+// app.put('/addUpvote', (req, res) => {
+//     db.collection('exercises').updateOne(
+//         {exercise: req.body.exerciseS},
+//         {$set: {upvotes: req.body.upvotesS +1}},
+//         {sort: {_id: -1}, upsert: true})
+//     .then( result => {
+//         console.log('One exercise upvoted')
+//         response.json('Upvote applied')
+//         }).catch( error => console.error(error))
+// })
 
+
+app.put('/', (req, res) => {
+    db.collection('exercises')
+      .updateOne(
+        { _id: new ObjectId(req.body.id) },
+        // { $set: { taskDescription: req.body.taskDescription } }
+      )
+      .then((data) => {
+        return res.send(
+          db.collection('exercises').findOne({ _id: new ObjectId(req.body.id) })
+        )
+      })
+  })
+
+app.delete('/deleteExercise', (req, res) => {
+db.collection('exercises').deleteOne({
+    exercise: req.body.exerciseS
+})
+        .then(result =>{
+            console.log('Exercise Deleted')
+            res.json('Exercise Deleted')
+        })
 })
 
-app.put('/markUnComplete', (request, response) => {
-    db.collection('todos').updateOne({thing: request.body.itemFromJS},{
-        $set: {
-            completed: false
-          }
-    },{
-        sort: {_id: -1},
-        upsert: false
-    })
-    .then(result => {
-        console.log('Marked Complete')
-        response.json('Marked Complete')
-    })
-    .catch(error => console.error(error))
-
-})
-
-app.delete('/deleteItem', (request, response) => {
-    db.collection('todos').deleteOne({thing: request.body.itemFromJS})
-    .then(result => {
-        console.log('Todo Deleted')
-        response.json('Todo Deleted')
-    })
-    .catch(error => console.error(error))
-
-})
-
-app.listen(process.env.PORT || PORT, ()=>{
-    console.log(`Server running on port ${PORT}`)
+app.listen(process.env.PORT || PORT, () => {
+    console.log(`Server running on wave ${PORT}`)
 })
