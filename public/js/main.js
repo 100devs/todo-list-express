beb7c9217ce37302585609420328a4ddf1b20ab5
Amanda Faulkenberry <amandalfwrites@gmail.com>
2022-07-27 20:00:53 -0400

    added comments

--- public/js/main.js
+++ public/js/main.js
@@ -1,20 +1,32 @@
+//declares a variable for the delete button
 const deleteBtn = document.querySelectorAll('.fa-trash')
+//declares a variable for the list items
 const item = document.querySelectorAll('.item span')
+//declaring a variable for the completed items
 const itemCompleted = document.querySelectorAll('.item span.completed')
 
+//a loop to create event listeners for the delete function (click event)
 Array.from(deleteBtn).forEach((element)=>{
     element.addEventListener('click', deleteItem)
 })
 
+//a loop to create event listeners for the marking items complete function (click event)
 Array.from(item).forEach((element)=>{
     element.addEventListener('click', markComplete)
 })
 
+//a loop to create event listeners for the marking items uncomplete function (click event)
 Array.from(itemCompleted).forEach((element)=>{
     element.addEventListener('click', markUnComplete)
 })
 
+//function that deletes items from the list (async function)
 async function deleteItem(){
+    //declaring a variable to the list item in the DOM
+    //The read-only parentNode property of the Node interface returns the parent of the specified node in the DOM tree.
+    //The read-only childNodes property of the Node interface returns a live NodeList of child nodes of the given element where the first child node is assigned index 0. 
+    //Child nodes include elements, text and comments.
+    //SOURCE: MDN
     const itemText = this.parentNode.childNodes[1].innerText
     try{
         const response = await fetch('deleteItem', {
@@ -24,6 +36,7 @@ async function deleteItem(){
               'itemFromJS': itemText
             })
           })
+           //declare a data variable to console log json
         const data = await response.json()
         console.log(data)
         location.reload()
@@ -32,10 +45,11 @@ async function deleteItem(){
         console.log(err)
     }
 }
-
+//function that marks an item complete on the list (async function)
 async function markComplete(){
     const itemText = this.parentNode.childNodes[1].innerText
     try{
+        // declaring a variable to get a delete response from the server (deleteItem method)
         const response = await fetch('markComplete', {
             method: 'put',
             headers: {'Content-Type': 'application/json'},
@@ -43,6 +57,7 @@ async function markComplete(){
                 'itemFromJS': itemText
             })
           })
+          //declare a data variable to console log json
         const data = await response.json()
         console.log(data)
         location.reload()
@@ -52,9 +67,12 @@ async function markComplete(){
     }
 }
 
+//function that marks an item uncomplete on the list (async function)
 async function markUnComplete(){
+    
     const itemText = this.parentNode.childNodes[1].innerText
     try{
+        / declaring a variable to get a markUncomplete response from the server (mark Uncomplete method)
         const response = await fetch('markUnComplete', {
             method: 'put',
             headers: {'Content-Type': 'application/json'},
@@ -62,6 +80,7 @@ async function markUnComplete(){
                 'itemFromJS': itemText
             })
           })
+           //declare a data variable to console log json
         const data = await response.json()
         console.log(data)
         location.reload()
--- server.js
+++ server.js
@@ -1,26 +1,40 @@
+//assigns a variable for the express packages that are installed allowing access to express
 const express = require('express')
+//Assigns a variable to express so that it is easier to use in the code when needed
 const app = express()
+//assigns a variable for the express packages that are installed allowing access to MongDB
 const MongoClient = require('mongodb').MongoClient
+//Assigns PORT as a variable so that you don't have to remember the specified number
 const PORT = 2121
+//requiring the dotenv.config used to store things in MongDB? to create environment variables by granting access to the .env file
+//require the .env module to create environment variables
 require('dotenv').config()
 
-
+//Specifies the MongoDB database that will be used
 let db,
     dbConnectionStr = process.env.DB_STRING,
     dbName = 'todo'
-
-MongoClient.connect(dbConnectionStr, { useUnifiedTopology: true })
+//Links the node server to the MongoDB database
+MongoClient.connect(dbConnectionStr, { useUnifiedTopology: true }) //** look into useUnifiedTopology required or not
     .then(client => {
         console.log(`Connected to ${dbName} Database`)
         db = client.db(dbName)
     })
-    
+ //Used to set and configure the behavior of the server   
 app.set('view engine', 'ejs')
+//Setting up access to the public folder for css and client side JS
 app.use(express.static('public'))
+//setting up The express.urlencoded() function which is a built-in middleware function in Express. 
+//It parses incoming requests with urlencoded payloads and is based on body-parser.
 app.use(express.urlencoded({ extended: true }))
+//setting up access to allow the app to convert the object into json
 app.use(express.json())
 
-
+//chaining using async await
+// GET Method
+// setting up get request in the root folder,
+// with async function. declaring a todoItems variable and passing it to the array.
+// declaring a variable for the uncompleted tasks
 app.get('/',async (request, response)=>{
     const todoItems = await db.collection('todos').find().toArray()
     const itemsLeft = await db.collection('todos').countDocuments({completed: false})
@@ -35,32 +49,45 @@ app.get('/',async (request, response)=>{
     // .catch(error => console.error(error))
 })
 
+//Promise Chaining (.then .catch .finally)
+//Post method 
+// creating a new request to add new documents to the DB and assigning false status(incomplete task)
 app.post('/addTodo', (request, response) => {
     db.collection('todos').insertOne({thing: request.body.todoItem, completed: false})
+    //Notifies you that the item has been added and the redirects back to the homepage
     .then(result => {
         console.log('Todo Added')
         response.redirect('/')
     })
+    //handling error
     .catch(error => console.error(error))
 })
 
+//Put method
+// updating the document's completed value to true
 app.put('/markComplete', (request, response) => {
     db.collection('todos').updateOne({thing: request.body.itemFromJS},{
         $set: {
             completed: true
           }
     },{
+        //add's updated documents to the top of the list
         sort: {_id: -1},
+        //**won't create new entry if required information is missing
         upsert: false
     })
+    //notifies you that it has been marked complete
     .then(result => {
         console.log('Marked Complete')
         response.json('Marked Complete')
     })
+    //handling error
     .catch(error => console.error(error))
 
 })
 
+// PUT Method
+// updates the document's completed value to false
 app.put('/markUnComplete', (request, response) => {
     db.collection('todos').updateOne({thing: request.body.itemFromJS},{
         $set: {
@@ -71,13 +98,15 @@ app.put('/markUnComplete', (request, response) => {
         upsert: false
     })
     .then(result => {
-        console.log('Marked Complete')
-        response.json('Marked Complete')
+        console.log('Marked Complete') //should say "Marked Uncomplete"
+        response.json('Marked Complete') //should say "Marked Uncomplete"
     })
     .catch(error => console.error(error))
 
 })
 
+// DELETE Method
+// removes the documents from the DB,console logs and responds with json
 app.delete('/deleteItem', (request, response) => {
     db.collection('todos').deleteOne({thing: request.body.itemFromJS})
     .then(result => {
@@ -88,6 +117,8 @@ app.delete('/deleteItem', (request, response) => {
 
 })
 
+// Starts Server
+// if the server is running, it sets up a listen method for Heroku (process.env) or the local port from the PORT variable
 app.listen(process.env.PORT || PORT, ()=>{
     console.log(`Server running on port ${PORT}`)
 })
