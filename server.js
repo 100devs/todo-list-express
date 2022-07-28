4370ebd5d4fa127501d6912bf78f89d084b0161e
jazzypants1989 <jessepence@gmail.com>
2022-07-28 09:13:27 -0700

    FINALLY

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ let db,
     dbConnectionStr = process.env.DB_STRING,
     dbName = 'todo'
 
-MongoClient.connect(dbConnectionStr, { useUnifiedTopology: true })
+MongoClient.connect(dbConnectionStr), { useUnifiedTopology: true }
     .then(client => {
         console.log(`Connected to ${dbName} Database`)
         db = client.db(dbName)
