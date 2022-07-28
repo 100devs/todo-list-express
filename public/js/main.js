ef0fd10f37b668f1566fdf52ff8a10cb37c0bb8c
cesar-codes <cesar.avina@gmail.com>
2022-07-28 17:34:52 -0400

    update main.js comments

--- public/js/main.js
+++ public/js/main.js
@@ -1,65 +1,70 @@
 
-// targeting all of the trash icon elems 
+// Assigns a NodeList of all HTMLElements with a class of `fa-trash` 
+// to the const variable `deleteBtn`
 const deleteBtn = document.querySelectorAll('.fa-trash')
-
-// targets <span> tags where parent has class='item'
+// Assigns a NodeList of all HTMLElements that are <span>s and children of 
+// elements with a class of `item` to the const variable `item`
 const item = document.querySelectorAll('.item span')
-
-// tagetting all <span> tags with the class of completed where the parent has the class of item
+// Assign a NodeList of all HTMLElements that are <span>s with a class of `completed` 
+// and children of elements with a class of `item` to the const variable `itemCompleted`
 const itemCompleted = document.querySelectorAll('.item span.completed')
 
 
-// create anm array from the queryselectorall results, 
-// loop through all, and add a click event listener 
-// that fires off that deleteItem function 
+// Convert `deleteBtn` to an array of HTMLElements, then call `forEach` on it
 Array.from(deleteBtn).forEach((element)=>{
+    // Adds a click event listener (smurf) to each element, calling the `deleteItem` func
     element.addEventListener('click', deleteItem)
 })
 
-// create anm array from the queryselectorall results, 
-// loop through all, and add a click event listener 
-// that fires off that markComplete function 
+// Convert `item` to an array of HTMLElements, then call `forEach` on it
 Array.from(item).forEach((element)=>{
+    // Adds an click event listener to each element, calling the `markComplete` function
     element.addEventListener('click', markComplete)
 })
 
-// create anm array from the queryselectorall results, 
-// loop through all, and add a click event listener 
-// that fires off that markUnßComplete function 
+// Convert `itemCompleted` to an array of HTMLElements, then call `forEach` on it
 Array.from(itemCompleted).forEach((element)=>{
+    // Add an click event listener to each element, calling the `markUnComplete` function
     element.addEventListener('click', markUnComplete)
 })
 
+// ASYNC FUNCTIONS 
 // function to delete elems
 async function deleteItem(){
-    // grabs 1st item inside <li> w/ class of completed and delets it
-    // sets headers to expect JSON response, and the itemText var
-    // contents in the body
+    // From the current <span> (bound to this by the event listener), get the parentNode - 
+    // the <li> - then get the 1th childNode - the <span>, then retrieve the text content of it 
+    // by reading the `innerText` property
     const itemText = this.parentNode.childNodes[1].innerText
     try{
-        // sends DELETE request to the deleteItem endpoint, 
+        // Make a fetch to the relative path `deleteItem`
         const response = await fetch('deleteItem', {
+            // Set method of request to DELETE
             method: 'delete',
+            // Set header `Content-Type` to `application/json` so it knows to expect and parse JSON
             headers: {'Content-Type': 'application/json'},
+            // Send the obj with a property of itemFromJS and value of the `itemText` of the current item as a JSON string
             body: JSON.stringify({
               'itemFromJS': itemText
             })
           })
-          // waiting for JSON response and parses it => console
+        // Try to load and parse the response body as JSON, assigning it to data
         const data = await response.json()
         console.log(data)
         // reloads page
         location.reload()
 
     }catch(err){
+        // log errors, if any
         console.log(err)
     }
 }
-
+// function to mark items as complete
 async function markComplete(){
+    // Make a fetch to the relative path `markComplete`
     const itemText = this.parentNode.childNodes[1].innerText
     try{
         const response = await fetch('markComplete', {
+            // Set method of the request to PUT, otherwise same as deleteItem()
             method: 'put',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
@@ -71,13 +76,17 @@ async function markComplete(){
         location.reload()
 
     }catch(err){
+        // logs errors, if any
         console.log(err)
     }
 }
 
+// function to mark items as unComplete
 async function markUnComplete(){
     const itemText = this.parentNode.childNodes[1].innerText
     try{
+        // Make a fetch to the relative path `markUnComplete`
+        // otherwise, same as markComplete()
         const response = await fetch('markUnComplete', {
             method: 'put',
             headers: {'Content-Type': 'application/json'},
@@ -90,6 +99,7 @@ async function markUnComplete(){
         location.reload()
 
     }catch(err){
+        // logs errors, if any
         console.log(err)
     }
 }
