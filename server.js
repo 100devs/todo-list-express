f7be84092c17192747d6132946f5ec92d3d6797c
Mohamed A Hameed <emah84@gmail.com>
2022-07-28 17:08:45 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,30 +1,32 @@
-const express = require('express')
-const app = express()
-const MongoClient = require('mongodb').MongoClient
-const PORT = 2121
-require('dotenv').config()
+// this is the main express file "server.js" where all the heavy lifitng happens
+
+const express = require('express')  // for using express magic 
+const app = express()       // for using express magic 
+const MongoClient = require('mongodb').MongoClient  // for using Mongo
+const PORT = 2121                 // using port 2121 on local host 
+require('dotenv').config()      // to use env variables
 
 
 let db,
-    dbConnectionStr = process.env.DB_STRING,
-    dbName = 'todo'
+    dbConnectionStr = process.env.DB_STRING,      // using env to enable heroku to use the required port 
+    dbName = 'todo'                                 // name of the data base on mongo db
 
-MongoClient.connect(dbConnectionStr, { useUnifiedTopology: true })
+MongoClient.connect(dbConnectionStr, { useUnifiedTopology: true })     // this is for initializing & connecting to mongo atlas db
     .then(client => {
-        console.log(`Connected to ${dbName} Database`)
+        console.log(`Connected to ${dbName} Database`)                  // will log the port # (2121 on local , some other value on heroku
         db = client.db(dbName)
     })
     
-app.set('view engine', 'ejs')
-app.use(express.static('public'))
-app.use(express.urlencoded({ extended: true }))
-app.use(express.json())
+app.set('view engine', 'ejs')                                           // to use ejs 
+app.use(express.static('public'))                                       // public folder where all the files used (style.css, main.js * any other file) will reside
+app.use(express.urlencoded({ extended: true }))                         
+app.use(express.json())                                                 
 
 
-app.get('/',async (request, response)=>{
-    const todoItems = await db.collection('todos').find().toArray()
+app.get('/',async (request, response)=>{                                                   // the homepage or main route (/)
+    const todoItems = await db.collection('todos').find().toArray()                         // loads the db items on to an Array
     const itemsLeft = await db.collection('todos').countDocuments({completed: false})
-    response.render('index.ejs', { items: todoItems, left: itemsLeft })
+    response.render('index.ejs', { items: todoItems, left: itemsLeft })                     // ejs process it to feed to the html
     // db.collection('todos').find().toArray()
     // .then(data => {
     //     db.collection('todos').countDocuments({completed: false})
@@ -35,8 +37,8 @@ app.get('/',async (request, response)=>{
     // .catch(error => console.error(error))
 })
 
-app.post('/addTodo', (request, response) => {
-    db.collection('todos').insertOne({thing: request.body.todoItem, completed: false})
+app.post('/addTodo', (request, response) => {                                                     // the post or Create function that adds things to the todolist
+    db.collection('todos').insertOne({thing: request.body.todoItem, completed: false})               // the insertOne() fucntion goes through the db and inserts the item specified in the DOM "request.body.todoItem"
     .then(result => {
         console.log('Todo Added')
         response.redirect('/')
@@ -44,8 +46,8 @@ app.post('/addTodo', (request, response) => {
     .catch(error => console.error(error))
 })
 
-app.put('/markComplete', (request, response) => {
-    db.collection('todos').updateOne({thing: request.body.itemFromJS},{
+app.put('/markComplete', (request, response) => {                                                   // the put or update command that updates the status of todo item whether compeleted or not
+    db.collection('todos').updateOne({thing: request.body.itemFromJS},{                                  // the updateOne() fucntion goes through the db and updates the item specified in the DOM "request.body.itemFromJS"
         $set: {
             completed: true
           }
@@ -61,8 +63,8 @@ app.put('/markComplete', (request, response) => {
 
 })
 
-app.put('/markUnComplete', (request, response) => {
-    db.collection('todos').updateOne({thing: request.body.itemFromJS},{
+app.put('/markUnComplete', (request, response) => {                                                     // the put or update command that marks the status of the item as un complete
+    db.collection('todos').updateOne({thing: request.body.itemFromJS},{                                   // the updateOne() fucntion goes through the db and updates the item specified in the DOM "request.body.itemFromJS" 
         $set: {
             completed: false
           }
@@ -78,8 +80,8 @@ app.put('/markUnComplete', (request, response) => {
 
 })
 
-app.delete('/deleteItem', (request, response) => {
-    db.collection('todos').deleteOne({thing: request.body.itemFromJS})
+app.delete('/deleteItem', (request, response) => {                                                  // the delete function that deletes the item from the todo list
+    db.collection('todos').deleteOne({thing: request.body.itemFromJS})                              // deleteOne() function goes through the db and looks for the item specified in the DOM "request.body.itemFromJS" & deletes it
     .then(result => {
         console.log('Todo Deleted')
         response.json('Todo Deleted')
@@ -88,6 +90,6 @@ app.delete('/deleteItem', (request, response) => {
 
 })
 
-app.listen(process.env.PORT || PORT, ()=>{
+app.listen(process.env.PORT || PORT, ()=>{                                                      // the listemn fucntion that listens to the user activity on the site, like clicks etc..
     console.log(`Server running on port ${PORT}`)
 })
