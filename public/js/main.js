227b649a5209872c27d32f2278698acfc48346ba
monicacodes <102931868+monicacodes@users.noreply.github.com>
2022-07-27 21:55:01 -0400

    Update main.js

--- public/js/main.js
+++ public/js/main.js
@@ -1,13 +1,13 @@
-const deleteBtn = document.querySelectorAll('.fa-trash')
-const item = document.querySelectorAll('.item span')
-const itemCompleted = document.querySelectorAll('.item span.completed')
+const deleteBtn = document.querySelectorAll('.fa-trash') //Creating a variable and assigning it to a selection of all elements of a class of trashcan
+const item = document.querySelectorAll('.item span') //Creating a variable and assigning it to a selection of span tags inside of a parent that has a class of item 
+const itemCompleted = document.querySelectorAll('.item span.completed') //Creating a variable and assigning it to a selection of spans with a class of "completed" inside of a parent with a class of "item" 
 
-Array.from(deleteBtn).forEach((element)=>{
-    element.addEventListener('click', deleteItem)
-})
+Array.from(deleteBtn).forEach((element)=>{ //Creating an array from our selection and starting a loop
+    element.addEventListener('click', deleteItem) //Adding an event listener to the current item that waits for a click and then calls a function called deleteItem
+}) //Close our loop
 
-Array.from(item).forEach((element)=>{
-    element.addEventListener('click', markComplete)
+Array.from(item).forEach((element)=>{ //Creating an array from our selection and starting a loop
+    element.addEventListener('click', markComplete) //Adding an event listener to the current item that waits for a click and then calls a function called markComplete
 })
 
 Array.from(itemCompleted).forEach((element)=>{
