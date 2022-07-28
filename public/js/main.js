8e6905185b4286a2ced7fa91ec20299ab3975b58
HondaChan14 <92832561+HondaChan14@users.noreply.github.com>
2022-07-27 20:11:35 -0400

    comments

--- public/js/main.js
+++ public/js/main.js
@@ -1,22 +1,31 @@
+// declaring a variable for delete button
 const deleteBtn = document.querySelectorAll('.fa-trash')
+// declaring a variable for list item
 const item = document.querySelectorAll('.item span')
+// declaring a variable for completed task items
 const itemCompleted = document.querySelectorAll('.item span.completed')
 
+//a loop to create event listeners for delete function
 Array.from(deleteBtn).forEach((element)=>{
     element.addEventListener('click', deleteItem)
 })
 
+//a loop to create event listeners for mark complete function
 Array.from(item).forEach((element)=>{
     element.addEventListener('click', markComplete)
 })
 
+//a loop to create event listeners for mark uncomplete function
 Array.from(itemCompleted).forEach((element)=>{
     element.addEventListener('click', markUnComplete)
 })
 
+//creating an async function...
 async function deleteItem(){
+    //declaring a variable to the list item in the DOM
     const itemText = this.parentNode.childNodes[1].innerText
     try{
+        // declaring a variable to get a delete response from the server (deleteItem method)
         const response = await fetch('deleteItem', {
             method: 'delete',
             headers: {'Content-Type': 'application/json'},
@@ -24,6 +33,7 @@ async function deleteItem(){
               'itemFromJS': itemText
             })
           })
+          //declare a data variable to console log json
         const data = await response.json()
         console.log(data)
         location.reload()
@@ -34,6 +44,8 @@ async function deleteItem(){
 }
 
 async function markComplete(){
+    // declaring a variable to get a markComplete response from the server (mark complete method)
+    
     const itemText = this.parentNode.childNodes[1].innerText
     try{
         const response = await fetch('markComplete', {
@@ -53,6 +65,8 @@ async function markComplete(){
 }
 
 async function markUnComplete(){
+
+    // declaring a variable to get a markUncomplete response from the server (mark Uncomplete method)
     const itemText = this.parentNode.childNodes[1].innerText
     try{
         const response = await fetch('markUnComplete', {
