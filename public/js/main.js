de3d1d4ebc462e9fa2794fc4a3124aee1f40ba30
Michael Joel <michaeljoelt@gmail.com>
2022-07-28 15:19:21 -0700

    added comments to main.js and index.ejs

--- public/js/main.js
+++ public/js/main.js
@@ -1,72 +1,92 @@
+/*
+    set up references to elements on the page that we need to set up listeners for
+*/
 const deleteBtn = document.querySelectorAll('.fa-trash')
 const item = document.querySelectorAll('.item span')
 const itemCompleted = document.querySelectorAll('.item span.completed')
 
-Array.from(deleteBtn).forEach((element)=>{
+/*
+    add event listeners to the elements listed above
+*/
+Array.from(deleteBtn).forEach((element) => {
     element.addEventListener('click', deleteItem)
 })
-
-Array.from(item).forEach((element)=>{
+Array.from(item).forEach((element) => {
     element.addEventListener('click', markComplete)
 })
-
-Array.from(itemCompleted).forEach((element)=>{
+Array.from(itemCompleted).forEach((element) => {
     element.addEventListener('click', markUnComplete)
 })
 
-async function deleteItem(){
+/*
+    request that the server deletes an item from database whenever a delete icon is clicked
+*/
+async function deleteItem() {
     const itemText = this.parentNode.childNodes[1].innerText
-    try{
+    try {
         const response = await fetch('deleteItem', {
             method: 'delete',
-            headers: {'Content-Type': 'application/json'},
+            headers: {
+                'Content-Type': 'application/json'
+            },
             body: JSON.stringify({
-              'itemFromJS': itemText
+                'itemFromJS': itemText
             })
-          })
+        })
         const data = await response.json()
         console.log(data)
         location.reload()
 
-    }catch(err){
+    } catch (err) {
         console.log(err)
     }
 }
 
-async function markComplete(){
+
+/*
+    request that the server updates an item to be marked as "complete"
+*/
+async function markComplete() {
     const itemText = this.parentNode.childNodes[1].innerText
-    try{
+    try {
         const response = await fetch('markComplete', {
             method: 'put',
-            headers: {'Content-Type': 'application/json'},
+            headers: {
+                'Content-Type': 'application/json'
+            },
             body: JSON.stringify({
                 'itemFromJS': itemText
             })
-          })
+        })
         const data = await response.json()
         console.log(data)
         location.reload()
 
-    }catch(err){
+    } catch (err) {
         console.log(err)
     }
 }
 
-async function markUnComplete(){
+/*
+    request that the server updates an item to be marked as "not complete"
+*/
+async function markUnComplete() {
     const itemText = this.parentNode.childNodes[1].innerText
-    try{
+    try {
         const response = await fetch('markUnComplete', {
             method: 'put',
-            headers: {'Content-Type': 'application/json'},
+            headers: {
+                'Content-Type': 'application/json'
+            },
             body: JSON.stringify({
                 'itemFromJS': itemText
             })
-          })
+        })
         const data = await response.json()
         console.log(data)
         location.reload()
 
-    }catch(err){
+    } catch (err) {
         console.log(err)
     }
 }
