5b67d031059b738e62dba6ed2ddcf4f9a4fdec54
Joonbo Rhie <jb.rhie@gmail.com>
2022-07-29 12:21:19 -0700

    Add comments for each section to describe what code does

--- public/js/main.js
+++ public/js/main.js
@@ -2,21 +2,26 @@ const deleteBtn = document.querySelectorAll('.fa-trash')
 const item = document.querySelectorAll('.item span')
 const itemCompleted = document.querySelectorAll('.item span.completed')
 
+// add event listener for each delete button
 Array.from(deleteBtn).forEach((element)=>{
     element.addEventListener('click', deleteItem)
 })
 
+// add event listener for each item if completed
 Array.from(item).forEach((element)=>{
     element.addEventListener('click', markComplete)
 })
 
+// add event listener for each item if uncompleted
 Array.from(itemCompleted).forEach((element)=>{
     element.addEventListener('click', markUnComplete)
 })
 
+// function to run each time the delete button is clicked
 async function deleteItem(){
     const itemText = this.parentNode.childNodes[1].innerText
     try{
+        // send deleteItem URL request 
         const response = await fetch('deleteItem', {
             method: 'delete',
             headers: {'Content-Type': 'application/json'},
@@ -33,9 +38,11 @@ async function deleteItem(){
     }
 }
 
+// function to run each time the text is clicked and the task is completed
 async function markComplete(){
     const itemText = this.parentNode.childNodes[1].innerText
     try{
+        // send markComplete URL request 
         const response = await fetch('markComplete', {
             method: 'put',
             headers: {'Content-Type': 'application/json'},
@@ -52,9 +59,11 @@ async function markComplete(){
     }
 }
 
+// function to run each time the text is clicked and the task is reverted back
 async function markUnComplete(){
     const itemText = this.parentNode.childNodes[1].innerText
     try{
+        // send markUncomplete URL request
         const response = await fetch('markUnComplete', {
             method: 'put',
             headers: {'Content-Type': 'application/json'},
