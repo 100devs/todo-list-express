b0ced8be62d952ec6dab333e0bffcc52beed1670
Garland <hello@garlandkey.com>
2022-07-29 17:15:31 -0500

    completed line-by-line comments

--- public/js/main.js
+++ public/js/main.js
@@ -1,71 +1,103 @@
+// targeting all DOM elements with the 'fa-trash' class
 const deleteBtn = document.querySelectorAll('.fa-trash')
+// targeting all DOM elements that are spans that are a child of of the 'item' class
 const item = document.querySelectorAll('.item span')
+//  targeting all DOM elements that are the class of 'completed' that are children of spans that are children of the 'item' class 
 const itemCompleted = document.querySelectorAll('.item span.completed')
 
+
+// create an array fromm the querySelectorAll results, loop through all, and add a 'click' event listener that fires the 'deleteItem' function
 Array.from(deleteBtn).forEach((element)=>{
     element.addEventListener('click', deleteItem)
 })
 
+// create an array fromm the querySelectorAll results, loop through all, and add a 'click' event listener that fires the 'markComplete' function
 Array.from(item).forEach((element)=>{
     element.addEventListener('click', markComplete)
 })
 
+// create an array fromm the querySelectorAll results, loop through all, and add a 'click' event listener that fires the 'markUnComplete' function
 Array.from(itemCompleted).forEach((element)=>{
     element.addEventListener('click', markUnComplete)
 })
 
+// Asyncronous function 
 async function deleteItem(){
+    // traverses the DOM up to the partent li and gets the text of the first span element
     const itemText = this.parentNode.childNodes[1].innerText
+    // awaits the completion of the delete request
     try{
         const response = await fetch('deleteItem', {
+            // sets the method name in the header
             method: 'delete',
+            // sets headers to json response
             headers: {'Content-Type': 'application/json'},
+            // sends itemText to be deleted
             body: JSON.stringify({
               'itemFromJS': itemText
             })
           })
+        // sets response to json
         const data = await response.json()
+        // logs data in the browser console
         console.log(data)
+        // refreshes the page in the browser
         location.reload()
-
+    // returns an error to the console log if an error is returned
     }catch(err){
         console.log(err)
     }
 }
-
+// Asyncronous function 
 async function markComplete(){
+    // traverses the DOM up to the partent li and gets the text of the first span element
     const itemText = this.parentNode.childNodes[1].innerText
+    // awaits the completion of the delete request
     try{
         const response = await fetch('markComplete', {
+            // sets the method name in the header
             method: 'put',
+            // sets headers to json response
             headers: {'Content-Type': 'application/json'},
+            // sends itemText to be deleted
             body: JSON.stringify({
                 'itemFromJS': itemText
             })
           })
+        // sets response to json
         const data = await response.json()
+        // logs data in the browser console
         console.log(data)
+        // refreshes the page in the browser
         location.reload()
-
+    // returns an error to the console log if an error is returned
     }catch(err){
         console.log(err)
     }
 }
-
+// Asyncronous function 
 async function markUnComplete(){
+    // traverses the DOM up to the partent li and gets the text of the first span element
     const itemText = this.parentNode.childNodes[1].innerText
+    // awaits the completion of the delete request
     try{
         const response = await fetch('markUnComplete', {
+            // sets the method name in the header
             method: 'put',
+            // sets headers to json response
             headers: {'Content-Type': 'application/json'},
+            // sends itemText to be deleted
             body: JSON.stringify({
                 'itemFromJS': itemText
             })
           })
+        // sets response to json
         const data = await response.json()
+        // logs data in the browser console
         console.log(data)
+        // refreshes the page in the browser
         location.reload()
-
+    // returns an error to the console log if an error is returned
     }catch(err){
         console.log(err)
     }
--- server.js
+++ server.js
@@ -1,28 +1,45 @@
+// declaring the variable express and assigning the value of require('express') so that you don't have to change it in multiple places
 const express = require('express')
+// declaring the variable app and assigning the value of the express() module so that you don't have to change it in multiple places
 const app = express()
+// declaring the variable MongoClient and assigning the value of require('mongodb').MongoClient so that you don't have to change it in multiple places
 const MongoClient = require('mongodb').MongoClient
+// declaring the constant variable PORT and assigning a port number to is so that you can easily change it later
 const PORT = 2121
+
+// importing the dotenv module and calling the config method, which reads from the .env file and adds them all to process.env
 require('dotenv').config()
 
-
+// declaring unassigned db variable
 let db,
+    // declaring dbConnectionStr variable and assigning process.env.DB_STRING, which is the data from .env
     dbConnectionStr = process.env.DB_STRING,
+    // declaring dbName variable and assigning the string todo so that you don't have to change it in multiple places
     dbName = 'todo'
 
+/* Connecting server to mongoDB client */
 MongoClient.connect(dbConnectionStr, { useUnifiedTopology: true })
+    /* Console logs a string and assigns the database name when the promise is fulfilled */
     .then(client => {
         console.log(`Connected to ${dbName} Database`)
         db = client.db(dbName)
     })
-    
+
+/* sets 'ejs' as the renering engine for the .render() method */
 app.set('view engine', 'ejs')
+
+// serves 'public' folder contents as is
 app.use(express.static('public'))
+//middleware (intercepts requests/responses) - allows query data to be passed to server via request
 app.use(express.urlencoded({ extended: true }))
+// middle ware to load the json body parser for incoming requests
 app.use(express.json())
 
-
+// defvines a 'get method to the root of the se5r4ver
 app.get('/',async (request, response)=>{
+    // request to mongo to return all records from 'todos' collection in an array
     const todoItems = await db.collection('todos').find().toArray()
+    // request to monogoDB to return 
     const itemsLeft = await db.collection('todos').countDocuments({completed: false})
     response.render('index.ejs', { items: todoItems, left: itemsLeft })
     // db.collection('todos').find().toArray()
@@ -35,34 +52,47 @@ app.get('/',async (request, response)=>{
     // .catch(error => console.error(error))
 })
 
+// POST request to add items to todo list
 app.post('/addTodo', (request, response) => {
+    // Adds new todo item to the db, with the completed field defaulted to false.
     db.collection('todos').insertOne({thing: request.body.todoItem, completed: false})
+    // Handles returned promise, logs to the server console and redirects back to the root page.
     .then(result => {
         console.log('Todo Added')
         response.redirect('/')
     })
+    // logs an error to the console if there is one.
     .catch(error => console.error(error))
 })
 
+// PUT request (update) updating one item in the todo list
 app.put('/markComplete', (request, response) => {
+    // selecting the 'todos' collection of our db and updating an item
     db.collection('todos').updateOne({thing: request.body.itemFromJS},{
+        // using $set operator from mongoDB to change the completed key to true
         $set: {
             completed: true
           }
     },{
+        // using the mongoDB sort method to sort by id. -1 means that we are getting the latest first (descending order)
         sort: {_id: -1},
+        // setting the upsert (insert + update) mongoDB method to false
         upsert: false
     })
+    // Sends console log to server and in json format when the promise is completed.
     .then(result => {
         console.log('Marked Complete')
         response.json('Marked Complete')
     })
+    // Sends console log to server if there is an error.
     .catch(error => console.error(error))
 
 })
 
+// Add request handler to the `PUT` method of the `/markUnComplete` path
 app.put('/markUnComplete', (request, response) => {
     db.collection('todos').updateOne({thing: request.body.itemFromJS},{
+        // UpdateFilter containing the `$set` update operator, telling MongoDB to set the completed property to false.
         $set: {
             completed: false
           }
@@ -78,16 +108,21 @@ app.put('/markUnComplete', (request, response) => {
 
 })
 
+// handles DELETE requests at the defined endpoint
 app.delete('/deleteItem', (request, response) => {
+    // mongoDB function to delete a single todo item
     db.collection('todos').deleteOne({thing: request.body.itemFromJS})
+    // Handles returned promise, logs to the server console and sends json response.
     .then(result => {
         console.log('Todo Deleted')
         response.json('Todo Deleted')
     })
+    // Sends message to server console if there is an error.
     .catch(error => console.error(error))
 
 })
-
+// Starts the server and tells it to listen on the environment's defined port number. It will default to the provided PORT number if the environment doesn't define the port.
 app.listen(process.env.PORT || PORT, ()=>{
+    // Logs to the server console if the server successfully starts
     console.log(`Server running on port ${PORT}`)
 })
