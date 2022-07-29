e46169a937dd6f55afcb3f3e2ccfe16ad0bdac01
AwsYaseen22 <aws.yaseen.22@gmail.com>
2022-07-29 23:33:56 +0300

    start commenting main.js file

--- public/js/main.js
+++ public/js/main.js
@@ -1,20 +1,38 @@
+// hold elements with a class of fa-trash with deleteBtn variable
 const deleteBtn = document.querySelectorAll(".fa-trash");
+// hold span elements that found inside a parent has class of 'item' with item variable
 const item = document.querySelectorAll(".item span");
+// hold span elements that has class 'completed' and that found inside a parent has class of 'item' with itemCompleted variable
 const itemCompleted = document.querySelectorAll(".item span.completed");
 
+// create array from the iterable variable deleteBtn
+// loop through them
+// add event listener for each element listen for 'click' when it catched fire a deleteItem function
 Array.from(deleteBtn).forEach((element) => {
   element.addEventListener("click", deleteItem);
 });
 
+// create array from the iterable variable item
+// loop through them
+// add event listener for each element listen for 'click' when it catched fire a markComplete function
 Array.from(item).forEach((element) => {
   element.addEventListener("click", markComplete);
 });
 
+// create array from the iterable variable itemCompleted
+// loop through them
+// add event listener for each element listen for 'click' when it catched fire a markUnComplete function
 Array.from(itemCompleted).forEach((element) => {
   element.addEventListener("click", markUnComplete);
 });
 
+// definition of deleteItem function that will used for deletion and make it async as it will deals with the server that will take time to fulfilled
 async function deleteItem() {
+  // store 'this' which hold the clicked item's parent's first child's text in itemText variable
+  // 'this': span.fa.fa-trash
+  // 'parentNode' : li.item
+  // 'childNodes[1]': [text, *span*, text, text,span.fa.fa-trash, text]
+  // 'innerText': the text of the above selected span
   const itemText = this.parentNode.childNodes[1].innerText;
   try {
     const response = await fetch("deleteItem", {
@@ -32,6 +50,7 @@ async function deleteItem() {
   }
 }
 
+// definition of markComplete function that will used for make a clicked item complete if it is incomplete and make it async as it will deals with the server that will take time to fulfilled
 async function markComplete() {
   console.log(this.parentNode.childNodes[1]);
   const itemText = this.parentNode.childNodes[1].innerText;
@@ -51,6 +70,7 @@ async function markComplete() {
   }
 }
 
+// definition of markUnComplete function that will used for make a clicked item incomplete if it is complete and make it async as it will deals with the server that will take time to fulfilled
 async function markUnComplete() {
   const itemText = this.parentNode.childNodes[1].innerText;
   try {
