bf4d1d970cc69aabadf63aef371646732d7f0300
AwsYaseen22 <aws.yaseen.22@gmail.com>
2022-07-31 00:29:11 +0300

    complete commeting server.js file and that finish comment the total project

--- server.js
+++ server.js
@@ -26,18 +26,29 @@ MongoClient.connect(dbConnectionStr, { useUnifiedTopology: true }).then(
   }
 );
 
-
+// set the view engine to handle render generated html file to the client to EJS
 app.set("view engine", "ejs");
+// locate the folder that hold the static files like css and js then it will link to the generated html
 app.use(express.static("public"));
+// use this middleware encode the url that may contain extra data like parameters
 app.use(express.urlencoded({ extended: true }));
+// use middleware to use json encode and decode inside the server
 app.use(express.json());
 
+// routes definitions:
+// the root route that will display the main page of the app
+// it has the get method represent the R of Read in CRUD
+// the route is '/' and has the async function take request and response
 app.get("/", async (request, response) => {
+  // hold the data received from the todos collection on the db after converted to array in todoItems variable (await used because they will take time to come back from the db)
   const todoItems = await db.collection("todos").find().toArray();
+  // hold the number of all documents on todos collection that has a property {completed" false} so show all uncompleted todos (await used because they will take time to come back from the db)
   const itemsLeft = await db
     .collection("todos")
     .countDocuments({ completed: false });
-  response.render("index.ejs", { items: todoItems, left: itemsLeft });
+    // render the index.ejs file as a response from the server when the root called, injecting data in an object containing the todos list and number of incompleted todos
+    response.render("index.ejs", { items: todoItems, left: itemsLeft });
+  // the equivalent method but with the '.then' method instead of 'await'
   // db.collection('todos').find().toArray()
   // .then(data => {
   //     db.collection('todos').countDocuments({completed: false})
@@ -48,69 +59,118 @@ app.get("/", async (request, response) => {
   // .catch(error => console.error(error))
 });
 
+// the addTodo route that will handle todo add to the database
+// it has the post method represent the C of Creat in CRUD
+// the route is '/addTodo' and has the function take request and response
 app.post("/addTodo", (request, response) => {
+  // choose the collection in the database 'todos'
   db.collection("todos")
+    // use insertOne method to add one document to the db with the iformation of:
+    // thing: the data came from the frontend form that can catched here with the request.body.todoItem
+    // completed: give it a default value 'false' as it incomplete
     .insertOne({ thing: request.body.todoItem, completed: false })
+    // use .then method to wait for response come from the db
     .then((result) => {
+      // inform the user that the todo added successfully
       console.log("Todo Added");
+      // refresh the page so we go to the root rout '/' to refresh the data and show the recently added todo in the list
       response.redirect("/");
     })
+    // if there is an error witht above the following catch it and console the error
     .catch((error) => console.error(error));
 });
 
+// the markComplete route that will handle mark the todo as completed in the database
+// it has the put method represent the U of Update in CRUD
+// the route is '/markComplete' and has the function take request and response
 app.put("/markComplete", (request, response) => {
-  console.log("*****", request.body.itemFromJS, "*****");
+  // choose the collection in the database 'todos'
   db.collection("todos")
+    // use updateOne method to update one document in the db with the iformation of:
+    // thing: the data came from the frontend click on specific todo text that can catched here with the request.body.todoItem then search the db by its text
     .updateOne(
       { thing: request.body.itemFromJS },
+      // after find the document update the completed property by making it true use the $set method came form the db of mongodb
       {
         $set: {
           completed: true,
         },
       },
+      // sort the result by _id decendently
+      // upsert set to false so if it dosnt find the document not create new one
       {
         sort: { _id: -1 },
         upsert: false,
       }
     )
+    // use .then method to wait for response come from the db
     .then((result) => {
+      // inform the user in the console that the updated done
       console.log("Marked Complete");
+      // send json data "Marked Complete" as a response to the request from the fronend
       response.json("Marked Complete");
     })
+    // if there is an error witht above the following catch it and console the error
     .catch((error) => console.error(error));
 });
 
+// the markUnComplete route that will handle mark the todo as uncompleted in the database
+// it has the put method represent the U of Update in CRUD
+// the route is '/markUnComplete' and has the function take request and response
 app.put("/markUnComplete", (request, response) => {
+  // choose the collection in the database 'todos'
   db.collection("todos")
+    // use updateOne method to update one document in the db with the iformation of:
+    // thing: the data came from the frontend click on specific todo text that can catched here with the request.body.todoItem then search the db by its text
     .updateOne(
       { thing: request.body.itemFromJS },
+      // after find the document update the completed property by making it false use the $set method came form the db of mongodb
       {
         $set: {
           completed: false,
         },
       },
+      // sort the result by _id decendently
+      // upsert set to false so if it dosnt find the document not create new one
       {
         sort: { _id: -1 },
         upsert: false,
       }
     )
+    // use .then method to wait for response come from the db should be "Marked Un Complete"
     .then((result) => {
+      // inform the user in the console that the updated done
       console.log("Marked Complete");
+      // send json data "Marked Complete" should be "Marked Un Complete" as a response to the request from the fronend
       response.json("Marked Complete");
     })
+    // if there is an error witht above the following catch it and console the error
     .catch((error) => console.error(error));
 });
 
+
+// the deleteItem route that will handle delete selected todo from the database
+// it has the delete method represent the D of Delete in CRUD
+// the route is '/deleteItem' and has the function take request and response
 app.delete("/deleteItem", (request, response) => {
+  // choose the collection in the database 'todos'
   db.collection("todos")
+    // use deleteOne method to delete one document in the db with the iformation of:
+    // thing: the data came from the frontend click on specific todo text that can catched here with the request.body.todoItem then search the db by its text
     .deleteOne({ thing: request.body.itemFromJS })
+    // use .then method to wait for response come from the db should be "Marked Un Complete"
     .then((result) => {
+      // inform the user in the console that the delete done
       console.log("Todo Deleted");
+      // send json data "Todo Deleted" as a response to the request from the fronend
       response.json("Todo Deleted");
     })
+    // if there is an error witht above the following catch it and console the error
     .catch((error) => console.error(error));
 });
 
+// the main function that will fire the server to run and listening in the predefined port number either as the variable or from the .env file 2121
 app.listen(process.env.PORT || PORT, () => {
+  // anouncement that it is run and ready to listen to the requests
   console.log(`Server running on port ${PORT}`);
 });
