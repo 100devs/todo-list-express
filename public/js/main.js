9546f3861a757a96496a0e7d981290a978ef6c36
AwsYaseen22 <aws.yaseen.22@gmail.com>
2022-07-30 16:57:18 +0300

    comment the main.js file

--- public/js/main.js
+++ public/js/main.js
@@ -34,57 +34,96 @@ async function deleteItem() {
   // 'childNodes[1]': [text, *span*, text, text,span.fa.fa-trash, text]
   // 'innerText': the text of the above selected span
   const itemText = this.parentNode.childNodes[1].innerText;
+  // use try catch mechanism that will catch any errors if code in try not work correctly and generate error
   try {
+    // save the response from calling the deleteItem route in the server in response variable
     const response = await fetch("deleteItem", {
+      // the method of http protocol 
       method: "delete",
+      // header of the request tells the server that the expected content is in json format
       headers: { "Content-Type": "application/json" },
+      // encode the body content (the data passed to the server with the request method) to json format
       body: JSON.stringify({
+        // the data will be passed it the itemText variable which it is the text of the todo item and will be accessed in the server as: request.body.itemFromJS
         itemFromJS: itemText,
       }),
     });
+    // use await as it may take time to solve the fetch method and convert the data received from the server as the response to json format that we could use it in the frontend
     const data = await response.json();
+    // show the data variable in the console
     console.log(data);
+    // refresh the page so it will update the deletion action and we actually send another request to the server with the route '/' which will recalculate the data received from the database then update the screen with new data the will not include the deleted item
     location.reload();
-  } catch (err) {
+  } catch (err) { // catch part that will be affected if there is an error inside the try block
+    // show the catched error in the console
     console.log(err);
   }
 }
 
 // definition of markComplete function that will used for make a clicked item complete if it is incomplete and make it async as it will deals with the server that will take time to fulfilled
 async function markComplete() {
-  console.log(this.parentNode.childNodes[1]);
+  // store 'this' which hold the clicked item's parent's first child's text in itemText variable
+  // 'this': span.fa.fa-trash
+  // 'parentNode' : li.item
+  // 'childNodes[1]': [text, *span*, text, text,span.fa.fa-trash, text]
+  // 'innerText': the text of the above selected span
   const itemText = this.parentNode.childNodes[1].innerText;
+  // use try catch mechanism that will catch any errors if code in try not work correctly and generate error
   try {
+    // save the response from calling the markComplete route in the server in response variable
     const response = await fetch("markComplete", {
+      // the method of http protocol 
       method: "put",
+      // header of the request tells the server that the expected content is in json format
       headers: { "Content-Type": "application/json" },
+      // encode the body content (the data passed to the server with the request method) to json format
       body: JSON.stringify({
+        // the data will be passed it the itemText variable which it is the text of the todo item and will be accessed in the server as: request.body.itemFromJS
         itemFromJS: itemText,
       }),
     });
+    // use await as it may take time to solve the fetch method and convert the data received from the server as the response to json format that we could use it in the frontend
     const data = await response.json();
+    // show the data variable in the console
     console.log(data);
+    // refresh the page so it will update the deletion action and we actually send another request to the server with the route '/' which will recalculate the data received from the database then update the screen with new data the will not include the deleted item
     location.reload();
-  } catch (err) {
+  } catch (err) { // catch part that will be affected if there is an error inside the try block
+    // show the catched error in the console
     console.log(err);
   }
 }
 
 // definition of markUnComplete function that will used for make a clicked item incomplete if it is complete and make it async as it will deals with the server that will take time to fulfilled
 async function markUnComplete() {
+  // store 'this' which hold the clicked item's parent's first child's text in itemText variable
+  // 'this': span.fa.fa-trash
+  // 'parentNode' : li.item
+  // 'childNodes[1]': [text, *span*, text, text,span.fa.fa-trash, text]
+  // 'innerText': the text of the above selected span
   const itemText = this.parentNode.childNodes[1].innerText;
+  // use try catch mechanism that will catch any errors if code in try not work correctly and generate error
   try {
+    // save the response from calling the markUnComplete route in the server in response variable
     const response = await fetch("markUnComplete", {
+      // the method of http protocol 
       method: "put",
+      // header of the request tells the server that the expected content is in json format
       headers: { "Content-Type": "application/json" },
+      // encode the body content (the data passed to the server with the request method) to json format
       body: JSON.stringify({
+        // the data will be passed it the itemText variable which it is the text of the todo item and will be accessed in the server as: request.body.itemFromJS
         itemFromJS: itemText,
       }),
     });
+    // use await as it may take time to solve the fetch method and convert the data received from the server as the response to json format that we could use it in the frontend
     const data = await response.json();
+    // show the data variable in the console
     console.log(data);
+    // refresh the page so it will update the deletion action and we actually send another request to the server with the route '/' which will recalculate the data received from the database then update the screen with new data the will not include the deleted item
     location.reload();
-  } catch (err) {
+  } catch (err) { // catch part that will be affected if there is an error inside the try block
+    // show the catched error in the console
     console.log(err);
   }
 }
