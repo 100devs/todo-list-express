95220db881bfece02a67b2b6e35c0e52a92585f7
Edward Way <emtek995@gmail.com>
2022-07-31 15:39:43 -0500

    Code comment homework

--- public/js/main.js
+++ public/js/main.js
@@ -1,72 +1,72 @@
-const deleteBtn = document.querySelectorAll('.fa-trash')
-const item = document.querySelectorAll('.item span')
-const itemCompleted = document.querySelectorAll('.item span.completed')
+const deleteBtn = document.querySelectorAll('.fa-trash') // grab all of the delete buttons from the DOM
+const item = document.querySelectorAll('.item span') // grab all of the todo items from the DOM list
+const itemCompleted = document.querySelectorAll('.item span.completed') // grab all of the completed todo items from the DOM list
 
-Array.from(deleteBtn).forEach((element)=>{
-    element.addEventListener('click', deleteItem)
+Array.from(deleteBtn).forEach((element)=>{ // add event listener to every delete button in the DOM
+    element.addEventListener('click', deleteItem) // on click, call deleteItem(). 
 })
 
-Array.from(item).forEach((element)=>{
-    element.addEventListener('click', markComplete)
+Array.from(item).forEach((element)=>{ // add mark complete event listener to every 'item' class span element.  
+    element.addEventListener('click', markComplete) // on click, call markComplete(). 
 })
 
-Array.from(itemCompleted).forEach((element)=>{
-    element.addEventListener('click', markUnComplete)
+Array.from(itemCompleted).forEach((element)=>{ // add mark uncomplete event listener to every 'itemCompleted' class span element. 
+    element.addEventListener('click', markUnComplete) // on click, call markUnComplete().
 })
 
-async function deleteItem(){
-    const itemText = this.parentNode.childNodes[1].innerText
-    try{
-        const response = await fetch('deleteItem', {
-            method: 'delete',
-            headers: {'Content-Type': 'application/json'},
-            body: JSON.stringify({
-              'itemFromJS': itemText
-            })
-          })
-        const data = await response.json()
-        console.log(data)
-        location.reload()
+async function deleteItem(){ // function to delete an item
+    const itemText = this.parentNode.childNodes[1].innerText // grab descriptor text of todoitem
+    try{ // use try because fetch might fail
+        const response = await fetch('deleteItem', { // send request to delete item
+            method: 'delete', // use the delete method
+            headers: {'Content-Type': 'application/json'}, // tell server we are sending JSON
+            body: JSON.stringify({   // \
+              'itemFromJS': itemText //  } convert the item text to a JSON object
+            })                       // /
+          }) // closing brackets for the delete request
+        const data = await response.json() // parse the servers response as json
+        console.log(data) // log the parsed data to the console
+        location.reload() // reload the page
 
-    }catch(err){
-        console.log(err)
+    } catch(err){ //catch any errors...
+        console.log(err) // and log them to the console
     }
 }
 
-async function markComplete(){
-    const itemText = this.parentNode.childNodes[1].innerText
-    try{
-        const response = await fetch('markComplete', {
-            method: 'put',
-            headers: {'Content-Type': 'application/json'},
-            body: JSON.stringify({
-                'itemFromJS': itemText
-            })
-          })
-        const data = await response.json()
-        console.log(data)
-        location.reload()
+async function markComplete(){ // async function...
+    const itemText = this.parentNode.childNodes[1].innerText // grab descriptor text of todoitem
+    try{ // use try because fetch might fail
+        const response = await fetch('markComplete', { // send request to put item
+            method: 'put', // use the put method
+            headers: {'Content-Type': 'application/json'}, // tell server we are sending JSON
+            body: JSON.stringify({   // \
+              'itemFromJS': itemText //  } convert the item text to a JSON object
+            })                       // /
+          }) // closing brackets for the fetch request
+        const data = await response.json() // parse the servers response as json
+        console.log(data) // log the parsed data to the console
+        location.reload() // reload the page
 
-    }catch(err){
-        console.log(err)
+    }catch(err){ //catch any errors...
+        console.log(err) // and log them to the console
     }
 }
 
-async function markUnComplete(){
-    const itemText = this.parentNode.childNodes[1].innerText
-    try{
-        const response = await fetch('markUnComplete', {
-            method: 'put',
-            headers: {'Content-Type': 'application/json'},
-            body: JSON.stringify({
-                'itemFromJS': itemText
-            })
+async function markUnComplete(){ // async function...
+    const itemText = this.parentNode.childNodes[1].innerText // grab descriptor text of todoitem
+    try{ // use try because fetch might fail
+        const response = await fetch('markUnComplete', { // send request to put item
+            method: 'put', // use the put method
+            headers: {'Content-Type': 'application/json'}, // tell server we are sending JSON
+            body: JSON.stringify({   // \
+              'itemFromJS': itemText //  } convert the item text to a JSON object
+            })                       // /
           })
-        const data = await response.json()
-        console.log(data)
-        location.reload()
+        const data = await response.json() // parse the servers response as json
+        console.log(data) // log the parsed data to the console
+        location.reload() // reload the page
 
-    }catch(err){
-        console.log(err)
+    }catch(err){ //catch any errors...
+        console.log(err) // and log them to the console
     }
 }
