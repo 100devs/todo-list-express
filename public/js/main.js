99ceee70eb40298aa68cc1a235d54a0b3e137b12
Sage <hwilliams6047@gmail.com>
2022-08-01 17:17:50 -0400

    Added comments throughout code

--- public/js/main.js
+++ public/js/main.js
@@ -1,72 +1,95 @@
-const deleteBtn = document.querySelectorAll('.fa-trash')
-const item = document.querySelectorAll('.item span')
-const itemCompleted = document.querySelectorAll('.item span.completed')
+//Select and store groups of items from the DOM
+const deleteBtn = document.querySelectorAll('.fa-trash') //All the delete buttons (trash icon)
+const item = document.querySelectorAll('.item span') //All spans with class="item"
+const itemCompleted = document.querySelectorAll('.item span.completed') //All spans with both 'item' and 'completed' classes
 
+//Add an event listener to each delete button 
 Array.from(deleteBtn).forEach((element)=>{
+    //When clicked, call 'deleteItem'
     element.addEventListener('click', deleteItem)
 })
 
+//Add an event listener to each list item. 
 Array.from(item).forEach((element)=>{
+    //When clicked, call 'markComplete'
     element.addEventListener('click', markComplete)
 })
 
+//Add an event listener to each item in itemCompleted
 Array.from(itemCompleted).forEach((element)=>{
+    //When clicked, call 'markUnComplete'
     element.addEventListener('click', markUnComplete)
 })
 
+//Async function to delete todo items
 async function deleteItem(){
-    const itemText = this.parentNode.childNodes[1].innerText
+    // Select the parent node of this (this is the trash icon within the todo list item), then get the inner text of the second child of it (should be text content of list item)
+    //Had to change to childNodes[5] from [1] due to added comments!
+    const itemText = this.parentNode.childNodes[5].innerText
+    console.log(itemText)
+    //Async try this
     try{
+        //Send fetch request to '/deleteItem'
         const response = await fetch('deleteItem', {
-            method: 'delete',
-            headers: {'Content-Type': 'application/json'},
-            body: JSON.stringify({
-              'itemFromJS': itemText
+            method: 'delete', /* Tell server to the delete method */
+            headers: {'Content-Type': 'application/json'}, /* Tell server to expect JSON content */
+            body: JSON.stringify({ 
+              'itemFromJS': itemText /* Convert itemText to JSON */
             })
           })
-        const data = await response.json()
-        console.log(data)
-        location.reload()
+        const data = await response.json() /* Wait for the server's response */
+        console.log(data) /* Output response to the console */
+        location.reload() /* Reload window */
 
     }catch(err){
+        // Catch any error and display on the console.
         console.log(err)
     }
 }
 
+//Async function for marking items as complete
 async function markComplete(){
-    const itemText = this.parentNode.childNodes[1].innerText
+    // Get identifying info  **look into parentNode/childNodes**
+    const itemText = this.parentNode.childNodes[5].innerText
+
+    //Try to contact the server
     try{
+        //Send fetch request to '/markComplete'
         const response = await fetch('markComplete', {
-            method: 'put',
-            headers: {'Content-Type': 'application/json'},
+            method: 'put', /* Tell server to use 'put' method */
+            headers: {'Content-Type': 'application/json'}, /* Tell server to expect JSON content */
             body: JSON.stringify({
-                'itemFromJS': itemText
+                'itemFromJS': itemText /* Convert itemText to JSON */
             })
           })
-        const data = await response.json()
-        console.log(data)
-        location.reload()
+        const data = await response.json() /* Wait for the server's response */
+        console.log(data) /* Output response to the console */
+        location.reload() /* Reload window */
 
     }catch(err){
-        console.log(err)
+        console.log(err) // Catch any error and display on the console.
     }
 }
 
+//Async function for marking items un-complete
 async function markUnComplete(){
-    const itemText = this.parentNode.childNodes[1].innerText
+    // Get identifying info  **look into parentNode/childNodes**
+    const itemText = this.parentNode.childNodes[5].innerText
+    console.log(itemText)
     try{
+        //Send fetch request to '/markUnComplete'
         const response = await fetch('markUnComplete', {
-            method: 'put',
-            headers: {'Content-Type': 'application/json'},
+            method: 'put',  /* Tell server to use 'put' method */
+            headers: {'Content-Type': 'application/json'}, /* Tell server to expect JSON content */
             body: JSON.stringify({
-                'itemFromJS': itemText
+                'itemFromJS': itemText /* Convert itemText to JSON */
             })
           })
-        const data = await response.json()
-        console.log(data)
-        location.reload()
+        const data = await response.json() /* Wait for the server's response */
+        console.log(data) /* Output response to the console */
+        location.reload() /* Reload window */
 
     }catch(err){
-        console.log(err)
+        console.log(err) // Catch any error and display on the console.
     }
 }
