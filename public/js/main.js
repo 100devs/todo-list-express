5e737aa02a30971ab27868e347ad3b9b12c25886
thomas <thomaalanstevens@gmail.com>
2022-08-02 12:58:32 -0700

    finish commenting main.js

--- public/js/main.js
+++ public/js/main.js
@@ -31,42 +31,42 @@ async function deleteItem(){ // async function declared and named deleteItem, no
     }catch(err){ // start catch block to catch errors if fetch request goes wrong
         console.log(err) //  console log the error
     } // end catch block
-}
+} // close function
 
-async function markComplete(){
-    const itemText = this.parentNode.childNodes[1].innerText
-    try{
-        const response = await fetch('markComplete', {
-            method: 'put',
-            headers: {'Content-Type': 'application/json'},
-            body: JSON.stringify({
-                'itemFromJS': itemText
-            })
-          })
-        const data = await response.json()
-        console.log(data)
-        location.reload()
+async function markComplete(){ // async function declared and named markComplete, no params used.
+    const itemText = this.parentNode.childNodes[1].innerText // reach into tag the event listener is attached to, go up to its parent than down to its child note that contains the inner text of the tag and places the inner text into itemText
+    try{ // Start try block
+        const response = await fetch('markComplete', { // create and store in response the data returned from the fetch request sent to the path markComplete
+            method: 'put',  //declare the method of the fetch request as put
+            headers: {'Content-Type': 'application/json'}, //declare the type of data being sent in the fetch request
+            body: JSON.stringify({ //place into the body of the request a stringified json object 
+                'itemFromJS': itemText // name the key itemFromJs and value as itemText from variable itemText for the object
+            }) // end obj
+          }) // end fetch request
+        const data = await response.json() // declare and store into data variable the json response from fetch
+        console.log(data) //console.log data
+        location.reload() // reload page
 
-    }catch(err){
-        console.log(err)
-    }
-}
+    }catch(err){ // declare catch block
+        console.log(err) //log error
+    }// close catch block
+}// close function
 
-async function markUnComplete(){
-    const itemText = this.parentNode.childNodes[1].innerText
-    try{
-        const response = await fetch('markUnComplete', {
-            method: 'put',
-            headers: {'Content-Type': 'application/json'},
-            body: JSON.stringify({
-                'itemFromJS': itemText
-            })
-          })
-        const data = await response.json()
-        console.log(data)
-        location.reload()
+async function markUnComplete(){ // declare async function
+    const itemText = this.parentNode.childNodes[1].innerText // declare and store in var ItemText info from the tag then move up to the tags parent than down to the tags child[1] and take the inner text
+    try{ // declare try black
+        const response = await fetch('markUnComplete', { // declare and store data from fetch request to path markUnComplete
+            method: 'put', // declare method type as put
+            headers: {'Content-Type': 'application/json'}, // declare type of data being sent in fetch request
+            body: JSON.stringify({ // send in the body of the fetch request a JSON.stringified object
+                'itemFromJS': itemText // place into the object a key value pair
+            }) // end body object
+          }) // end fetch request
+        const data = await response.json() // declare and store in the data var the fetch json response
+        console.log(data) // log data
+        location.reload() // reload page
 
-    }catch(err){
-        console.log(err)
-    }
-}
+    }catch(err){ // start catch
+        console.log(err) // log error
+    } // end catch
+} // end function
