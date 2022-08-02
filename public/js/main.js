24a56799b039f3d5819846ef43c4e1b8f1651654
moomoonnow <changtime247@gmail.com>
2022-08-02 16:11:40 -0400

    Add line by line comments

--- public/js/main.js
+++ public/js/main.js
@@ -1,72 +1,87 @@
-const deleteBtn = document.querySelectorAll('.fa-trash')
-const item = document.querySelectorAll('.item span')
-const itemCompleted = document.querySelectorAll('.item span.completed')
+// Storing all elements in DOM with class specified in a nodelist specified as variables declared with keyword 'const'
+const deleteBtn = document.querySelectorAll('.fa-trash')                // (all elements with class of'fa-trash')
+const item = document.querySelectorAll('.item span')                    // (within elements with class of 'item', all span elements)
+const itemCompleted = document.querySelectorAll('.item span.completed') // (within elements with class of 'item', all span elements with class of 'completed')
 
-Array.from(deleteBtn).forEach((element)=>{
-    element.addEventListener('click', deleteItem)
+// Creating an array from the nodelist to add an event listener to each node using forEach method. 
+// When a specific node is clicked, the corresponding callback function is called.
+Array.from(deleteBtn).forEach((element) => {
+  element.addEventListener('click', deleteItem)
 })
 
-Array.from(item).forEach((element)=>{
-    element.addEventListener('click', markComplete)
+Array.from(item).forEach((element) => {
+  element.addEventListener('click', markComplete)
 })
 
-Array.from(itemCompleted).forEach((element)=>{
-    element.addEventListener('click', markUnComplete)
+Array.from(itemCompleted).forEach((element) => {
+  element.addEventListener('click', markUnComplete)
 })
 
-async function deleteItem(){
-    const itemText = this.parentNode.childNodes[1].innerText
-    try{
-        const response = await fetch('deleteItem', {
-            method: 'delete',
-            headers: {'Content-Type': 'application/json'},
-            body: JSON.stringify({
-              'itemFromJS': itemText
-            })
-          })
-        const data = await response.json()
-        console.log(data)
-        location.reload()
-
-    }catch(err){
-        console.log(err)
-    }
+// The Following 3 asynchronous functions are the callback functions within the event listenters.
+// Declare an async function to delete the specified todo item associated with the trash can icon
+async function deleteItem() {
+  // the 'this' keyword references the specific node that was clicked on. 
+  // that node's parent node is the list item element that also has a child, text, that can be displayed. 
+  // trim the text and save to const itemText
+  const itemText = this.parentNode.children[0].innerText.trim()
+  try { // try catch block to try to delete the specified node (if error, console log the error)
+    // send a delete req to the server w the following parameters
+    const response = await fetch('deleteItem', { // create a response variable that waits on a fetch (global method) to get data from the result of the deleteItem route
+      method: 'delete', // sets CRUD method to delete
+      headers: { 'Content-Type': 'application/json' }, // specifying the headers that the content-type expected is JSON
+      body: JSON.stringify({ // body property holds the text. turn that text into JSON and set to value for body property
+        itemFromJS: itemText, // property 'itemFromJS' with value set to the text of the list item
+      }), // close fetch's second parameter
+    }) // close response object
+    const data = await response.json() // waiting on the JSON object in the response to be converted back to JavaScript object
+    console.log(data) // log returned object
+    location.reload() // reload page to show an updated page (post-delete action)
+  } catch (err) {
+    console.log(err) // log the error object to console
+  }
 }
-
-async function markComplete(){
-    const itemText = this.parentNode.childNodes[1].innerText
-    try{
-        const response = await fetch('markComplete', {
-            method: 'put',
-            headers: {'Content-Type': 'application/json'},
-            body: JSON.stringify({
-                'itemFromJS': itemText
-            })
-          })
-        const data = await response.json()
-        console.log(data)
-        location.reload()
-
-    }catch(err){
-        console.log(err)
-    }
-}
-
-async function markUnComplete(){
-    const itemText = this.parentNode.childNodes[1].innerText
-    try{
-        const response = await fetch('markUnComplete', {
-            method: 'put',
-            headers: {'Content-Type': 'application/json'},
-            body: JSON.stringify({
-                'itemFromJS': itemText
-            })
-          })
-        const data = await response.json()
-        console.log(data)
-        location.reload()
-
-    }catch(err){
-        console.log(err)
-    }
-}
+// Declare an async function to mark the specified todo item as complete
+async function markComplete() {
+  // the 'this' keyword references the specific node that was clicked on.
+  // that node's parent node is the list item element that also has a child, text, that can be displayed.
+  // trim the text and save to const itemText
+  const itemText = this.parentNode.children[0].innerText.trim()
+  // sending a put request to the server
+  try { // try catch block to try to update the specified node (if error, console log the error)
+    // send a put req to the server w the following parameters
+    const response = await fetch('markComplete', { // create a response variable that waits on a fetch (global method) to get data from the result of the markComplete route
+      method: 'put', // sets CRUD method to update
+      headers: { 'Content-Type': 'application/json' }, // specifying the headers that the content-type expected is JSON
+      body: JSON.stringify({ // body property holds the text. turn that text into JSON and set to value for body property
+        itemFromJS: itemText // property 'itemFromJS' with value set to the text of the list item
+        }) // close fetch's second parameter
+      }) // close response object
+    const data = await response.json() // waiting on the JSON object in the response to be converted back to JavaScript object
+    console.log(data) // log returned object
+    location.reload() // reload page to show an updated page (post-update action)
+  } catch (err) {
+    console.log(err) // log the error object to console
+  } // close try catch block
+} // close function declaration
+// Declare an async function to mark the specified todo item as 'un'-complete
+async function markUnComplete() {
+  // the 'this' keyword references the specific node that was clicked on.
+  // that node's parent node is the list item element that also has a child, text, that can be displayed.
+  // trim the text and save to const itemText
+  const itemText = this.parentNode.children[0].innerText.trim()
+  // sending a put request to the server
+  try { // try catch block to try to update the specified node (if error, console log the error)
+    const response = await fetch('markUnComplete', { // create a response variable that waits on a fetch (global method) to get data from the result of the markComplete route 
+      method: 'put', // sets CRUD method to update
+      headers: { 'Content-Type': 'application/json' }, // specifying the headers that the content-type expected is JSON
+      body: JSON.stringify({ // body property holds the text. turn that text into JSON and set to value for body property
+        itemFromJS: itemText, // property 'itemFromJS' with value set to the text of the list item
+      }), // close fetch's second parameter
+    }) // close response object
+    const data = await response.json() // waiting on the JSON object in the response to be converted back to JavaScript object
+    console.log(data) // log returned object
+    location.reload() // reload page to show an updated page (post-update action)
+  } catch (err) {
+    console.log(err) // log the error object to console
+  } // close try catch block
+} // close function declaration
--- server.js
+++ server.js
@@ -1,93 +1,109 @@
-const express = require('express')
-const app = express()
-const MongoClient = require('mongodb').MongoClient
-const PORT = 2121
-require('dotenv').config()
+const express = require('express')                  // require (or import) express
+const app = express()                               // invoke express
+const MongoClient = require('mongodb').MongoClient  // require mongodb's mongoClient allowing interaction with mongodb
+const PORT = 2121                                   // port variable (location where we will be listening) set to a constant
+require('dotenv').config()                          // require dotenv, allowing access to the .env file
 
+let db,                                             // declare global variable called db allowing it to be accessible globally
+    dbConnectionStr = process.env.DB_STRING,        // declare global variable for the connection string in .env file
+    dbName = 'todo'                                 // declare global variable and assigning name of database
 
-let db,
-    dbConnectionStr = process.env.DB_STRING,
-    dbName = 'todo'
-
-MongoClient.connect(dbConnectionStr, { useUnifiedTopology: true })
-    .then(client => {
-        console.log(`Connected to ${dbName} Database`)
-        db = client.db(dbName)
-    })
-    
-app.set('view engine', 'ejs')
-app.use(express.static('public'))
-app.use(express.urlencoded({ extended: true }))
-app.use(express.json())
-
-
-app.get('/',async (request, response)=>{
-    const todoItems = await db.collection('todos').find().toArray()
-    const itemsLeft = await db.collection('todos').countDocuments({completed: false})
-    response.render('index.ejs', { items: todoItems, left: itemsLeft })
-    // db.collection('todos').find().toArray()
-    // .then(data => {
-    //     db.collection('todos').countDocuments({completed: false})
-    //     .then(itemsLeft => {
-    //         response.render('index.ejs', { items: data, left: itemsLeft })
-    //     })
-    // })
-    // .catch(error => console.error(error))
+MongoClient.connect(dbConnectionStr, { useUnifiedTopology: true })  // connect to the database, passing in connection string with an additional property
+  .then((client) => {                                               // if connection is successful,
+    console.log(`Connected to ${dbName} Database`)                  // CL string with db name
+    db = client.db(dbName)                                          // assign value to global db variable which is the db client factory method
 })
 
+// Middleware
+app.set('view engine', 'ejs')                        // set default render method to ejs
+app.use(express.static('public'))                    // static assets folder (contents are automagically accessible)
+app.use(express.urlencoded({ extended: true }))      // automatically encode and decode URLs
+app.use(express.json())                              // parses JSON from incoming requests
+
+// Asynchronous function
+
+app.get('/', async (request, response)=>{                             // responding to the get request to the root route
+  const todoItems = await db.collection('todos').find().toArray()     // searching collection to find all documents from the todos collection
+  const itemsLeft = await db                                          // creating a variable itemsLeft with a value set to the count of how many todos has its 'completed' property set to false
+    .collection('todos')
+    .countDocuments({ completed: false })
+  response.render('index.ejs', { items: todoItems, left: itemsLeft }) // sending over the variables todoitems and itemsleft to EJS
+  // db.collection('todos').find().toArray()                          // extra code to show how to set up same code using Promises and the 'then' keyword
+  // .then(data => {
+  //     db.collection('todos').countDocuments({completed: false})
+  //     .then(itemsLeft => {
+  //         response.render('index.ejs', { items: data, left: itemsLeft })
+  //     })
+  // })
+  // .catch(error => console.error(error))
+})
+
+// responding to the post request to route '/addTodo' (specified in the form element, in index.ejs)
 app.post('/addTodo', (request, response) => {
+    // inserting a new todo item to the list
+    // The request.body.todoItem is found from the input element with name attribute in index.ejs
+    // completed is false (otherwise, there will be a strikethrough)
     db.collection('todos').insertOne({thing: request.body.todoItem, completed: false})
+    // if successful, proceed
     .then(result => {
-        console.log('Todo Added')
-        response.redirect('/')
+        console.log('Todo Added')           // CL success message
+        response.redirect('/')              // redirect client to root page (if this is missing, there will be an unresolved promise error)
     })
-    .catch(error => console.error(error))
+    .catch(error => console.error(error))   // if not successful, catch the error
 })
 
+// responding to put requests to route '/markComplete'
 app.put('/markComplete', (request, response) => {
+    // go to db. Inside the todos collection find an object with thing property with value that matches of that as found in req.body.itemFromJS
+    // update that object if found
     db.collection('todos').updateOne({thing: request.body.itemFromJS},{
         $set: {
-            completed: true
+            completed: true // set the found object's completed property to true
           }
     },{
-        sort: {_id: -1},
-        upsert: false
+        sort: {_id: -1},    // give me highest ids first (i.e. newest ones first)
+        upsert: false       // if not found, do not insert
     })
     .then(result => {
-        console.log('Marked Complete')
-        response.json('Marked Complete')
+        console.log('Marked Complete')      // CL success message 
+        response.json('Marked Complete')    // respond back to the client in JSON with success message
     })
-    .catch(error => console.error(error))
+    .catch(error => console.error(error))   // if not successful, catch the error
 
 })
 
+// responding to update request to the route '/markUnComplete'
 app.put('/markUnComplete', (request, response) => {
+    // go to db and inside 'todos' collection, update the specified object as follows:
     db.collection('todos').updateOne({thing: request.body.itemFromJS},{
         $set: {
-            completed: false
+            completed: false// set the found object's completed property to false
           }
     },{
-        sort: {_id: -1},
-        upsert: false
+        sort: {_id: -1},    // give me highest ids first (i.e. newest ones first)
+        upsert: false       // if not found, do not insert
     })
     .then(result => {
-        console.log('Marked Complete')
-        response.json('Marked Complete')
+        console.log('Marked Complete')      // CL success message 
+        response.json('Marked Complete')    // respond back to the client in JSON with success message
     })
-    .catch(error => console.error(error))
+    .catch(error => console.error(error))   // if not successful, catch the error
 
 })
 
+// responding to delete request to the route '/deleteItem'
 app.delete('/deleteItem', (request, response) => {
-    db.collection('todos').deleteOne({thing: request.body.itemFromJS})
-    .then(result => {
-        console.log('Todo Deleted')
-        response.json('Todo Deleted')
+  db.collection('todos')                    // go to db and inside 'todos' collection, delete specified object
+    .then((result) => {
+      console.log('Marked Complete')        // CL success message
+      response.json('Marked Complete')      // respond back to the client in JSON with success message
     })
-    .catch(error => console.error(error))
-
+    .catch((error) => console.error(error)) // if not successful, catch the error
 })
 
+// run your server
+// dotenv file has the PORT (if not there, just go with PORT as defined in server.js)
+// simple CL to let us know that server is running
 app.listen(process.env.PORT || PORT, ()=>{
     console.log(`Server running on port ${PORT}`)
 })
