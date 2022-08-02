52b94a8cf7d1f94282d6c60a699c8588ed6dfc60
David Provenzano <dprovenzano13@gmail.com>
2022-08-02 15:48:38 -0400

    Add more comments

--- server.js
+++ server.js
@@ -1,28 +1,30 @@
-const express = require('express')
-const app = express()
-const MongoClient = require('mongodb').MongoClient
-const PORT = 2121
-require('dotenv').config()
+const express = require('express') // making it possible to use express in this file
+const app = express() // create a variable and assigning it to the instance of express
+const MongoClient = require('mongodb').MongoClient // makes it possible to use methods associated with MongoClient and talk to our DB
+const PORT = 2121 // setting a variable that sets up the port so we can run our page locally
+require('dotenv').config() // letting us access what we've put in our .env file
 
 
-let db,
-    dbConnectionStr = process.env.DB_STRING,
-    dbName = 'todo'
+let db, // declare a variable called db but no assign a value
+    dbConnectionStr = process.env.DB_STRING, // declare a variable and assign our database connection string to it
+    dbName = 'todo' // declaring a variable and assigning the name of the database we will be using
 
-MongoClient.connect(dbConnectionStr, { useUnifiedTopology: true })
-    .then(client => {
-        console.log(`Connected to ${dbName} Database`)
-        db = client.db(dbName)
-    })
+MongoClient.connect(dbConnectionStr, { useUnifiedTopology: true }) //Creating a connection to MongoDB and passing in our connection string. Also passing in an additional property
+    .then(client => { //Waiting for the connection and proceeding if successful, and passing in all the client information
+        console.log(`Connected to ${dbName} Database`) //log to the console a template literal stating "connected to todo database"
+        db = client.db(dbName) //assigning previously declared db variable that contains db client factory method
+    }) //close .then
     
-app.set('view engine', 'ejs')
-app.use(express.static('public'))
-app.use(express.urlencoded({ extended: true }))
-app.use(express.json())
+
+//middleware
+app.set('view engine', 'ejs') //sets ejs as the default render
+app.use(express.static('public')) //sets the location for static assets
+app.use(express.urlencoded({ extended: true })) //Tells express to decode and encode URLs where the header matches the content. Supports arrays and objects
+app.use(express.json()) //Parses JSON content
 
 
-app.get('/',async (request, response)=>{
-    const todoItems = await db.collection('todos').find().toArray()
+app.get('/',async (request, response)=>{ //starts a GET method when the root route is passed in, sets up and req and res parameters
+    const todoItems = await db.collection('todos').find().toArray() //sets a variable and awaits
     const itemsLeft = await db.collection('todos').countDocuments({completed: false})
     response.render('index.ejs', { items: todoItems, left: itemsLeft })
     // db.collection('todos').find().toArray()
