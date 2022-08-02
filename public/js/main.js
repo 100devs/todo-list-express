04fa7a6c107c435ad06f9d487d66d977ea55ac70
Justin KA <justinalexander101@gmail.com>
2022-08-02 19:23:46 -0400

    additional comments

--- public/js/main.js
+++ public/js/main.js
@@ -1,4 +1,4 @@
-const deleteBtn = document.querySelectorAll('.fa-trash')
+const deleteBtn = document.querySelectorAll('.fa-trash') //delete button link
 const item = document.querySelectorAll('.item span')
 const itemCompleted = document.querySelectorAll('.item span.completed')
 
