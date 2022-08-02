484822efbf57050550559b871acbeb94897e1572
thomas <thomaalanstevens@gmail.com>
2022-08-02 11:16:13 -0700

    Finished commenting css and ejs files, started commenting js files

--- public/js/main.js
+++ public/js/main.js
@@ -1,21 +1,21 @@
-const deleteBtn = document.querySelectorAll('.fa-trash')
-const item = document.querySelectorAll('.item span')
-const itemCompleted = document.querySelectorAll('.item span.completed')
+const deleteBtn = document.querySelectorAll('.fa-trash') //creating a variable storing all tags with class .fa-trash in the  variable
+const item = document.querySelectorAll('.item span') //creating a variable storing all span tags with class .item in the variable
+const itemCompleted = document.querySelectorAll('.item span.completed') //creating a variable storing all  spans with .completed tags within tags with class .item in the variable 
 
-Array.from(deleteBtn).forEach((element)=>{
-    element.addEventListener('click', deleteItem)
-})
+Array.from(deleteBtn).forEach((element)=>{ // Creating an array from the variable deleteBtn, and calling each item in the array as an element
+    element.addEventListener('click', deleteItem) // Adding an event listener to run the deleteItem function when the tag is clicked.
+}) // Ending forEach
 
-Array.from(item).forEach((element)=>{
-    element.addEventListener('click', markComplete)
-})
+Array.from(item).forEach((element)=>{ // Creating an array from the variable item, and calling each item in the array as an element
+    element.addEventListener('click', markComplete)// Adding an event listener to run the markComplete function when the tag is clicked.
+}) // Ending forEach
 
-Array.from(itemCompleted).forEach((element)=>{
-    element.addEventListener('click', markUnComplete)
-})
+Array.from(itemCompleted).forEach((element)=>{ // Creating an array from the variable deleteBtn, and calling each item in the array as an element
+    element.addEventListener('click', markUnComplete) // Adding an event listener to run the itemCompleted function when the tag is clicked.
+}) // Ending forEach
 
-async function deleteItem(){
-    const itemText = this.parentNode.childNodes[1].innerText
+async function deleteItem(){ // async function declared and named deleteItem, no parameters used.
+    const itemText = this.parentNode.childNodes[1].innerText //declare variable and store inputted text contained in the second child node of a parent node
     try{
         const response = await fetch('deleteItem', {
             method: 'delete',
