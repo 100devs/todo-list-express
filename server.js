ad5cca5beb91dc17c365f36fb223c7d348437812
thomas <thomaalanstevens@gmail.com>
2022-08-02 22:35:10 -0700

    added comments to server.js

--- server.js
+++ server.js
@@ -1,30 +1,30 @@
-const express = require('express')
-const app = express()
-const MongoClient = require('mongodb').MongoClient
-const PORT = 2121
-require('dotenv').config()
+const express = require('express') // Making it possible to use express in this file
+const app = express() //Storing express call to variable called app, shortcut
+const MongoClient = require('mongodb').MongoClient // Making it possible to use methods associated with MongoClient and talk to out DB
+const PORT = 2121 // Set a variable to store our port location where our server will be listening.
+require('dotenv').config() // Making it possible that allows use to create and use our .env file to hide our files and still access what is hidden
 
 
-let db,
-    dbConnectionStr = process.env.DB_STRING,
-    dbName = 'todo'
+let db, // declaring variables, first one db
+    dbConnectionStr = process.env.DB_STRING, // declare variable dbConnectionStr  and assigning our database connection string to it
+    dbName = 'todo' //declare and store todo in dbName, naming our database that we want to access.
 
-MongoClient.connect(dbConnectionStr, { useUnifiedTopology: true })
-    .then(client => {
-        console.log(`Connected to ${dbName} Database`)
-        db = client.db(dbName)
-    })
+MongoClient.connect(dbConnectionStr, { useUnifiedTopology: true }) //Makes request to connect to mongo client database and passing our connection string and addition props.
+    .then(client => { //takes request and then when successful does something
+        console.log(`Connected to ${dbName} Database`) //console logs a message 
+        db = client.db(dbName) //assigning db to be the client database that we want to connect to
+    }) //Closing our then
     
-app.set('view engine', 'ejs')
-app.use(express.static('public'))
-app.use(express.urlencoded({ extended: true }))
-app.use(express.json())
+app.set('view engine', 'ejs') // middleware to allow use to render html using ejs
+app.use(express.static('public')) // middleware to set the location for static assests, css, js, images, etc.
+app.use(express.urlencoded({ extended: true })) // Tells the express to decode and encode urls where the header matches the content. Supports arrays and objects
+app.use(express.json()) //
 
 
-app.get('/',async (request, response)=>{
-    const todoItems = await db.collection('todos').find().toArray()
-    const itemsLeft = await db.collection('todos').countDocuments({completed: false})
-    response.render('index.ejs', { items: todoItems, left: itemsLeft })
+app.get('/',async (request, response)=>{ //
+    const todoItems = await db.collection('todos').find().toArray() //
+    const itemsLeft = await db.collection('todos').countDocuments({completed: false}) //
+    response.render('index.ejs', { items: todoItems, left: itemsLeft }) //
     // db.collection('todos').find().toArray()
     // .then(data => {
     //     db.collection('todos').countDocuments({completed: false})
@@ -35,59 +35,59 @@ app.get('/',async (request, response)=>{
     // .catch(error => console.error(error))
 })
 
-app.post('/addTodo', (request, response) => {
-    db.collection('todos').insertOne({thing: request.body.todoItem, completed: false})
-    .then(result => {
-        console.log('Todo Added')
-        response.redirect('/')
-    })
-    .catch(error => console.error(error))
-})
+app.post('/addTodo', (request, response) => { //
+    db.collection('todos').insertOne({thing: request.body.todoItem, completed: false}) //
+    .then(result => { //
+        console.log('Todo Added') //
+        response.redirect('/') //
+    }) //
+    .catch(error => console.error(error)) //
+}) //
 
-app.put('/markComplete', (request, response) => {
-    db.collection('todos').updateOne({thing: request.body.itemFromJS},{
-        $set: {
-            completed: true
-          }
-    },{
-        sort: {_id: -1},
-        upsert: false
-    })
-    .then(result => {
-        console.log('Marked Complete')
-        response.json('Marked Complete')
-    })
-    .catch(error => console.error(error))
+app.put('/markComplete', (request, response) => { //
+    db.collection('todos').updateOne({thing: request.body.itemFromJS},{ //
+        $set: { //
+            completed: true //
+          } //
+    },{ //
+        sort: {_id: -1}, //
+        upsert: false //
+    }) //
+    .then(result => { //
+        console.log('Marked Complete') //
+        response.json('Marked Complete') //
+    }) //
+    .catch(error => console.error(error)) //
 
-})
+}) //
 
-app.put('/markUnComplete', (request, response) => {
-    db.collection('todos').updateOne({thing: request.body.itemFromJS},{
-        $set: {
-            completed: false
-          }
-    },{
-        sort: {_id: -1},
-        upsert: false
-    })
-    .then(result => {
-        console.log('Marked Complete')
-        response.json('Marked Complete')
-    })
-    .catch(error => console.error(error))
+app.put('/markUnComplete', (request, response) => { //
+    db.collection('todos').updateOne({thing: request.body.itemFromJS},{ //
+        $set: { //
+            completed: false //
+          } //
+    },{ //
+        sort: {_id: -1}, //
+        upsert: false //
+    }) //
+    .then(result => { //
+        console.log('Marked Complete') //
+        response.json('Marked Complete') //
+    }) //
+    .catch(error => console.error(error)) //
 
-})
+}) //
 
-app.delete('/deleteItem', (request, response) => {
-    db.collection('todos').deleteOne({thing: request.body.itemFromJS})
-    .then(result => {
-        console.log('Todo Deleted')
-        response.json('Todo Deleted')
-    })
-    .catch(error => console.error(error))
+app.delete('/deleteItem', (request, response) => { //
+    db.collection('todos').deleteOne({thing: request.body.itemFromJS}) //
+    .then(result => { //
+        console.log('Todo Deleted') //
+        response.json('Todo Deleted') //
+    }) //
+    .catch(error => console.error(error)) //
 
-})
+}) //
 
-app.listen(process.env.PORT || PORT, ()=>{
-    console.log(`Server running on port ${PORT}`)
-})
+app.listen(process.env.PORT || PORT, ()=>{ //
+    console.log(`Server running on port ${PORT}`) //
+}) //
