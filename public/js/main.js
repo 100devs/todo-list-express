48432eafd41e62ae7e227c2fed04f8038c3c2652
Kayla Burns <kaylabri95@gmail.com>
2022-08-03 23:00:16 -0400

    Kayla Burns commented lines

--- public/js/main.js
+++ public/js/main.js
@@ -1,18 +1,24 @@
 const deleteBtn = document.querySelectorAll('.fa-trash')
+// The above creates the delete button, it is made to connect to the html fa-trash icon that is connected to each task
 const item = document.querySelectorAll('.item span')
+// The above creates an item for the todo list and assigns it an icon
 const itemCompleted = document.querySelectorAll('.item span.completed')
+// The above creates a completed item for the todo list and assigns it an icon that denotes a finished state
 
 Array.from(deleteBtn).forEach((element)=>{
     element.addEventListener('click', deleteItem)
 })
+// The above connects the deleteBtn to the act of deletion through a click event. 
 
 Array.from(item).forEach((element)=>{
     element.addEventListener('click', markComplete)
 })
+// The above takes the array that pertains to the item collection, and does a forEach function to it. The forEach selects every item that is clicked, and marks it complete.
 
 Array.from(itemCompleted).forEach((element)=>{
     element.addEventListener('click', markUnComplete)
 })
+// see line 16, except it takes completed items and switches them over to the collection for uncompleted items, or their original undone array
 
 async function deleteItem(){
     const itemText = this.parentNode.childNodes[1].innerText
@@ -27,11 +33,11 @@ async function deleteItem(){
         const data = await response.json()
         console.log(data)
         location.reload()
-
     }catch(err){
         console.log(err)
     }
 }
+// the above async await function waits to (choose the item in the first list and inserts action from our api (utilizes delete method from our api to remove the clicked item from JS) in this case it will delete the item once the api knows which list item and html piece is deleted then publish the resulting data and refresh the page to show now a shorter list. If there is a problem publish that an error occured
 
 async function markComplete(){
     const itemText = this.parentNode.childNodes[1].innerText
@@ -41,7 +47,9 @@ async function markComplete(){
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
                 'itemFromJS': itemText
-            })
+            })45
+            41
+
           })
         const data = await response.json()
         console.log(data)
@@ -51,6 +59,7 @@ async function markComplete(){
         console.log(err)
     }
 }
+// the above async await function waits to (choose the item in the first list and change action from our api (utilizes put method from our api to add the clicked item from one array or collection to another) in this case it will change the item once the api knows which list item and html piece is completed then publish the resulting data aka crossed out task and refresh the page to show now a shorter list of to dos and longer of completed. If there is a problem publish that an error occured
 
 async function markUnComplete(){
     const itemText = this.parentNode.childNodes[1].innerText
@@ -70,3 +79,4 @@ async function markUnComplete(){
         console.log(err)
     }
 }
+// the above async await function waits to (choose the item in the first list and inserts action from our api (utilizes change method from our api to alter the clicked item from JS) in this case it will move the item from one array or collection to the other once the api knows which list item and html piece is affected and connected to which data page then publish the resulting data and refresh the page to show now a different list. If there is a problem publish that an error occured
--- server.js
+++ server.js
@@ -1,24 +1,35 @@
 const express = require('express')
+// This defines express as requiring the use of the middleware 'express'
 const app = express()
+// This makes the app use express
 const MongoClient = require('mongodb').MongoClient
+// This forces the MongoDB data base to be engaged when referenced
 const PORT = 2121
+// This denotes which server port the app should focus on using
 require('dotenv').config()
+// This requires the environment to be configured
 
 
 let db,
     dbConnectionStr = process.env.DB_STRING,
     dbName = 'todo'
+// This establishes short cuts to the data base and stores your login for Mongo and your personal info which contains all that to the .env file
 
 MongoClient.connect(dbConnectionStr, { useUnifiedTopology: true })
     .then(client => {
         console.log(`Connected to ${dbName} Database`)
         db = client.db(dbName)
     })
-    
+// The above connects the MongoClient info to connect to the exterior database using the login info and the method appropriate. It then handles what to do with that connect, and it publishes the name of the database it linked to. 
+
 app.set('view engine', 'ejs')
+// This sets the app to use express.js in accessing the info in these files
 app.use(express.static('public'))
+// This tells the app to use everything in express files publicly and without direction
 app.use(express.urlencoded({ extended: true }))
+// This makes your app use a url encoded express which extends your capabilities
 app.use(express.json())
+// This makes your app use json which allows communication with the api
 
 
 app.get('/',async (request, response)=>{
@@ -33,6 +44,7 @@ app.get('/',async (request, response)=>{
     //     })
     // })
     // .catch(error => console.error(error))
+    // The above is express doing the heavy lifting, 
 })
 
 app.post('/addTodo', (request, response) => {
@@ -43,6 +55,7 @@ app.post('/addTodo', (request, response) => {
     })
     .catch(error => console.error(error))
 })
+// This adds a todo item to the collection stored in mongoDB and refreshes the page to show the additional list item, if it doesnt work it will throw an error
 
 app.put('/markComplete', (request, response) => {
     db.collection('todos').updateOne({thing: request.body.itemFromJS},{
@@ -58,8 +71,8 @@ app.put('/markComplete', (request, response) => {
         response.json('Marked Complete')
     })
     .catch(error => console.error(error))
-
 })
+// This changes a todo item from one collection to the other collection stored in mongoDB and refreshes the page to show the different list item(maybe crossed out or lightened and made italics), if it doesnt work it will throw an error
 
 app.put('/markUnComplete', (request, response) => {
     db.collection('todos').updateOne({thing: request.body.itemFromJS},{
@@ -75,7 +88,7 @@ app.put('/markUnComplete', (request, response) => {
         response.json('Marked Complete')
     })
     .catch(error => console.error(error))
-
+    // This changes a todo item from one collection to the other collection stored in mongoDB and refreshes the page to show the different list item(maybe crossed out or lightened and made italics), if it doesnt work it will throw an error
 })
 
 app.delete('/deleteItem', (request, response) => {
@@ -85,9 +98,10 @@ app.delete('/deleteItem', (request, response) => {
         response.json('Todo Deleted')
     })
     .catch(error => console.error(error))
-
+// This deletes a todo item from the collection stored in mongoDB and refreshes the page to show the shorter list, if it doesnt work it will throw an error
 })
 
 app.listen(process.env.PORT || PORT, ()=>{
     console.log(`Server running on port ${PORT}`)
 })
+// This shows that the server is ready to do work and communicate for/with the app and databases, it also shows what port the app's server is functioning off of
