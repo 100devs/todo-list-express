a50f1417e870b2e2a7df526ea4b8f1b96a0bd966
Olivia Bloway <blowayol@msu.edu>
2022-08-03 22:41:45 -0400

    comment

--- public/js/main.js
+++ public/js/main.js
@@ -1,45 +1,45 @@
-const deleteBtn = document.querySelectorAll('.fa-trash')
-const item = document.querySelectorAll('.item span')
-const itemCompleted = document.querySelectorAll('.item span.completed')
+const deleteBtn = document.querySelectorAll('.fa-trash') // grabbing the elements with a class fa-trash and storing it into a variable to call upon
+const item = document.querySelectorAll('.item span') // grabbign the spans that's in a parent element with the class of item and storing it into a variable named item
+const itemCompleted = document.querySelectorAll('.item span.completed')  // grabbing the span with a class completed that's in a list element with class name item and storing it into a variable itemcompleted
 
-Array.from(deleteBtn).forEach((element)=>{
-    element.addEventListener('click', deleteItem)
-})
+Array.from(deleteBtn).forEach((element)=>{   // it's taking the deltebtn elements and turning the items into an array, and take each element in that array  and attaching an event listener to the parent element so that when theres a click, the deleteItem function will fire 
+    element.addEventListener('click', deleteItem)  // and add an event listener to those elemnts, which will fire when theres a click an item will delete
+}) //closing loop
 
-Array.from(item).forEach((element)=>{
+Array.from(item).forEach((element)=>{      //creating an array from variables Item starting a loop and attaching an event handler to  the elments that are spans that have a parent of item (these will be marked complete)
     element.addEventListener('click', markComplete)
-})
+}) //close the loop
 
-Array.from(itemCompleted).forEach((element)=>{
+Array.from(itemCompleted).forEach((element)=>{   // creats an array from each item that is in the varialbes itemUncompleted and adding an event lsitener to the current item that run when there's a click which will initiate the function called 'markUncompleted'
     element.addEventListener('click', markUnComplete)
-})
+}) //close the loop
 
-async function deleteItem(){
+async function deleteItem(){   // the delte sync function in which we formed earlier
+    const itemText = this.parentNode.childNodes[1].innerText  // looks inside of the list item to extract the text value only of the specified list item 
+    try{ // try catch block, does something
+        const response = await fetch('deleteItem', {  // creates a response variable that waits on a fetch to get data,from the result of deleteitem
+            method: 'delete',   // using a delelte more 
+            headers: {'Content-Type': 'application/json'}, //specifying the content which is json
+            body: JSON.stringify({     // then it will take the message content thats being passed and turn into json and returns it as a string 
+              'itemFromJS': itemText   // setting the contnt of the body to the inner text of the list item and naming it itemfrom js 
+            }) // closing body
+          }) //closing the object
+        const data = await response.json() // waiing for server to respond with json 
+        console.log(data) // when it's done it will console log the data and reload 
+        location.reload() //reloads page to update what is displayed
+
+    }catch(err){ //if error pass eroor into catch
+        console.log(err)   // if theres an error it will let me know by logging it
+    } //closing catch block
+} //close function
+ 
+async function markComplete(){    // the asyc funtion we determined earlier
     const itemText = this.parentNode.childNodes[1].innerText
     try{
-        const response = await fetch('deleteItem', {
-            method: 'delete',
-            headers: {'Content-Type': 'application/json'},
-            body: JSON.stringify({
-              'itemFromJS': itemText
-            })
-          })
-        const data = await response.json()
-        console.log(data)
-        location.reload()
-
-    }catch(err){
-        console.log(err)
-    }
-}
-
-async function markComplete(){
-    const itemText = this.parentNode.childNodes[1].innerText
-    try{
-        const response = await fetch('markComplete', {
-            method: 'put',
-            headers: {'Content-Type': 'application/json'},
-            body: JSON.stringify({
+        const response = await fetch('markComplete', {   //creating a variable and storing the awaited fetch and it's fetvhing the markcomplete
+            method: 'put', // telling us it's using a put method which is updating the marked item
+            headers: {'Content-Type': 'application/json'}, // and again inserting content in the headers and retirning a json app
+            body: JSON.stringify({   // that's brought back in a string 
                 'itemFromJS': itemText
             })
           })
@@ -53,10 +53,10 @@ async function markComplete(){
 }
 
 async function markUnComplete(){
-    const itemText = this.parentNode.childNodes[1].innerText
+    const itemText = this.parentNode.childNodes[1].innerText // same thing with the functions to mark things incomplete
     try{
         const response = await fetch('markUnComplete', {
-            method: 'put',
+            method: 'put',  // updating the uncomplete items when the click happens
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
                 'itemFromJS': itemText
--- server.js
+++ server.js
@@ -1,30 +1,31 @@
-const express = require('express')
-const app = express()
-const MongoClient = require('mongodb').MongoClient
-const PORT = 2121
-require('dotenv').config()
+const express = require('express') // brings in express into node after installing it
+const app = express()  // creating express application, when we use app were talking about express
+const MongoClient = require('mongodb').MongoClient // requires mongdo client library be brought in 
+const PORT = 2121   // establishing your port, this is the localtion or number your app runs on, and creates a server for app to run
+require('dotenv').config()   // allow you to import your your private varialbes in dotenv and keep it private, when uploading to github put it in gitignore file
 
 
 let db,
-    dbConnectionStr = process.env.DB_STRING,
-    dbName = 'todo'
+    dbConnectionStr = process.env.DB_STRING,  //puts connection string in a varialbe and allows us to put it in dotenv and find it in there
+    dbName = 'todo'    // saves our data base from mongo into a varialbe for us to reference
 
-MongoClient.connect(dbConnectionStr, { useUnifiedTopology: true })
-    .then(client => {
-        console.log(`Connected to ${dbName} Database`)
-        db = client.db(dbName)
+MongoClient.connect(dbConnectionStr, { useUnifiedTopology: true })  //defines how we connect to mongodata base and unified typology, 
+    .then(client => {                                                    // then respond on client side
+        console.log(`Connected to ${dbName} Database`)                  // and say were connected to 'todo' database
+        db = client.db(dbName)                                          // sets db to 'todo' database in the client so connect to database on client side and store it in variable
     })
-    
-app.set('view engine', 'ejs')
-app.use(express.static('public'))
-app.use(express.urlencoded({ extended: true }))
-app.use(express.json())
+
+    //Middle ware
+app.set('view engine', 'ejs')   //were using ejs template to convey our info
+app.use(express.static('public')) // tells express to look into the static forder called public for server side files (js, css, html), so express knows to use it when we call it 
+app.use(express.urlencoded({ extended: true })) // tells express to decode and encode URLSs where the header matches the content.Supports array and objects
+app.use(express.json())   // telling our app to use json method to convert things we ask it to show us into json
 
 
-app.get('/',async (request, response)=>{
-    const todoItems = await db.collection('todos').find().toArray()
-    const itemsLeft = await db.collection('todos').countDocuments({completed: false})
-    response.render('index.ejs', { items: todoItems, left: itemsLeft })
+app.get('/',async (request, response)=>{          // get the root , async funtion so things happen all at the same time, and bring back 2 objects req, res
+    const todoItems = await db.collection('todos').find().toArray()  // set a varialbe and awaits go into database 'todos' collection find the documents in it and turn them into an array and store it into a varialbe (todoItems)
+    const itemsLeft = await db.collection('todos').countDocuments({completed: false}) // go into the 'todos' collection and count the documents, using countDocuments call back method, and get all the items that have a query param completed that set to false 
+    response.render('index.ejs', { items: todoItems, left: itemsLeft }) // respond back to server and tell it to  render the page in index.ejs, and update the items (which come from the todoItems variable ) and the left (whcih comes for itemsleft variable) so tells us how much items we have and how much we dont have completed and putting it into ejs
     // db.collection('todos').find().toArray()
     // .then(data => {
     //     db.collection('todos').countDocuments({completed: false})
@@ -35,56 +36,56 @@ app.get('/',async (request, response)=>{
     // .catch(error => console.error(error))
 })
 
-app.post('/addTodo', (request, response) => {
-    db.collection('todos').insertOne({thing: request.body.todoItem, completed: false})
-    .then(result => {
-        console.log('Todo Added') 
-        response.redirect('/')
+app.post('/addTodo', (request, response) => {  // this is telling app, to allow people to create new things (tasks) using a path of '/addTodo (mathces action in form in ejs) and then takes 2 objects (req, res)
+    db.collection('todos').insertOne({thing: request.body.todoItem, completed: false}) // go into db collection ('todos) and use the insetOne call back to insert 2 things that meet the query parameters or property, which are a thing, and completed, the thing which comes from the request input, go into the body Object and look for the todoItem property, and the items that have a value of false
+    .then(result => {  // then take the results of this 
+        console.log('Todo Added') // print on the ocnsole that it was added
+        response.redirect('/')   // then respond back to the server and tell it to redirect to the root page
     })
-    .catch(error => console.error(error))
+    .catch(error => console.error(error)) // if theres an error catch it and print what's wrong with it 
 })
 
-app.put('/markComplete', (request, response) => {
-    db.collection('todos').updateOne({thing: request.body.itemFromJS},{
+app.put('/markComplete', (request, response) => {   //telling app to update when we click the markComplete thing 
+    db.collection('todos').updateOne({thing: request.body.itemFromJS},{ // go into the 'todos' collection and use the updateOne to update any thing (it's called thing) that meets the name criteria which will be the requested input that's written into the body object with a property name of itemFromJs
         $set: {
-            completed: true
+            completed: true // it will change the compledted from false to true 
           }
     },{
-        sort: {_id: -1},
-        upsert: false
+        sort: {_id: -1}, // and it will then sort the array and once something has been marked completed, it says if theres more one of the same thing give us the first one and will sort the array in decending order
+        upsert: false // doesnt create a new item it's the query does not get met
     })
-    .then(result => {
-        console.log('Marked Complete')
-        response.json('Marked Complete')
+    .then(result => { //then take the result of that and 
+        console.log('Marked Complete')  //then print it into console
+        response.json('Marked Complete') // and return marked completed and convert it to json which is going back to our fetch in the main js file
     })
-    .catch(error => console.error(error))
+    .catch(error => console.error(error)) // if theres an error catch it and let us know
 
 })
 
-app.put('/markUnComplete', (request, response) => {
-    db.collection('todos').updateOne({thing: request.body.itemFromJS},{
-        $set: {
+app.put('/markUnComplete', (request, response) => { // again were updating this time were marking things as UNcomplete by starting a post method when the mark uncomplete route is passed
+    db.collection('todos').updateOne({thing: request.body.itemFromJS},{ // go into collection and use update method. update the thing param which comes from the requested body input with property name itemfromJs 
+        $set: {             // set the completed status to false, which will undo what we did with mark com[plete]
             completed: false
           }
     },{
-        sort: {_id: -1},
-        upsert: false
+        sort: {_id: -1}, //moves item to the bottom of the list 
+        upsert: false  // upsert is a mixture of insert and update,  it's set to false becuse it prevents insertion if item does not already exist
     })
     .then(result => {
         console.log('Marked Complete')
-        response.json('Marked Complete')
+        response.json('Marked Complete')  // tells you thing thing has been uncompleted 
     })
     .catch(error => console.error(error))
 
 })
 
-app.delete('/deleteItem', (request, response) => {
-    db.collection('todos').deleteOne({thing: request.body.itemFromJS})
-    .then(result => {
-        console.log('Todo Deleted')
-        response.json('Todo Deleted')
+app.delete('/deleteItem', (request, response) => {   // now were deleting, tell app to delte with our delete item method found in main.js
+    db.collection('todos').deleteOne({thing: request.body.itemFromJS}) // go in db collection and use delete one method, delete the stuff that match the query params, 
+    .then(result => { // return result 
+        console.log('Todo Deleted') // console.log(it's been deleted)
+        response.json('Todo Deleted') // if everything is full ok it will return the the result in json format
     })
-    .catch(error => console.error(error))
+    .catch(error => console.error(error)) // if theres an error it will show
 
 })
 
