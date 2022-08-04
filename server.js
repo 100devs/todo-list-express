fad427f42221b97420b525bc73b7fbb6bcd2253a
Omary Ally <omaryally.em@gmail.com>
2022-08-04 02:36:14 -0400

    testing github

--- server.js
+++ server.js
@@ -11,19 +11,21 @@ let db,
 
 MongoClient.connect(dbConnectionStr, { useUnifiedTopology: true })
     .then(client => {
+        //let you test if you are connected
         console.log(`Connected to ${dbName} Database`)
         db = client.db(dbName)
     })
-    
+
+//install dependencies
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 
-
-app.get('/',async (request, response)=>{
+//set / as a route
+app.get('/', async (request, response) => {
     const todoItems = await db.collection('todos').find().toArray()
-    const itemsLeft = await db.collection('todos').countDocuments({completed: false})
+    const itemsLeft = await db.collection('todos').countDocuments({ completed: false })
     response.render('index.ejs', { items: todoItems, left: itemsLeft })
     // db.collection('todos').find().toArray()
     // .then(data => {
@@ -34,60 +36,66 @@ app.get('/',async (request, response)=>{
     // })
     // .catch(error => console.error(error))
 })
-
+//api call to create a new todo item
 app.post('/addTodo', (request, response) => {
-    db.collection('todos').insertOne({thing: request.body.todoItem, completed: false})
-    .then(result => {
-        console.log('Todo Added')
-        response.redirect('/')
-    })
-    .catch(error => console.error(error))
+    db.collection('todos').insertOne({ thing: request.body.todoItem, completed: false })
+        .then(result => {
+            console.log('Todo Added')
+            response.redirect('/')
+        })
+        .catch(error => console.error(error))
 })
 
+//api call to update todo
 app.put('/markComplete', (request, response) => {
-    db.collection('todos').updateOne({thing: request.body.itemFromJS},{
+    db.collection('todos').updateOne({ thing: request.body.itemFromJS }, {
+        //dollar sign set key value completed to true
         $set: {
             completed: true
-          }
-    },{
-        sort: {_id: -1},
+        }
+    }, {
+        //Prevent from updating an item that does'nt exist
+        sort: { _id: -1 },
         upsert: false
     })
-    .then(result => {
-        console.log('Marked Complete')
-        response.json('Marked Complete')
-    })
-    .catch(error => console.error(error))
+        .then(result => {
+            //message confirming update completed
+            console.log('Marked Complete')
+            //response to server.js request
+            response.json('Marked Complete')
+        })
+        .catch(error => console.error(error))
 
 })
 
+
 app.put('/markUnComplete', (request, response) => {
-    db.collection('todos').updateOne({thing: request.body.itemFromJS},{
+    db.collection('todos').updateOne({ thing: request.body.itemFromJS }, {
         $set: {
             completed: false
-          }
-    },{
-        sort: {_id: -1},
+        }
+    }, {
+        sort: { _id: -1 },
         upsert: false
     })
-    .then(result => {
-        console.log('Marked Complete')
-        response.json('Marked Complete')
-    })
-    .catch(error => console.error(error))
+        .then(result => {
+            console.log('Marked Complete')
+            response.json('Marked Complete')
+        })
+        .catch(error => console.error(error))
 
 })
 
 app.delete('/deleteItem', (request, response) => {
-    db.collection('todos').deleteOne({thing: request.body.itemFromJS})
-    .then(result => {
-        console.log('Todo Deleted')
-        response.json('Todo Deleted')
-    })
-    .catch(error => console.error(error))
+    db.collection('todos').deleteOne({ thing: request.body.itemFromJS })
+        .then(result => {
+            console.log('Todo Deleted')
+            response.json('Todo Deleted')
+        })
+        .catch(error => console.error(error))
 
 })
 
-app.listen(process.env.PORT || PORT, ()=>{
+app.listen(process.env.PORT || PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
