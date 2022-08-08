4d73dde38a835084a37ff8d0af5ee908252ce869
Ray Anthoney <rmaxey911@gmail.com>
2022-08-08 07:56:00 -0600

    Commented code for pull request

--- public/js/main.js
+++ public/js/main.js
@@ -1,72 +1,72 @@
-const deleteBtn = document.querySelectorAll('.fa-trash')
-const item = document.querySelectorAll('.item span')
-const itemCompleted = document.querySelectorAll('.item span.completed')
+const deleteBtn = document.querySelectorAll('.fa-trash') /* UNCHANGEABLE VARIABLE SELECTING CLASS ELEMENTS STORING IT IN THE DELETEBTN */
+const item = document.querySelectorAll('.item span') /* UNCHANGEABLE VARIABLE SELECTING CLASS ELEMENTS ASSIGNING THEM TO THE CHILD ITEM SPAN */
+const itemCompleted = document.querySelectorAll('.item span.completed') /* UNCHANGEABLE VARIABLE SELECTING CLASS ELEMENTS ASSIGNING THEM TO THE CHILD ITEM CLASS COMPLETED */
 
-Array.from(deleteBtn).forEach((element)=>{
-    element.addEventListener('click', deleteItem)
-})
+Array.from(deleteBtn).forEach((element)=>{ /* ARRAY METHOD CREATED FROM SELECTION AND BEGINING A LOOP */
+    element.addEventListener('click', deleteItem) /* EVENT LISTENER CLICK FROM THE ITEM CALLING FUNCTION TO deleteItem */
+}) /* ARRAY LOOP CLOSING TAG */
 
-Array.from(item).forEach((element)=>{
-    element.addEventListener('click', markComplete)
-})
+Array.from(item).forEach((element)=>{ /* ARRAY METHOD CREATED FROM SELECTION AND BEGINING A LOOP */
+    element.addEventListener('click', markComplete) /* EVENT LISTENER CLICK FROM THE ITEM CALLING FUNCTION TO markComplete */
+}) /* ARRAY LOOP CLOSING TAG */
 
-Array.from(itemCompleted).forEach((element)=>{
-    element.addEventListener('click', markUnComplete)
-})
+Array.from(itemCompleted).forEach((element)=>{ /* ARRAY METHOD CREATED FROM SELECTION AND BEGINING A LOOP */
+    element.addEventListener('click', markUnComplete) /* EVENT LISTENER FOR COMPLETED ITEMS */
+}) /* ARRAY LOOP CLOSISNG TAG */
 
-async function deleteItem(){
-    const itemText = this.parentNode.childNodes[1].innerText
-    try{
-        const response = await fetch('deleteItem', {
-            method: 'delete',
-            headers: {'Content-Type': 'application/json'},
-            body: JSON.stringify({
-              'itemFromJS': itemText
-            })
-          })
-        const data = await response.json()
-        console.log(data)
-        location.reload()
+async function deleteItem(){ /* ASYNCHRONOUS FUNCTION DECLARED  */
+    const itemText = this.parentNode.childNodes[1].innerText /* LIST ITEM THAT GRABS THE INNER TEXT OF THE LIST SPAN */
+    try{ /* TRY BLOCK START */
+        const response = await fetch('deleteItem', { /* FETCH VARIABLE TO RETRIEVE DATA AND GIVES A RESPONSE OF deleteItem */
+            method: 'delete', /* DELETE AY METHOD  */
+            headers: {'Content-Type': 'application/json'}, /* DECLARING THE USE OF JSON AS THE CONTENT TYPE USED */
+            body: JSON.stringify({ /* STRINGIFY THE RETURN MESSAGE CONTENT */
+              'itemFromJS': itemText /*  CONTENT OF THE BODY TO THE INNER TEXT OF THE LIST ITEM */
+            }) /* CLOSING THE BODY */
+          }) /* CLOSING THE OBJECT */
+        const data = await response.json() /* WAITING ON JSON FROM THE RESPONSE TO BE CONVERTED */
+        console.log(data) /* LOG THE RESULT THE CONSOLE */
+        location.reload() /* RELOADS THE PAGE TO UPDATE WHAT IS DISPLAYED  */
 
-    }catch(err){
-        console.log(err)
-    }
-}
+    }catch(err){ /* IF AN ERROR OCCURS, PASS THE ERROR INTO THE CATCH BLOCK */
+        console.log(err) /* LOGS THE ERROR TO THE CONSOLE */
+    } /* CLOSE THE CATCH BLOCK */
+} /* END THE FUNCTION */
 
-async function markComplete(){
-    const itemText = this.parentNode.childNodes[1].innerText
-    try{
-        const response = await fetch('markComplete', {
-            method: 'put',
-            headers: {'Content-Type': 'application/json'},
-            body: JSON.stringify({
-                'itemFromJS': itemText
-            })
-          })
-        const data = await response.json()
-        console.log(data)
-        location.reload()
+async function markComplete(){ /* ASYNCHRONOUS FUNCTION DECLARED */
+    const itemText = this.parentNode.childNodes[1].innerText /* LIST ITEM THAT GRABS THE INNER TEXT OF THE LIST SPAN */
+    try{ /* TRY BLOCK START */
+        const response = await fetch('markComplete', { /* FETCH VARIABLE TO RETRIEVE DATA AND GIVES A RESPONSE OF markComplete */
+            method: 'put', /* SETTING THE CRUD METHOD TO "update" FOR THE ROUTE */
+            headers: {'Content-Type': 'application/json'}, /* SPECIFYING THE TYPE OF CONTENT EXPECTED, JSON */
+            body: JSON.stringify({ /* STRINGIFY THE RETURN MESSAGE CONTENT */
+                'itemFromJS': itemText /* CONTENT OF THE BODY TO THE INNER TEXT OF THE LIST ITEM */
+            }) /* CLOSING THE BODY  */
+          }) /* CLOSING THE OBJECT */
+        const data = await response.json() /* WAITING ON JSON FROM THE RESPONSE TO BE CONVERTED */
+        console.log(data) /* LOG THE RESULT THE CONSOLE */
+        location.reload() /* RELOADS THE PAGE TO UPDATE WHAT IS DISPLAYED */
 
-    }catch(err){
-        console.log(err)
-    }
-}
+    }catch(err){ /* IF AN ERROR OCCURS, PASS THE ERROR INTO THE CATCH BLOCK  */
+        console.log(err) /* LOGS THE ERROR TO THE CONSOLE */
+    } /* CLOSE THE CATCH BLOCK */
+} /* END THE FUNCTION */
 
-async function markUnComplete(){
-    const itemText = this.parentNode.childNodes[1].innerText
-    try{
-        const response = await fetch('markUnComplete', {
-            method: 'put',
-            headers: {'Content-Type': 'application/json'},
-            body: JSON.stringify({
-                'itemFromJS': itemText
-            })
-          })
-        const data = await response.json()
-        console.log(data)
-        location.reload()
+async function markUnComplete(){ /* ASYNCHRONOUS FUNCTION DECLARED */
+    const itemText = this.parentNode.childNodes[1].innerText /* LIST ITEM THAT GRABS THE INNER TEXT OF THE LIST SPAN */
+    try{ /* TRY BLOCK START  */
+        const response = await fetch('markUnComplete', { /* FETCH VARIABLE TO RETRIEVE DATA AND GIVES A RESPONSE OF markComplete */
+            method: 'put', /* SETTING THE CRUD METHOD TO "update" FOR THE ROUTE */
+            headers: {'Content-Type': 'application/json'}, /* SPECIFYING THE TYPE OF CONTENT EXPECTED, JSON */
+            body: JSON.stringify({ /* STRINGIFY THE RETURN MESSAGE CONTENT */
+                'itemFromJS': itemText /* CONTENT OF THE BODY TO THE INNER TEXT OF THE LIST ITEM */
+            }) /* CLOSING THE BODY */
+          }) /* CLOSING THE OBJECT */
+        const data = await response.json() /* WAITING ON JSON FROM THE RESPONSE TO BE CONVERTED */
+        console.log(data) /* LOG THE RESULT THE CONSOLE */
+        location.reload() /* RELOADS THE PAGE TO UPDATE WHAT IS DISPLAYED */
 
-    }catch(err){
-        console.log(err)
-    }
-}
+    }catch(err){ /* IF AN ERROR OCCURS, PASS THE ERROR INTO THE CATCH BLOCK */
+        console.log(err) /* LOGS THE ERROR TO THE CONSOLE */
+    } /* CLOSE THE CATCH BLOCK */
+} /* END THE FUNCTION */    
--- server.js
+++ server.js
@@ -1,30 +1,74 @@
-const express = require('express')
-const app = express()
-const MongoClient = require('mongodb').MongoClient
-const PORT = 2121
-require('dotenv').config()
+/*
+    npm init
+    ( CREATES PACKAGE.JSON )
+    npm install
+    ( NODE MODULES )
+    npm install --save-dev nodemon
+    "start": "nodemon server.js"
+*/
+
+/* PREP
+    P = PARAMETERS - (ONCLICK TEXTBOX, TRASHCAN, ITEMS SUBMIT BUTTON)
+    R = RETURN - (NEW ITEM COMES BACK)
+    E = EXAMPLES - (RETURN MARKED AS COMPLETE, REMOVE ITEM FROM LIST, COUNTER DECRAMENTS, INCREMENT TO ZERO AS BASE)
+    P = PSUEDOCODE - (
+            USER FACING FRONT END
+                TYPE A NEW ITEM 
+                SUBMIT NEW ITEM
+                   CLICK EVENT LISTENER
+                   SEND DATA TO AN API (SERVER)
+                   NEW ITEM CREATED (DATABASE ENTRY)
+                CLICK ON TRASHCAN (DELETE REQUEST)
+                    CLICK EVENT LISTENER
+                    SEND DATA WITH INFO TO SERVER
+                CLICK ON AN ITEM 
+                    CLICK EVENT LISTENER
+                    UPDATE "PUT" FLAG CHANGE
+            MONGODB BACKEND
+                STORE DATA
+
+    ) 
+*/
+/* CRUD 
+    C = CREATE 
+    R = READ 
+    U = UPDATE
+    D = DELETE
+*/
+
+// DECLARE VARIBLES
+const express = require('express') // DECLARATION OF THE USE OF EXPRESS  
+const app = express() // SETTING AND ASSIGNING EXPRESS AS A VARIABLE
+const MongoClient = require('mongodb').MongoClient // DECLARATION OF THE USE OF MONGODB 
+const PORT = 2121 // ASSIGNING THE PORT TO USE LOCALLY
+require('dotenv').config() // ASSIGNING A A FILE (.env) WHERE PRIVATE VARIABLES CAN BE ASSIGNED 
 
 
-let db,
-    dbConnectionStr = process.env.DB_STRING,
-    dbName = 'todo'
+// CONNECT TO MONGODB
+let db, // DECLARING A VARIABLE (db) SO A VALUE CAN BE ASSIGNED TO IT
+    dbConnectionStr = process.env.DB_STRING, // DECLARING A VARIABLE AND ASSIGNING A DATABASE CONNECTION STRING
+    dbName = 'todo' // DECLARING A VARIABLE AND ASSIGNING A NAME OF THE DATABASE BEING USED
 
-MongoClient.connect(dbConnectionStr, { useUnifiedTopology: true })
-    .then(client => {
-        console.log(`Connected to ${dbName} Database`)
-        db = client.db(dbName)
-    })
+MongoClient.connect(dbConnectionStr, { useUnifiedTopology: true }) // CREATING A CONNECTION TO MOGODB, AND PASSING IN OUR CONNECTIOIN STRING AND IT'S PROPERTY
+    .then(client => { // WAITS FOR THE CONNECTION. PROCEDDING IF SUCCESFUL, AND POSTING ALL CLIENT INFO
+        console.log(`Connected to ${dbName} Database`) // LOG TO CONSOLE A TEMPLATE LITERAL USING THE VARIABLE "dbName" 
+        db = client.db(dbName) // ASSIGNING A VALUE TO PREVIOUSLYDECLARED db VARIABLE THAT CONTAINS A db CLIENT FACTORY METHOD
+    }) // CLOSING THEN 
     
-app.set('view engine', 'ejs')
-app.use(express.static('public'))
-app.use(express.urlencoded({ extended: true }))
-app.use(express.json())
+// MIDDLEWARE SET THE TEMPLATE ENGINE (USING EJS)
+app.set('view engine', 'ejs') // SETTING ejs AS THE DEFAULT VIEW ENGINE 
+
+// STORE STATIC FILES FOLDER
+app.use(express.static('public')) // LOCATION FOR STATIC ASSETS FOLDER
+app.use(express.urlencoded({ extended: true })) // TELLS EXPRESS TO DECODE AND ENCODE URLs WHERE THE HEADER MATCHES THE CONTENT.
+app.use(express.json()) // PARSES INCOMING CONTENT FROM INCOMING REQUESTS
 
 
-app.get('/',async (request, response)=>{
-    const todoItems = await db.collection('todos').find().toArray()
-    const itemsLeft = await db.collection('todos').countDocuments({completed: false})
-    response.render('index.ejs', { items: todoItems, left: itemsLeft })
+// GET METHOD
+app.get('/',async (request, response)=>{ // STARTS A get METHOD WHEN THE ROUTE IS PASSED IN, SETS UP REQUEST, AND RESPONSE PARAMETERS
+    const todoItems = await db.collection('todos').find().toArray() // SETS A VARIABLE AND AWAITS  ALL ITEMS FROM THE todos COLLECTION
+    const itemsLeft = await db.collection('todos').countDocuments({completed: false}) // SETS A VARIABLE AND AWAITS A COUNT OF UNCOMPLETED ITEMS TO LATER DISPLAY IN EJS
+    response.render('index.ejs', { items: todoItems, left: itemsLeft }) // RENDERS EJS FILES AND PASSING THRU THE db ITEMS AND THE COUNT REMAINING INSIDE OF AN OBJECT
     // db.collection('todos').find().toArray()
     // .then(data => {
     //     db.collection('todos').countDocuments({completed: false})
@@ -33,61 +77,66 @@ app.get('/',async (request, response)=>{
     //     })
     // })
     // .catch(error => console.error(error))
-})
+}) // CLOSES THE GET METHOD
 
-app.post('/addTodo', (request, response) => {
-    db.collection('todos').insertOne({thing: request.body.todoItem, completed: false})
-    .then(result => {
-        console.log('Todo Added')
-        response.redirect('/')
-    })
-    .catch(error => console.error(error))
-})
+// POST METHOD
+app.post('/addTodo', (request, response) => { // STARTS A POST METHOD WHEN THE ADD ROUTE IS PASSED 
+    db.collection('todos').insertOne({thing: request.body.todoItem, completed: false}) // INSERTS AN ITEM INTO todos COLLECTION WITH COMPLETED VALUE OF FALSE BY DEFAULT
+    .then(result => { // IF SUCCESSFUL DO SOMETHING 
+        console.log('Todo Added') // CONSOLE LOG ACTION 
+        response.redirect('/') // GETS RID OF THE ADD addTodo ROUTE 
+    }) // CLOSING THE  THEN
+    .catch(error => console.error(error)) // CATCHING ERRORS
+}) // SENDING THE POST
 
-app.put('/markComplete', (request, response) => {
-    db.collection('todos').updateOne({thing: request.body.itemFromJS},{
-        $set: {
-            completed: true
+// PUT METHOD
+app.put('/markComplete', (request, response) => { // STARTS A PUT METHOD WHEN THE MARKCOMPLETE ROUTE IS PASSED 
+    db.collection('todos').updateOne({thing: request.body.itemFromJS},{ // LOOKS IN THE DATABASE FOR ONE ITEM MATCHING THE NAME OF THE ITEM PASSED IN FROM THE MAIN.JS THAT WAS CLICKED ON
+        $set: { // 
+            completed: true // SETS COMPLETED STATUS TO TRUE 
           }
     },{
-        sort: {_id: -1},
-        upsert: false
+        sort: {_id: -1}, // MOVES ITEM TO THE BOTTOM OF THE LIST
+        upsert: false // PREVENTS INSERTION IF ITEM DOES NOT ALREADY EXIST
     })
-    .then(result => {
-        console.log('Marked Complete')
-        response.json('Marked Complete')
-    })
-    .catch(error => console.error(error))
+    .then(result => { // STARTS A THEN IF UPDATE WAS SUCCESSFUL
+        console.log('Marked Complete') // CONSOLE LOGGING SUCCESS
 
-})
+        response.json('Marked Complete') // SENDING A RESPONSE BACK TO SENDER
+    }) // CLOSING THE TRHEN STATEMENT
+    .catch(error => console.error(error)) // CATCH ERRORS
 
-app.put('/markUnComplete', (request, response) => {
-    db.collection('todos').updateOne({thing: request.body.itemFromJS},{
-        $set: {
-            completed: false
-          }
+}) // CLOSING THE PUT METHOD
+
+app.put('/markUnComplete', (request, response) => { // STARTS A PUT METHOD WHEN THE MARKCOMPLETE ROUTE IS PASSED 
+    db.collection('todos').updateOne({thing: request.body.itemFromJS},{ // LOOKS IN THE DATABASE FOR ONE ITEM MATCHING THE NAME OF THE ITEM PASSED IN FROM THE MAIN.JS THAT WAS CLICKED ON
+        $set: { // SETS COMPLETED STATUS TO FALSE
+            completed: false // SETS COMPLETED STATUS TO FLASE
+          } 
     },{
-        sort: {_id: -1},
-        upsert: false
+        sort: {_id: -1}, // MOVES ITEM TO THE BOTTOM OF THE LIST
+        upsert: false // PREVENTS INSERTION IF ITEM DOES NOT ALREADY EXIST
     })
-    .then(result => {
-        console.log('Marked Complete')
-        response.json('Marked Complete')
+    .then(result => { // STARTS A THEN IF UPDATE WAS SUCCESSFUL
+        console.log('Marked Complete') // CONSOLE LOGGING SUCCESS
+        response.json('Marked Complete') // SENDING A RESPONSE BACK TO SENDER
     })
-    .catch(error => console.error(error))
+    .catch(error => console.error(error)) // CATCH ERRORS
 
-})
+}) // CLOSING THE PUT METHOD
 
-app.delete('/deleteItem', (request, response) => {
-    db.collection('todos').deleteOne({thing: request.body.itemFromJS})
-    .then(result => {
-        console.log('Todo Deleted')
-        response.json('Todo Deleted')
+// DELETE METHOD
+app.delete('/deleteItem', (request, response) => { // STARTS A DELETE METHOD WHEN THE DELETE ROUTE IS PASSED 
+    db.collection('todos').deleteOne({thing: request.body.itemFromJS}) // LOOKLS INSIDE THE todos COLLECTION FOR THE ONE ITEM THAT HAS A MATCHING NAME FROM OUR JS FILE
+    .then(result => { // STARTS A THEN IF UPDATE WAS SUCCESSFUL
+        console.log('Todo Deleted') // CONSOLE LOGGING SUCCESS
+        response.json('Todo Deleted') // SENDING A RESPONSE BACK TO SENDER
     })
-    .catch(error => console.error(error))
+    .catch(error => console.error(error))  // CATCH ERRORS
 
-})
+}) // CLOSING THE DELETE METHOD
 
-app.listen(process.env.PORT || PORT, ()=>{
-    console.log(`Server running on port ${PORT}`)
-})
+// START SERVER LISTENING ON PORT 2121
+app.listen(process.env.PORT || PORT, ()=>{ // SETTING UP WHICH PORT WE WILL BE LISTENING ON - EITHER THE PORT DROM THE .env FILE OR THE PORT VARIABLE SET ABOVE
+    console.log(`Server running on port ${PORT} You Betta go and catch it!`) // CONSOLE LOG THE RUNNING PORT 
+}) // CLOSING THE LISTEN METHOD
