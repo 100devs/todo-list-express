b1bdd252e141556ed60c53d021942aa39e6b33c8
Josh Moore <joshuamoore5899@gmail.com>
2022-08-08 09:12:30 -0600

    comments to server.js

--- server.js
+++ server.js
@@ -1,30 +1,30 @@
-const express = require('express')
+const express = require('express')  //importing express module
 const app = express()
 const MongoClient = require('mongodb').MongoClient
 const PORT = 2121
 require('dotenv').config()
 
 
-let db,
+let db,     //adding db variables, especially DB_STRING which is found in .env to connect to mongo
     dbConnectionStr = process.env.DB_STRING,
     dbName = 'todo'
 
-MongoClient.connect(dbConnectionStr, { useUnifiedTopology: true })
+MongoClient.connect(dbConnectionStr, { useUnifiedTopology: true })  //connecting to db
     .then(client => {
         console.log(`Connected to ${dbName} Database`)
         db = client.db(dbName)
     })
-    
-app.set('view engine', 'ejs')
-app.use(express.static('public'))
+
+app.set('view engine', 'ejs')  //settng up ejs middleware
+app.use(express.static('public')) //serves up all static files in public folder
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 
 
-app.get('/',async (request, response)=>{
-    const todoItems = await db.collection('todos').find().toArray()
-    const itemsLeft = await db.collection('todos').countDocuments({completed: false})
-    response.render('index.ejs', { items: todoItems, left: itemsLeft })
+app.get('/',async (request, response)=>{ //serves up main home page -- READ
+    const todoItems = await db.collection('todos').find().toArray() //getting all todo items and making an array out of them
+    const itemsLeft = await db.collection('todos').countDocuments({completed: false}) //returns number of todo items that haven't been completed
+    response.render('index.ejs', { items: todoItems, left: itemsLeft }) //sending the above two variables to the index.ejs page to be used there
     // db.collection('todos').find().toArray()
     // .then(data => {
     //     db.collection('todos').countDocuments({completed: false})
@@ -35,23 +35,23 @@ app.get('/',async (request, response)=>{
     // .catch(error => console.error(error))
 })
 
-app.post('/addTodo', (request, response) => {
-    db.collection('todos').insertOne({thing: request.body.todoItem, completed: false})
+app.post('/addTodo', (request, response) => { //POST/CREATE request coming from form on index.ejs page
+    db.collection('todos').insertOne({thing: request.body.todoItem, completed: false})//adding item to mongo (get request.body.todoItem from form on index.ejs page), setting variables for item added to mongo
     .then(result => {
         console.log('Todo Added')
-        response.redirect('/')
+        response.redirect('/') //reloading page with newly added item from mongo
     })
     .catch(error => console.error(error))
 })
 
-app.put('/markComplete', (request, response) => {
-    db.collection('todos').updateOne({thing: request.body.itemFromJS},{
-        $set: {
+app.put('/markComplete', (request, response) => {//PUT/UPDATE request coming from main.js page
+    db.collection('todos').updateOne({thing: request.body.itemFromJS},{//sending update request to mongo and finding item to update using request.body.itemFromJS
+        $set: {//updating variable in mongo
             completed: true
           }
     },{
-        sort: {_id: -1},
-        upsert: false
+        sort: {_id: -1}, //not soring?
+        upsert: false //not creating item if not found in list
     })
     .then(result => {
         console.log('Marked Complete')
@@ -61,9 +61,9 @@ app.put('/markComplete', (request, response) => {
 
 })
 
-app.put('/markUnComplete', (request, response) => {
-    db.collection('todos').updateOne({thing: request.body.itemFromJS},{
-        $set: {
+app.put('/markUnComplete', (request, response) => { //PUT/UPDATE request from main.js
+    db.collection('todos').updateOne({thing: request.body.itemFromJS},{//sending request to mongo and finding item requested to be updated
+        $set: { //updating item in mongo
             completed: false
           }
     },{
@@ -78,8 +78,8 @@ app.put('/markUnComplete', (request, response) => {
 
 })
 
-app.delete('/deleteItem', (request, response) => {
-    db.collection('todos').deleteOne({thing: request.body.itemFromJS})
+app.delete('/deleteItem', (request, response) => { //DELETE request from main.js
+    db.collection('todos').deleteOne({thing: request.body.itemFromJS}) //sending request to mongo using identifier from request.body.itemFromJS
     .then(result => {
         console.log('Todo Deleted')
         response.json('Todo Deleted')
@@ -88,6 +88,6 @@ app.delete('/deleteItem', (request, response) => {
 
 })
 
-app.listen(process.env.PORT || PORT, ()=>{
+app.listen(process.env.PORT || PORT, ()=>{//creating server and listening to env port or PORT variable port
     console.log(`Server running on port ${PORT}`)
 })
