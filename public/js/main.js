890d4c686922eae6501e6af5089ff990f1059a2e
Jason Cassella <jasoncassella1@gmail.com>
2022-08-08 14:13:31 -0400

    two space gang

--- public/js/main.js
+++ public/js/main.js
@@ -3,70 +3,70 @@ const item = document.querySelectorAll('.item span');
 const itemCompleted = document.querySelectorAll('.item span.completed');
 
 Array.from(deleteBtn).forEach((element) => {
-    element.addEventListener('click', deleteItem);
+	element.addEventListener('click', deleteItem);
 });
 
 Array.from(item).forEach((element) => {
-    element.addEventListener('click', markComplete);
+	element.addEventListener('click', markComplete);
 });
 
 Array.from(itemCompleted).forEach((element) => {
-    element.addEventListener('click', markIncomplete);
+	element.addEventListener('click', markIncomplete);
 });
 
 async function deleteItem() {
-    const itemText = this.parentNode.childNodes[1].innerText;
-    try {
-        const response = await fetch('deleteItem', {
-            method: 'delete',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify({
-                'itemFromJS': itemText
-            })
-        });
-        const data = await response.json();
-        console.log(data);
-        location.reload();
+	const itemText = this.parentNode.childNodes[1].innerText;
+	try {
+		const response = await fetch('deleteItem', {
+			method: 'delete',
+			headers: { 'Content-Type': 'application/json' },
+			body: JSON.stringify({
+				'itemFromJS': itemText
+			})
+		});
+		const data = await response.json();
+		console.log(data);
+		location.reload();
 
-    } catch (err) {
-        console.log(err);
-    }
+	} catch (err) {
+		console.log(err);
+	}
 }
 
 async function markComplete() {
-    const itemText = this.parentNode.childNodes[1].innerText;
-    try {
-        const response = await fetch('markComplete', {
-            method: 'put',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify({
-                'itemFromJS': itemText
-            })
-        });
-        const data = await response.json();
-        console.log(data);
-        location.reload();
+	const itemText = this.parentNode.childNodes[1].innerText;
+	try {
+		const response = await fetch('markComplete', {
+			method: 'put',
+			headers: { 'Content-Type': 'application/json' },
+			body: JSON.stringify({
+				'itemFromJS': itemText
+			})
+		});
+		const data = await response.json();
+		console.log(data);
+		location.reload();
 
-    } catch (err) {
-        console.log(err);
-    }
+	} catch (err) {
+		console.log(err);
+	}
 }
 
 async function markIncomplete() {
-    const itemText = this.parentNode.childNodes[1].innerText;
-    try {
-        const response = await fetch('markIncomplete', {
-            method: 'put',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify({
-                'itemFromJS': itemText
-            })
-        });
-        const data = await response.json();
-        console.log(data);
-        location.reload();
+	const itemText = this.parentNode.childNodes[1].innerText;
+	try {
+		const response = await fetch('markIncomplete', {
+			method: 'put',
+			headers: { 'Content-Type': 'application/json' },
+			body: JSON.stringify({
+				'itemFromJS': itemText
+			})
+		});
+		const data = await response.json();
+		console.log(data);
+		location.reload();
 
-    } catch (err) {
-        console.log(err);
-    }
+	} catch (err) {
+		console.log(err);
+	}
 }
