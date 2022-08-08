b344052a3e43fabd3c7c585a85870cb8893722e9
Jason Cassella <jasoncassella1@gmail.com>
2022-08-08 14:33:02 -0400

    changed markComplete to only run when an incomplete item is clicked

--- public/js/main.js
+++ public/js/main.js
@@ -1,15 +1,21 @@
+// delete buttons
 const deleteBtn = document.querySelectorAll('.fa-trash');
-const item = document.querySelectorAll('.item span');
+// items without a completed class
+const item = document.querySelectorAll('.item span:not(.completed)');
+// items with a completed class
 const itemCompleted = document.querySelectorAll('.item span.completed');
 
+// give delete buttons functionality of deleteItem on click
 Array.from(deleteBtn).forEach((element) => {
 	element.addEventListener('click', deleteItem);
 });
 
+// give incomplete items functionality of markComplete on click
 Array.from(item).forEach((element) => {
 	element.addEventListener('click', markComplete);
 });
 
+// give complete items functionality of markIncomplete on click
 Array.from(itemCompleted).forEach((element) => {
 	element.addEventListener('click', markIncomplete);
 });
