77fd5cee1fb04a12721fe56167bd04bb6656ec2d
Jacky Liu <jackyliudev@gmail.com>
2022-08-09 19:49:03 -0400

    Added comments for server.js

--- server.js
+++ server.js
@@ -1,26 +1,34 @@
+// Import express library
 const express = require('express')
+// Instaniate express
 const app = express()
+
+// Create database client
 const MongoClient = require('mongodb').MongoClient
+
+//Open port
 const PORT = 2121
 require('dotenv').config()
 
-
+// Create database variables
 let db,
     dbConnectionStr = process.env.DB_STRING,
     dbName = 'todo'
 
+    // Connect to mongodo once connected set database tothe mongodb database
 MongoClient.connect(dbConnectionStr, { useUnifiedTopology: true })
     .then(client => {
         console.log(`Connected to ${dbName} Database`)
         db = client.db(dbName)
     })
-    
+
+    // Set express view engines and folders for static sites
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 
-
+//Publish the static main site
 app.get('/',async (request, response)=>{
     const todoItems = await db.collection('todos').find().toArray()
     const itemsLeft = await db.collection('todos').countDocuments({completed: false})
@@ -35,15 +43,20 @@ app.get('/',async (request, response)=>{
     // .catch(error => console.error(error))
 })
 
+// Add a new to do item by listening on /addTodo for a post
 app.post('/addTodo', (request, response) => {
+    // Add new to do into the database
     db.collection('todos').insertOne({thing: request.body.todoItem, completed: false})
     .then(result => {
+        // refresh the page after todo is added
         console.log('Todo Added')
         response.redirect('/')
     })
     .catch(error => console.error(error))
 })
 
+// Listen for a update on the /markComplete
+// Update the completed property on database
 app.put('/markComplete', (request, response) => {
     db.collection('todos').updateOne({thing: request.body.itemFromJS},{
         $set: {
@@ -53,6 +66,7 @@ app.put('/markComplete', (request, response) => {
         sort: {_id: -1},
         upsert: false
     })
+    // respond to client with result marked complete
     .then(result => {
         console.log('Marked Complete')
         response.json('Marked Complete')
@@ -61,6 +75,7 @@ app.put('/markComplete', (request, response) => {
 
 })
 
+// Similar to mark complete, but set completed to false
 app.put('/markUnComplete', (request, response) => {
     db.collection('todos').updateOne({thing: request.body.itemFromJS},{
         $set: {
@@ -78,6 +93,9 @@ app.put('/markUnComplete', (request, response) => {
 
 })
 
+// Listen for a delete request at /deleteItem
+//remove item from database
+// Respond with todo completion when completed
 app.delete('/deleteItem', (request, response) => {
     db.collection('todos').deleteOne({thing: request.body.itemFromJS})
     .then(result => {
@@ -88,6 +106,7 @@ app.delete('/deleteItem', (request, response) => {
 
 })
 
+// Listen on the defined port to serve the site
 app.listen(process.env.PORT || PORT, ()=>{
     console.log(`Server running on port ${PORT}`)
 })
