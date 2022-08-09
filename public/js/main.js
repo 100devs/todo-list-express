7c0e7e45c76786eeefea4f23de3bf88436844b57
Cautious Engineer <jamaalwbrown@utexas.edu>
2022-08-08 22:50:30 -0500

    Adding the rest of the comments for the homework

--- public/js/main.js
+++ public/js/main.js
@@ -41,41 +41,41 @@ async function deleteItem(){ //declare an asynchronous function
 } //end the function
 
 /*Go find innertext you clicked on in server, then update completion status to true, log data, and reload*/
-async function markComplete(){
-    const itemText = this.parentNode.childNodes[1].innerText
-    try{
-        const response = await fetch('markComplete', {
-            method: 'put',
-            headers: {'Content-Type': 'application/json'},
-            body: JSON.stringify({
-                'itemFromJS': itemText
-            })
-          })
-        const data = await response.json()
-        console.log(data)
-        location.reload()
+async function markComplete(){ //declare an asynchronous function
+    const itemText = this.parentNode.childNodes[1].innerText //looks inside of the list item and grabs only the inner text within the list span
+    try{ //starting a try block to do something
+        const response = await fetch('markComplete', { //Creates a response variable that waits on a fetch to get data from the result of the markComplete route
+            method: 'put', //setting the CRUD method to "update" for the route
+            headers: {'Content-Type': 'application/json'}, //specifying the type of content expected, which is JSON
+            body: JSON.stringify({ //declare the message content being passed, and stringify that content
+                'itemFromJS': itemText //setting the content of the body to the inner text of the list item, and naming it 'itemFromJS'
+            }) //closing the body
+          }) //closing the object
+        const data = await response.json() //waiting on JSON from the response
+        console.log(data) //log the result to the console
+        location.reload() //reloads the page to update what is displayed
 
-    }catch(err){
-        console.log(err)
-    }
-}
+    }catch(err){ //if an error occurs, pass the error into the catch block
+        console.log(err) //log the error to the console
+    } //close the catch block
+} //end the function
 
 /*Go find innertext you clicked on in server, then update completion status to false, log data, and reload*/
 async function markUnComplete(){
-    const itemText = this.parentNode.childNodes[1].innerText
-    try{
-        const response = await fetch('markUnComplete', {
-            method: 'put',
-            headers: {'Content-Type': 'application/json'},
-            body: JSON.stringify({
-                'itemFromJS': itemText
-            })
-          })
-        const data = await response.json()
-        console.log(data)
-        location.reload()
+    const itemText = this.parentNode.childNodes[1].innerText //looks ins
+    try{ //starting a try block to do something
+        const response = await fetch('markUnComplete', { //creates a response variable that waits on a fetch to get data from the result of the markUnComplete route
+            method: 'put', //setting the CRUD method to "update" for the route
+            headers: {'Content-Type': 'application/json'}, //sepcifying the type of content expected, which is JSON
+            body: JSON.stringify({ //declare the message content being passed, stringify the content
+                'itemFromJS': itemText //setting the content of the body to the inner 
+            }) //closing the body
+          }) //closing the object
+        const data = await response.json() //waiting on JSON from the response
+        console.log(data) //log the result to the console
+        location.reload() //reloads the page to update what is displayed
 
-    }catch(err){
-        console.log(err)
-    }
-}
+    }catch(err){ //if an error occurs, pass the error into the catch block
+        console.log(err) //log the error to the console
+    } //close the catch block
+} //end the function
