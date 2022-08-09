57db83adf222ccca603ec624c5d1711daced3a93
Catalin <csiegling@gmail.com>
2022-08-09 18:21:22 -0500

    commented

--- public/js/main.js
+++ public/js/main.js
@@ -1,72 +1,72 @@
-const deleteBtn = document.querySelectorAll('.fa-trash')
-const item = document.querySelectorAll('.item span')
-const itemCompleted = document.querySelectorAll('.item span.completed')
+const deleteBtn = document.querySelectorAll('.fa-trash') //delete button
+const item = document.querySelectorAll('.item span')    //item text
+const itemCompleted = document.querySelectorAll('.item span.completed') //item text
 
-Array.from(deleteBtn).forEach((element)=>{
-    element.addEventListener('click', deleteItem)
-})
+Array.from(deleteBtn).forEach((element)=>{ //for each delete button
+    element.addEventListener('click', deleteItem) //add event listener
+}) //end for each delete button
 
-Array.from(item).forEach((element)=>{
-    element.addEventListener('click', markComplete)
-})
+Array.from(item).forEach((element)=>{ //for each item
+    element.addEventListener('click', markComplete) //add event listener
+}) //end for each item
 
-Array.from(itemCompleted).forEach((element)=>{
-    element.addEventListener('click', markUnComplete)
-})
+Array.from(itemCompleted).forEach((element)=>{  //for each item
+    element.addEventListener('click', markUnComplete) //add event listener
+}) //end for each item
 
-async function deleteItem(){
-    const itemText = this.parentNode.childNodes[1].innerText
-    try{
-        const response = await fetch('deleteItem', {
-            method: 'delete',
-            headers: {'Content-Type': 'application/json'},
-            body: JSON.stringify({
-              'itemFromJS': itemText
-            })
-          })
-        const data = await response.json()
-        console.log(data)
-        location.reload()
+async function deleteItem(){ //delete item
+    const itemText = this.parentNode.childNodes[1].innerText //get item text
+    try{ //try to delete item
+        const response = await fetch('deleteItem', { //fetch delete item
+            method: 'delete', //set method to delete
+            headers: {'Content-Type': 'application/json'}, //set headers
+            body: JSON.stringify({ //set body
+              'itemFromJS': itemText //set item text
+            }) //end body
+          }) //end fetch
+        const data = await response.json() //get response
+        console.log(data) //log response
+        location.reload() //reload page
 
-    }catch(err){
-        console.log(err)
-    }
-}
+    }catch(err){ //catch error
+        console.log(err) //log error
+    } //end try
+} //end delete item
 
-async function markComplete(){
-    const itemText = this.parentNode.childNodes[1].innerText
-    try{
-        const response = await fetch('markComplete', {
-            method: 'put',
-            headers: {'Content-Type': 'application/json'},
-            body: JSON.stringify({
-                'itemFromJS': itemText
-            })
-          })
-        const data = await response.json()
-        console.log(data)
-        location.reload()
+async function markComplete(){ //mark item as complete
+    const itemText = this.parentNode.childNodes[1].innerText //get item text
+    try{ //try to mark item as complete
+        const response = await fetch('markComplete', { //fetch mark item as complete
+            method: 'put', //set method to put
+            headers: {'Content-Type': 'application/json'}, //set headers
+            body: JSON.stringify({ //set body
+                'itemFromJS': itemText //set item text
+            }) //end body
+          }) //end fetch
+        const data = await response.json() //get response
+        console.log(data) //log response
+        location.reload() //reload page
 
-    }catch(err){
-        console.log(err)
-    }
-}
+    }catch(err){ //catch error
+        console.log(err) //log error
+    } //end try
+} //end mark item as complete
 
-async function markUnComplete(){
-    const itemText = this.parentNode.childNodes[1].innerText
-    try{
-        const response = await fetch('markUnComplete', {
-            method: 'put',
-            headers: {'Content-Type': 'application/json'},
-            body: JSON.stringify({
-                'itemFromJS': itemText
-            })
-          })
-        const data = await response.json()
-        console.log(data)
-        location.reload()
+async function markUnComplete(){ //mark item as uncomplete
+    const itemText = this.parentNode.childNodes[1].innerText    //get item text
+    try{  //try to mark item as uncomplete
+        const response = await fetch('markUnComplete', { //fetch mark item as uncomplete
+            method: 'put', //set method to put
+            headers: {'Content-Type': 'application/json'}, //set headers
+            body: JSON.stringify({ //set body
+                'itemFromJS': itemText //set item text
+            }) //end body
+          }) //end fetch
+        const data = await response.json() //get response
+        console.log(data)   //log response
+        location.reload() //reload page
 
-    }catch(err){
-        console.log(err)
-    }
-}
+    }catch(err){ //catch error
+        console.log(err) //log error
+    } //end try
+} //end mark item as uncomplete
--- server.js
+++ server.js
@@ -1,30 +1,30 @@
-const express = require('express')
-const app = express()
-const MongoClient = require('mongodb').MongoClient
-const PORT = 2121
-require('dotenv').config()
+const express = require('express') // Import express
+const app = express() // Create express app
+const MongoClient = require('mongodb').MongoClient // Import mongo client
+const PORT = 2121 // Set port
+require('dotenv').config() // Import dotenv
 
 
-let db,
-    dbConnectionStr = process.env.DB_STRING,
-    dbName = 'todo'
+let db,  // Declare db and collection
+    dbConnectionStr = process.env.DB_STRING, // Set db connection string
+    dbName = 'todo' // Set db name
 
-MongoClient.connect(dbConnectionStr, { useUnifiedTopology: true })
-    .then(client => {
-        console.log(`Connected to ${dbName} Database`)
-        db = client.db(dbName)
-    })
+MongoClient.connect(dbConnectionStr, { useUnifiedTopology: true }) // Connect to db
+    .then(client => { // If connected
+        console.log(`Connected to ${dbName} Database`) // Log connection
+        db = client.db(dbName) // Set db
+    }) // End if connected
     
-app.set('view engine', 'ejs')
-app.use(express.static('public'))
-app.use(express.urlencoded({ extended: true }))
-app.use(express.json())
+app.set('view engine', 'ejs') // Set view engine
+app.use(express.static('public')) // Set static folder
+app.use(express.urlencoded({ extended: true })) // Set extended true
+app.use(express.json()) // Set json true
 
 
-app.get('/',async (request, response)=>{
-    const todoItems = await db.collection('todos').find().toArray()
-    const itemsLeft = await db.collection('todos').countDocuments({completed: false})
-    response.render('index.ejs', { items: todoItems, left: itemsLeft })
+app.get('/',async (request, response)=>{ // Get /
+    const todoItems = await db.collection('todos').find().toArray() // Get todo items
+    const itemsLeft = await db.collection('todos').countDocuments({completed: false}) // Get items left
+    response.render('index.ejs', { items: todoItems, left: itemsLeft }) // Render index.ejs
     // db.collection('todos').find().toArray()
     // .then(data => {
     //     db.collection('todos').countDocuments({completed: false})
@@ -35,59 +35,59 @@ app.get('/',async (request, response)=>{
     // .catch(error => console.error(error))
 })
 
-app.post('/addTodo', (request, response) => {
-    db.collection('todos').insertOne({thing: request.body.todoItem, completed: false})
-    .then(result => {
-        console.log('Todo Added')
-        response.redirect('/')
-    })
-    .catch(error => console.error(error))
+app.post('/addTodo', (request, response) => { // Post /addTodo
+    db.collection('todos').insertOne({thing: request.body.todoItem, completed: false}) // Insert todo item
+    .then(result => { // If inserted
+        console.log('Todo Added')   // Log insert
+        response.redirect('/') // Redirect to /
+    }) // End if inserted
+    .catch(error => console.error(error)) // Catch error
 })
 
-app.put('/markComplete', (request, response) => {
-    db.collection('todos').updateOne({thing: request.body.itemFromJS},{
+app.put('/markComplete', (request, response) => { // Put /markComplete
+    db.collection('todos').updateOne({thing: request.body.itemFromJS},{  // Update todo item
+        $set: { 
+            completed: true // Set completed to true
+          } 
+    },{ 
+        sort: {_id: -1}, // Sort by id
+        upsert: false // Do not insert if not found
+    })
+    .then(result => { // If updated
+        console.log('Marked Complete') // Log update
+        response.json('Marked Complete') // Send json
+    })
+    .catch(error => console.error(error)) // Catch error
+
+})
+
+app.put('/markUnComplete', (request, response) => { // Put /markUnComplete
+    db.collection('todos').updateOne({thing: request.body.itemFromJS},{ // Update todo item
         $set: {
-            completed: true
+            completed: false // Set completed to false
           }
     },{
-        sort: {_id: -1},
-        upsert: false
+        sort: {_id: -1}, // Sort by id
+        upsert: false // Do not insert if not found
     })
-    .then(result => {
-        console.log('Marked Complete')
-        response.json('Marked Complete')
+    .then(result => { // If updated
+        console.log('Marked Complete') // Log update
+        response.json('Marked Complete') // Send json
     })
-    .catch(error => console.error(error))
+    .catch(error => console.error(error)) // Catch error
 
 })
 
-app.put('/markUnComplete', (request, response) => {
-    db.collection('todos').updateOne({thing: request.body.itemFromJS},{
-        $set: {
-            completed: false
-          }
-    },{
-        sort: {_id: -1},
-        upsert: false
+app.delete('/deleteItem', (request, response) => { // Delete /deleteItem
+    db.collection('todos').deleteOne({thing: request.body.itemFromJS}) // Delete todo item
+    .then(result => { // If deleted
+        console.log('Todo Deleted') // Log delete
+        response.json('Todo Deleted') // Send json
     })
-    .then(result => {
-        console.log('Marked Complete')
-        response.json('Marked Complete')
-    })
-    .catch(error => console.error(error))
+    .catch(error => console.error(error)) // Catch error
 
 })
 
-app.delete('/deleteItem', (request, response) => {
-    db.collection('todos').deleteOne({thing: request.body.itemFromJS})
-    .then(result => {
-        console.log('Todo Deleted')
-        response.json('Todo Deleted')
-    })
-    .catch(error => console.error(error))
-
-})
-
-app.listen(process.env.PORT || PORT, ()=>{
-    console.log(`Server running on port ${PORT}`)
+app.listen(process.env.PORT || PORT, ()=>{ // Listen on port
+    console.log(`Server running on port ${PORT}`) // Log port
 })
