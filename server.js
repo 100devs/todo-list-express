64e16ac7dc800bb0c033f8efd2f9f9ed57a403a3
Rebecca Key <rek990@gmail.com>
2022-08-10 06:11:36 -0400

    Added more comments to the app.

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ MongoClient.connect(dbConnectionStr, { useUnifiedTopology: true })
   .then((client) => {
     // logging message to console that the user is connected to the 'todo' database
     console.log(`Connected to ${dbName} Database`);
-    // reassigning variable to access the db in MongoDB (here, it's 'todo')
+    // reassigning variable to access the db in MongoDB (here, it's 'todo') holds connection to the MongoDB database
     db = client.db(dbName);
   });
 
@@ -35,7 +35,7 @@ app.use(express.urlencoded({ extended: true }));
 // built-in middleware that allows for the parsing of incoming requests with JSON payloads
 app.use(express.json());
 
-// GET request for the home page
+// GET request for the home page (root page, as it's "/")
 app.get("/", async (request, response) => {
   // goes into the 'todos' collection of the db, finds documents therein, and puts them into an array
   const todoItems = await db.collection("todos").find().toArray();
@@ -43,37 +43,48 @@ app.get("/", async (request, response) => {
   const itemsLeft = await db
     .collection("todos")
     .countDocuments({ completed: false });
-  // transfers data (todoItems and itemsLeft) into the EJS template and responds with HTML that appears on the UI
+  // transfers data (todoItems and itemsLeft, both arrays of objects) into the EJS template and responds with HTML that appears on the UI
   response.render("index.ejs", { items: todoItems, left: itemsLeft });
   // see above in app.get(): this is the code to get the number of items remaining on the todo list; transferring data (todoItems and itemsLeft) into the EJS template and responding with HTML that appears on the UI; and logging any errors to the console, all without using async/await
 
+  // goes into the 'todos' collection of the db, finds documents therein, and puts them into an array (returns a promise)
   // db.collection('todos').find().toArray()
+
+  // passed the array into the parameter of data (i.e., data is an array of the three todo objects)
+  // goes into the 'todos' collection of the db, counts the number of documents where completed is false, and assigns that number to the variable named itemsLeft
+
   // .then(data => {
   //     db.collection('todos').countDocuments({completed: false})
   //     .then(itemsLeft => {
+  // pass data (arr of objects/documents) into ejs template and giving data the name "items", responds with HTML, and HTML gets rendered
   //         response.render('index.ejs', { items: data, left: itemsLeft })
   //     })
   // })
   // .catch(error => console.error(error))
 });
 
-// POST request to /addTodo path
+// POST request to /addTodo path; route comes from action on the form that made the POST request (action="/addTodo")
 app.post("/addTodo", (request, response) => {
-  // go to db, find 'todos' collection, insert a new task (thing) into the document, and mark it is not completed (i.e., false)
+  console.log(request);
+  console.log(request.body);
+  // go to db, find 'todos' collection, insert a new task (thing, a property) into the document, and mark it is not completed (i.e., false)
+  // "completed" and "thing" are properties in the documents, which are all in the collection
+  // todoItem comes from name attribute from <input/> (name="todoItem")
+  // "false" is hardcoded value for completed property
   db.collection("todos")
     .insertOne({ thing: request.body.todoItem, completed: false })
     // promise chain
     .then((result) => {
       // logs "Todo Added" into the console
       console.log("Todo Added");
-      // responds via redirect back to homepage, which displays the newly added thing on the UI
+      // responds via redirect back to homepage, triggering GET request, which displays the newly added thing on the UI
       response.redirect("/");
     })
     // logs errors from POST request to the console (if any)
     .catch((error) => console.error(error));
 });
 
-// PUT request to /markComplete path 
+// PUT request to /markComplete path
 app.put("/markComplete", (request, response) => {
   // go to db, gets the 'todos' collection, finds the value of thing (itemFromJS) and updates it
   db.collection("todos")
@@ -103,7 +114,7 @@ app.put("/markComplete", (request, response) => {
     .catch((error) => console.error(error));
 });
 
-// PUT request to /markUnComplete path 
+// PUT request to /markUnComplete path
 app.put("/markUnComplete", (request, response) => {
   // go to db, gets the 'todos' collection, finds the value of thing (itemFromJS) and updates it
   db.collection("todos")
