cfa62f2d3a3c50fef84a3f0b1a351b0842ac0718
Kyle Clemente <kclemente11@me.com>
2022-08-11 18:04:19 -0500

    added more to server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 const MongoClient = require('mongodb').MongoClient
-const PORT = 2121
+const PORT = 2121 // set the server port to 2121
 require('dotenv').config()
 
 
@@ -22,9 +22,9 @@ app.use(express.json())
 
 
 app.get('/',async (request, response)=>{
-    const todoItems = await db.collection('todos').find().toArray()
+    const todoItems = await db.collection('todos').find().toArray() //got the todo items from the database and put into an array
     const itemsLeft = await db.collection('todos').countDocuments({completed: false})
-    response.render('index.ejs', { items: todoItems, left: itemsLeft })
+    response.render('index.ejs', { items: todoItems, left: itemsLeft }) //sending data to the index.ejs file, passing the array into "items"
     // db.collection('todos').find().toArray()
     // .then(data => {
     //     db.collection('todos').countDocuments({completed: false})
@@ -35,13 +35,13 @@ app.get('/',async (request, response)=>{
     // .catch(error => console.error(error))
 })
 
-app.post('/addTodo', (request, response) => {
-    db.collection('todos').insertOne({thing: request.body.todoItem, completed: false})
-    .then(result => {
-        console.log('Todo Added')
-        response.redirect('/')
+app.post('/addTodo', (request, response) => { // /addTodo comes from action on form that makes the post request, starts post.
+    db.collection('todos').insertOne({thing: request.body.todoItem, completed: false}) //find the todo collection on mongodb and we simply insert an "object" into the db with our specified property with the name of thing
+    .then(result => { //
+        console.log('Todo Added') //console logs "Todo Added"
+        response.redirect('/') // refreshes the page
     })
-    .catch(error => console.error(error))
+    .catch(error => console.error(error)) //console logs error if error occurs
 })
 
 app.put('/markComplete', (request, response) => {
