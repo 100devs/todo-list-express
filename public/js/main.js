d68e934ad14152f7e01cbc059a86c823f6b4ca34
Kyle Clemente <kclemente11@me.com>
2022-08-11 21:32:04 -0500

    more changes to server and main.js

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,5 @@
 const deleteBtn = document.querySelectorAll('.fa-trash')
-const item = document.querySelectorAll('.item span')
+const item = document.querySelectorAll('.item span') //the item is the span
 const itemCompleted = document.querySelectorAll('.item span.completed')
 
 Array.from(deleteBtn).forEach((element)=>{
@@ -7,45 +7,45 @@ Array.from(deleteBtn).forEach((element)=>{
 })
 
 Array.from(item).forEach((element)=>{
-    element.addEventListener('click', markComplete)
+    element.addEventListener('click', markComplete) //gave every .item span the ability to markComplete
 })
 
 Array.from(itemCompleted).forEach((element)=>{
-    element.addEventListener('click', markUnComplete)
+    element.addEventListener('click', markUnComplete)//gave every .item span the ability to markUnComplete
 })
 
 async function deleteItem(){
-    const itemText = this.parentNode.childNodes[1].innerText
+    const itemText = this.parentNode.childNodes[1].innerText //parent node == li child node == the span and then grab the text in the span
     try{
         const response = await fetch('deleteItem', {
-            method: 'delete',
+            method: 'delete', //delete method
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
-              'itemFromJS': itemText
+              'itemFromJS': itemText //the string in the span
             })
           })
         const data = await response.json()
-        console.log(data)
-        location.reload()
+        console.log(data)//logs the data in the console
+        location.reload() //reload the page
 
     }catch(err){
         console.log(err)
     }
 }
 
-async function markComplete(){
-    const itemText = this.parentNode.childNodes[1].innerText
+async function markComplete(){ //starts an asynchronus function of markComplete
+    const itemText = this.parentNode.childNodes[1].innerText //grabbing text on the todo
     try{
-        const response = await fetch('markComplete', {
-            method: 'put',
+        const response = await fetch('markComplete', { //made request to markComplete
+            method: 'put', //with a put request
             headers: {'Content-Type': 'application/json'},
-            body: JSON.stringify({
-                'itemFromJS': itemText
+            body: JSON.stringify({ //adds it as a string JSON 
+                'itemFromJS': itemText //itemFromJS is the text
             })
           })
         const data = await response.json()
-        console.log(data)
-        location.reload()
+        console.log(data)//logs the data in the console
+        location.reload() //refresh the page which triggers a get request
 
     }catch(err){
         console.log(err)
--- server.js
+++ server.js
@@ -1,8 +1,8 @@
-const express = require('express')
+const express = require('express')//makes sure express is installed
 const app = express()
-const MongoClient = require('mongodb').MongoClient
+const MongoClient = require('mongodb').MongoClient //requiring mongoclient and mongodb
 const PORT = 2121 // set the server port to 2121
-require('dotenv').config()
+require('dotenv').config()//requring our env file 
 
 
 let db,
@@ -14,7 +14,7 @@ MongoClient.connect(dbConnectionStr, { useUnifiedTopology: true })
         console.log(`Connected to ${dbName} Database`)
         db = client.db(dbName)
     })
-    
+    //lego brick ^!!
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
 app.use(express.urlencoded({ extended: true }))
@@ -44,18 +44,18 @@ app.post('/addTodo', (request, response) => { // /addTodo comes from action on f
     .catch(error => console.error(error)) //console logs error if error occurs
 })
 
-app.put('/markComplete', (request, response) => {
-    db.collection('todos').updateOne({thing: request.body.itemFromJS},{
+app.put('/markComplete', (request, response) => { //start put method
+    db.collection('todos').updateOne({thing: request.body.itemFromJS},{//update the db collection of todos with the itemFromJS
         $set: {
-            completed: true
+            completed: true //sets completed in the db to true
           }
     },{
         sort: {_id: -1},
-        upsert: false
+        upsert: false //if true, it will update the document for you if it cannot find what you are trying to update. for ex. get duck
     })
     .then(result => {
-        console.log('Marked Complete')
-        response.json('Marked Complete')
+        console.log('Marked Complete') //console logs Marked Complete
+        response.json('Marked Complete') //responds with json Marked Complete
     })
     .catch(error => console.error(error))
 
