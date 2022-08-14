abd651d8ea8ac4a55770a46a8320260c93de3d2f
Ticia Francisco <ticiafrancisco@gmail.com>
2022-08-14 16:24:13 -0700

    Update comments, env PORT

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require('express')  //import express package
 const app = express()   //initialise express package into app variable
 const MongoClient = require('mongodb').MongoClient  //import mongodb and client
-const PORT = 2121   //add port into a variable
 require('dotenv').config()  //import .env to use environment variables
 
 
@@ -22,24 +21,24 @@ app.use(express.json()) //settings to enable json
 
 
 app.get('/',async (request, response)=>{    //client requests route page (root) to send back or throw error - uses async/await
-    const todoItems = await db.collection('todos').find().toArray()     //find to-do items in collection and add to array
+    /*const todoItems = await db.collection('todos').find().toArray()     //find to-do items in collection and add to array
     const itemsLeft = await db.collection('todos').countDocuments({completed: false})   //count number for items where completed === false
-    response.render('index.ejs', { items: todoItems, left: itemsLeft })     //render ejs file to show todoItems and itemsLeft
-    // db.collection('todos').find().toArray()
-    // .then(data => {
-    //     db.collection('todos').countDocuments({completed: false})
-    //     .then(itemsLeft => {
-    //         response.render('index.ejs', { items: data, left: itemsLeft })
-    //     })
-    // })
-    // .catch(error => console.error(error))
+    response.render('index.ejs', { items: todoItems, left: itemsLeft })     //render ejs file to show todoItems and itemsLeft*/
+    db.collection('todos').find().toArray() //find to-do items in collection and add to array
+    .then(data => {
+        db.collection('todos').countDocuments({completed: false})   //count number of items in to-do collection where completed === false
+        .then(itemsLeft => {    //include itemsLeft
+        response.render('index.ejs', { items: data, left: itemsLeft })  //render on page using ejs file: data from above and itemsLeft
+        })
+    })
+    .catch(error => console.error(error))
 })
 
 app.post('/addTodo', (request, response) => {   //client requests route page (addTodo) to add items
     db.collection('todos').insertOne({thing: request.body.todoItem, completed: false})  //add an item to the todos collection with completed status false
     .then(result => {
         console.log('Todo Added')   //show success message in console log
-        response.redirect('/')  //after post request, redirect to root
+        response.redirect('/')  //after post request, redirect to root (triggers get request)
     })
     .catch(error => console.error(error))   //throw error in console if request unsuccessful
 })
@@ -89,5 +88,5 @@ app.delete('/deleteItem', (request, response) => {  //client requests route (del
 })
 
 app.listen(process.env.PORT || PORT, ()=>{  //tell express to listen on specified port
-    console.log(`Server running on port ${PORT}`)   //show server running message in console log
+    console.log(`Server running on port ${process.env.PORT}`)   //show server running message in console log
 })
