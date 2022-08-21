563fa61cb1bcb8f752218999b2da719fce29dbee
Donny Duncan <donnyduncan@gmail.com>
2022-08-21 15:42:19 -0500

    8/21/2022 - Full comments added to each line
    Signed-off-by: Donny Duncan <donnyduncan@gmail.com>

--- public/js/main.js
+++ public/js/main.js
@@ -1,72 +1,72 @@
-const deleteBtn = document.querySelectorAll('.fa-trash')
-const item = document.querySelectorAll('.item span')
-const itemCompleted = document.querySelectorAll('.item span.completed')
+const deleteBtn = document.querySelectorAll('.fa-trash') // Set a variable for our delete button code
+const item = document.querySelectorAll('.item span') // Set a variable for the item to target
+const itemCompleted = document.querySelectorAll('.item span.completed') // Set a variable for the completed items
 
-Array.from(deleteBtn).forEach((element)=>{
-    element.addEventListener('click', deleteItem)
+Array.from(deleteBtn).forEach((element)=>{ // Loop through all of the delete buttons and do the following
+    element.addEventListener('click', deleteItem) // Add an event listener to each of them
 })
 
-Array.from(item).forEach((element)=>{
-    element.addEventListener('click', markComplete)
+Array.from(item).forEach((element)=>{ // Loop through all of the items and do the following
+    element.addEventListener('click', markComplete) // Add an event listener to each of them
 })
 
-Array.from(itemCompleted).forEach((element)=>{
-    element.addEventListener('click', markUnComplete)
+Array.from(itemCompleted).forEach((element)=>{ // Loop through all of the completed items and do the following
+    element.addEventListener('click', markUnComplete) // Add an event listener to each of them
 })
 
-async function deleteItem(){
-    const itemText = this.parentNode.childNodes[1].innerText
+async function deleteItem(){ // Create a function that does the following
+    const itemText = this.parentNode.childNodes[1].innerText // Get the text of the selected item
     try{
-        const response = await fetch('deleteItem', {
+        const response = await fetch('deleteItem', { // Send a request to delete the item from the DB
             method: 'delete',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
               'itemFromJS': itemText
             })
           })
-        const data = await response.json()
-        console.log(data)
+        const data = await response.json() // Wait for a response from the server
+        console.log(data) // Console log the response
         location.reload()
 
     }catch(err){
-        console.log(err)
+        console.log(err) // catch any errors and console log them if detected
     }
 }
 
-async function markComplete(){
-    const itemText = this.parentNode.childNodes[1].innerText
+async function markComplete(){  // Create a function that does the following
+    const itemText = this.parentNode.childNodes[1].innerText // Get the text of the selected item
     try{
-        const response = await fetch('markComplete', {
+        const response = await fetch('markComplete', { // Send a request to update the item in the DB
             method: 'put',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
                 'itemFromJS': itemText
             })
           })
-        const data = await response.json()
-        console.log(data)
+        const data = await response.json() // Wait for a response from the server
+        console.log(data) // Console log the response
         location.reload()
 
     }catch(err){
-        console.log(err)
+        console.log(err) // Catch any errors and console log them if detected
     }
 }
 
-async function markUnComplete(){
-    const itemText = this.parentNode.childNodes[1].innerText
+async function markUnComplete(){ // Create a function that does the following
+    const itemText = this.parentNode.childNodes[1].innerText // Get the text of the selected item
     try{
-        const response = await fetch('markUnComplete', {
+        const response = await fetch('markUnComplete', { // Send a request to update the item in the DB
             method: 'put',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
                 'itemFromJS': itemText
             })
           })
-        const data = await response.json()
-        console.log(data)
+        const data = await response.json() // Wait for a response from the server
+        console.log(data) // Console log the response
         location.reload()
 
     }catch(err){
-        console.log(err)
+        console.log(err) // Catch any errors and console log them if detected
     }
 }
