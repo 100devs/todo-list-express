cd91a9ce18bbd0ad865b2689484d53e3a8f48cfc
Mariana West Hartenthal <97693233+marianaWest@users.noreply.github.com>
2022-09-12 20:10:18 +0100

    comment server.js

--- server.js
+++ server.js
@@ -1,30 +1,31 @@
-const express = require('express')
-const app = express()
-const MongoClient = require('mongodb').MongoClient
-const PORT = 2121
-require('dotenv').config()
+const express = require('express') // make it possible to use express in this file
+const app = express() // sets a variable and assigns it to the instance of express
+const MongoClient = require('mongodb').MongoClient // makes it possible to use methods associated with Mongoclient and talk to our DB
+const PORT = 2121 // sets constant to define the location where our server will be listening 
+require('dotenv').config() // allows us to access variables inside .env file
 
 
-let db,
-    dbConnectionStr = process.env.DB_STRING,
-    dbName = 'todo'
+let db, // declares variable
+    dbConnectionStr = process.env.DB_STRING, // declares variable assigned to our database connection string to it
+    dbName = 'todo' // declares variable assigning the name of the database we will use 
 
-MongoClient.connect(dbConnectionStr, { useUnifiedTopology: true })
-    .then(client => {
-        console.log(`Connected to ${dbName} Database`)
-        db = client.db(dbName)
-    })
+MongoClient.connect(dbConnectionStr, { useUnifiedTopology: true }) // creates connection to MongoDB and passes in our connection string
+    .then(client => { // if the connection is succesful, do what follows. it passes in all the client information
+        console.log(`Connected to ${dbName} Database`) // logs to the console a template literal connected to todo database
+        db = client.db(dbName) // assigns value to previously declared db variable that contains a db client factory method
+    }) // closes our .then
     
-app.set('view engine', 'ejs')
-app.use(express.static('public'))
-app.use(express.urlencoded({ extended: true }))
-app.use(express.json())
+    //middleware
+app.set('view engine', 'ejs') // sets ejs as the default render
+app.use(express.static('public')) // sets location for static assets
+app.use(express.urlencoded({ extended: true })) // tells express to decade and encode urls where the header matches the content
+app.use(express.json()) // parses JSON content from incoming requests
 
 
-app.get('/',async (request, response)=>{
-    const todoItems = await db.collection('todos').find().toArray()
-    const itemsLeft = await db.collection('todos').countDocuments({completed: false})
-    response.render('index.ejs', { items: todoItems, left: itemsLeft })
+app.get('/',async (request, response)=>{ // handles a GET method when the root route is passed in, sets up request and response parameters
+    const todoItems = await db.collection('todos').find().toArray() // sets a variable and awaits ALL items from the todos collection
+    const itemsLeft = await db.collection('todos').countDocuments({completed: false}) // sets a variable and awaits a count of uncompleted items to later display in EJS
+    response.render('index.ejs', { items: todoItems, left: itemsLeft }) //renders the EJS file and passes through the db items and the count remaining inside of an object 
     // db.collection('todos').find().toArray()
     // .then(data => {
     //     db.collection('todos').countDocuments({completed: false})
