fd7c380af861fc4f2b3742ed43af1031b5b8b9ae
Tee O <tdopress@gmail.com>
2022-10-03 16:57:01 -0400

    added comments

--- server.js
+++ server.js
@@ -1,23 +1,33 @@
+//Includes express module
 const express = require('express')
+//intialize express app
 const app = express()
+//Initialize mongodb client - this will allow us to connect to database
 const MongoClient = require('mongodb').MongoClient
+//define port
 const PORT = 2121
+//configure dotenv module for environment variables
 require('dotenv').config()
 
 
 let db,
     dbConnectionStr = process.env.DB_STRING,
     dbName = 'todo'
-
+//Allows us to connect to the database by passing in the connection string
 MongoClient.connect(dbConnectionStr, { useUnifiedTopology: true })
     .then(client => {
         console.log(`Connected to ${dbName} Database`)
+        //Store database in a variable
         db = client.db(dbName)
     })
-    
+
+//Sets view engine to ejs, this allows us to use ejs as our templating engine for rendering HTML
 app.set('view engine', 'ejs')
+//Defines public as a static folder - sets the place where app will retrieve all static files
 app.use(express.static('public'))
+//parse the incoming request with urlencoded values like the querystring - does the same thing that body parser does when processing the JSON data and converting it to JS object within the body property
 app.use(express.urlencoded({ extended: true }))
+//Parses JSON
 app.use(express.json())
 
 
@@ -35,22 +45,29 @@ app.get('/',async (request, response)=>{
     // .catch(error => console.error(error))
 })
 
+//create todo task document in database
 app.post('/addTodo', (request, response) => {
+    //inserts new todo document within the todos collection
     db.collection('todos').insertOne({thing: request.body.todoItem, completed: false})
     .then(result => {
         console.log('Todo Added')
+        //redirect to home
         response.redirect('/')
     })
     .catch(error => console.error(error))
 })
 
+//update todo task by marking it complete
 app.put('/markComplete', (request, response) => {
     db.collection('todos').updateOne({thing: request.body.itemFromJS},{
         $set: {
+            //sets completed property to true
             completed: true
           }
     },{
+        //sorts in order
         sort: {_id: -1},
+        //doesnt create a new document if one doesnt exist
         upsert: false
     })
     .then(result => {
@@ -61,6 +78,7 @@ app.put('/markComplete', (request, response) => {
 
 })
 
+//marks todo document as incomplete
 app.put('/markUnComplete', (request, response) => {
     db.collection('todos').updateOne({thing: request.body.itemFromJS},{
         $set: {
@@ -77,7 +95,7 @@ app.put('/markUnComplete', (request, response) => {
     .catch(error => console.error(error))
 
 })
-
+//deltes todo document from database
 app.delete('/deleteItem', (request, response) => {
     db.collection('todos').deleteOne({thing: request.body.itemFromJS})
     .then(result => {
@@ -88,6 +106,7 @@ app.delete('/deleteItem', (request, response) => {
 
 })
 
+//configures the application to be listening on specified port
 app.listen(process.env.PORT || PORT, ()=>{
     console.log(`Server running on port ${PORT}`)
 })
