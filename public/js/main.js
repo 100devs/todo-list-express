8c3bc6a16a36731c04ee005278fb8c27cde32070
Anthony <anthonychen2410@gmail.com>
2022-11-04 20:46:09 -0400

    added comments

--- public/js/main.js
+++ public/js/main.js
@@ -1,7 +1,11 @@
+// all delete buttons 
 const deleteBtn = document.querySelectorAll('.fa-trash')
+// all span elements with each item 
 const item = document.querySelectorAll('.item span')
+// all span items marked as completed by checking if they have the '.completed' css class
 const itemCompleted = document.querySelectorAll('.item span.completed')
 
+// Each NodeList is converted to an array, then within a forEach loop, each element has a click event assigned with a callback function
 Array.from(deleteBtn).forEach((element)=>{
     element.addEventListener('click', deleteItem)
 })
@@ -14,9 +18,12 @@ Array.from(itemCompleted).forEach((element)=>{
     element.addEventListener('click', markUnComplete)
 })
 
+// function to submit a delete request to the server
 async function deleteItem(){
+    // the text of the tiem whose delete button was clicked
     const itemText = this.parentNode.childNodes[1].innerText
     try{
+        // delete request sent to the server as a json converted to a string
         const response = await fetch('deleteItem', {
             method: 'delete',
             headers: {'Content-Type': 'application/json'},
@@ -24,6 +31,7 @@ async function deleteItem(){
               'itemFromJS': itemText
             })
           })
+        // server response is converted to json and page refreshes
         const data = await response.json()
         console.log(data)
         location.reload()
@@ -33,9 +41,12 @@ async function deleteItem(){
     }
 }
 
+// function to mark an item as complete on the server
 async function markComplete(){
+    // This is the text of the item whose mark-complete button was clicked.
     const itemText = this.parentNode.childNodes[1].innerText
     try{
+         // The edit/put request sent to the server so that this item can be marked as complete.
         const response = await fetch('markComplete', {
             method: 'put',
             headers: {'Content-Type': 'application/json'},
@@ -43,6 +54,7 @@ async function markComplete(){
                 'itemFromJS': itemText
             })
           })
+        // server response is converted to json and page refreshes
         const data = await response.json()
         console.log(data)
         location.reload()
@@ -52,9 +64,12 @@ async function markComplete(){
     }
 }
 
+// function to mark an item as incomplete on the server
 async function markUnComplete(){
+    // This is the text of the item whose "mark-uncomplete" button was clicked.
     const itemText = this.parentNode.childNodes[1].innerText
     try{
+        // The edit/put request sent to the server so that this item can be marked as incomplete
         const response = await fetch('markUnComplete', {
             method: 'put',
             headers: {'Content-Type': 'application/json'},
@@ -62,6 +77,7 @@ async function markUnComplete(){
                 'itemFromJS': itemText
             })
           })
+        // server response is converted to json and page refreshes
         const data = await response.json()
         console.log(data)
         location.reload()
--- server.js
+++ server.js
@@ -1,29 +1,41 @@
+// import express
 const express = require('express')
+// use express
 const app = express()
+// connect to mongo
 const MongoClient = require('mongodb').MongoClient
+// set port as a constant
 const PORT = 2121
+// able to use environement variables
 require('dotenv').config()
 
-
+// database connection string connected from environment variables
 let db,
     dbConnectionStr = process.env.DB_STRING,
     dbName = 'todo'
 
+// database connection
 MongoClient.connect(dbConnectionStr, { useUnifiedTopology: true })
     .then(client => {
         console.log(`Connected to ${dbName} Database`)
         db = client.db(dbName)
     })
-    
+
+// middleware using express and ejs as our view engine for rendering pages
 app.set('view engine', 'ejs')
+// serve up static files such as js css without needing to set routes for every file
 app.use(express.static('public'))
+// allows us to parse the body of a request as JSON
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 
-
+// handles home/root page get requests 
 app.get('/',async (request, response)=>{
+    // goes through todos collection, finds all documents and stores in an array with variable todoItems
     const todoItems = await db.collection('todos').find().toArray()
+    // same as above to get documents that have not been marked as completed stored as variable itemsLeft
     const itemsLeft = await db.collection('todos').countDocuments({completed: false})
+    // serve an ejs render of index.ejs as the response by passing in todoItems and itemsLeft
     response.render('index.ejs', { items: todoItems, left: itemsLeft })
     // db.collection('todos').find().toArray()
     // .then(data => {
@@ -35,25 +47,36 @@ app.get('/',async (request, response)=>{
     // .catch(error => console.error(error))
 })
 
+// handles post requests to the route /addToDo
 app.post('/addTodo', (request, response) => {
+    // inserts one document into the todos collection
+    // the inserted "thing" is in the request.body with a completed false property
     db.collection('todos').insertOne({thing: request.body.todoItem, completed: false})
     .then(result => {
+        // when inserted the page refreshes and triggers a get request with updated document
         console.log('Todo Added')
         response.redirect('/')
     })
     .catch(error => console.error(error))
 })
 
+// handles put requests to the /markComplete route
 app.put('/markComplete', (request, response) => {
+    // updates one document in the todos collection
+    // the query will be the itemFromJS from the request.body
     db.collection('todos').updateOne({thing: request.body.itemFromJS},{
+        // set "completed" to true for this document
         $set: {
             completed: true
           }
     },{
+        // sort the documents by their id in descending order
         sort: {_id: -1},
+        // no new document will be inserted if the document does not exist
         upsert: false
     })
     .then(result => {
+        // response sent back to client saying complete
         console.log('Marked Complete')
         response.json('Marked Complete')
     })
@@ -61,6 +84,7 @@ app.put('/markComplete', (request, response) => {
 
 })
 
+// Same as the mark complete route but this time to mark as incomplete
 app.put('/markUnComplete', (request, response) => {
     db.collection('todos').updateOne({thing: request.body.itemFromJS},{
         $set: {
@@ -78,7 +102,9 @@ app.put('/markUnComplete', (request, response) => {
 
 })
 
+// handles delete requests with the route 'deleteItem'
 app.delete('/deleteItem', (request, response) => {
+    // goes to that to the 'todos' collection and deletes the one document matching "itemFromJS" from the request body
     db.collection('todos').deleteOne({thing: request.body.itemFromJS})
     .then(result => {
         console.log('Todo Deleted')
@@ -88,6 +114,7 @@ app.delete('/deleteItem', (request, response) => {
 
 })
 
+// if enviornment variable has PORT property use that, otherwise use default PORT
 app.listen(process.env.PORT || PORT, ()=>{
     console.log(`Server running on port ${PORT}`)
 })
