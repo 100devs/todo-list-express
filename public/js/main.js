369405b5a1c1757f440cf8e9e0f8aa1549b5b801
Charles Errington <charleserringtondev@gmail.com>
2022-11-12 18:25:51 +0000

    added my comments

--- public/js/main.js
+++ public/js/main.js
@@ -1,31 +1,48 @@
+
+//These are the trash cans
 const deleteBtn = document.querySelectorAll('.fa-trash')
+//our client side event listeners are the only thing that can hear
+//us clicking on the items
 const item = document.querySelectorAll('.item span')
+//This listens for a click on a span with the class completed 
+//that is a child of an element with the class item
 const itemCompleted = document.querySelectorAll('.item span.completed')
 
+//this listens for a click on the trash cans and then runs deleteItem 
 Array.from(deleteBtn).forEach((element)=>{
     element.addEventListener('click', deleteItem)
 })
 
+//when an item  element is clicked the function markUnComplete runs
 Array.from(item).forEach((element)=>{
     element.addEventListener('click', markComplete)
 })
 
+//when an itemCompleted element is clicked the function markUnComplete runs
 Array.from(itemCompleted).forEach((element)=>{
     element.addEventListener('click', markUnComplete)
 })
 
 async function deleteItem(){
+    //this refers to the trash can 
+    // the parent node as the li
+    //The childNodes[1] is the span  
     const itemText = this.parentNode.childNodes[1].innerText
     try{
+        //a fetch is sent to the server, the fetch has the route deleteItem
         const response = await fetch('deleteItem', {
             method: 'delete',
             headers: {'Content-Type': 'application/json'},
+            //the fetch sends the innertext of the span to the server
+            //that is the itemText 
             body: JSON.stringify({
               'itemFromJS': itemText
             })
           })
         const data = await response.json()
         console.log(data)
+        //when our client side reseives the response from the server side
+        //it reloads the page
         location.reload()
 
     }catch(err){
@@ -34,17 +51,30 @@ async function deleteItem(){
 }
 
 async function markComplete(){
+    //this selects the text from the span that was clicked
+    //Child node one is actually the text in the span itself
+    //0 is like bullet points and things that take up space
     const itemText = this.parentNode.childNodes[1].innerText
     try{
+        //once the code hears the click we want to fetch 
+        //with the fetch we can describe what type of method we want
+        //it to be
+        // markComplete is our route
         const response = await fetch('markComplete', {
+            //the fetch is a put method
             method: 'put',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
+                //the item that was clicked will be refered to as itemFromJS 
+                //in the server request 
                 'itemFromJS': itemText
             })
           })
         const data = await response.json()
         console.log(data)
+        //once marked complete is sent from the server the page is reloaded
+        //triggering another get request which sees the changed 
+        //document in the database and so changes it in the DOM
         location.reload()
 
     }catch(err){
--- server.js
+++ server.js
@@ -1,30 +1,55 @@
+//This loads the express module, require is similar to import in other languages
 const express = require('express')
+// Calls the express function "express()" and puts new Express application inside 
+//the app variable (to start a new Express application). It's something like you 
+//are creating an object of a class. Where "express()" is just like class and 
+//app is it's newly created object.
 const app = express()
+//This loads the mongodb package, and we use mongoClient to talk to mongo 
 const MongoClient = require('mongodb').MongoClient
+//Sets the port as a variable so we can use that going forward
 const PORT = 2121
+//we are going to use our env files 
 require('dotenv').config()
 
-
+//this all connects to the database   
 let db,
+    //enviroment variables are used instead of the mongo connection string 
+    //the connection string is stored in the env file 
+    //this is so the mongo conection string doesn't get pushed up to github
+      
     dbConnectionStr = process.env.DB_STRING,
     dbName = 'todo'
 
 MongoClient.connect(dbConnectionStr, { useUnifiedTopology: true })
     .then(client => {
         console.log(`Connected to ${dbName} Database`)
+        //this is the actual database that we are connected to    
         db = client.db(dbName)
     })
     
+//we are using ejs as our templating language 
 app.set('view engine', 'ejs')
+//we are using our public folder to use all of our static files
 app.use(express.static('public'))
+//this does what body-parser does, we look at the requests coming through 
+//and allows us to get the data/ text out of the requests
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 
-
+//we are listening for a get request from the root route 
 app.get('/',async (request, response)=>{
+    //we are connecting to our database with db
+    //we find the collection in our database called todos
+    //find() returns all of the documents inside of the collection 
+    //We then put all of those documents found into an array
     const todoItems = await db.collection('todos').find().toArray()
+    //this gives us the number of documents that are not completed
     const itemsLeft = await db.collection('todos').countDocuments({completed: false})
-    response.render('index.ejs', { items: todoItems, left: itemsLeft })
+    //we are passing the returned objects into the template/ejs 
+    //We will not see todoItems, itemsLeft in the template 
+    //we will see items and left
+    response.render('index.ejs', { items: todoItems , left: itemsLeft })
     // db.collection('todos').find().toArray()
     // .then(data => {
     //     db.collection('todos').countDocuments({completed: false})
@@ -35,31 +60,52 @@ app.get('/',async (request, response)=>{
     // .catch(error => console.error(error))
 })
 
+//the route comes from the action on the form 
 app.post('/addTodo', (request, response) => {
+    //this allows us to see all the extra unimportant (to us) stuff
+    //that is in the request, vs the body 
+    console.log(request)
+    console.log(request.body)
+    //this takes the value from inside the input with the 'name' todoItem 
+    //and puts it into the mongodb database with property completed: false
+    //it connects to the database finds the collection called todos
+    //and then adds it
     db.collection('todos').insertOne({thing: request.body.todoItem, completed: false})
     .then(result => {
         console.log('Todo Added')
+        //Once this is completed it refreshes the page with a get request
+        //to the root, and now there will be an added item in the db
+        //that will be added to the dom
         response.redirect('/')
     })
     .catch(error => console.error(error))
 })
 
+//our fetch in main.js had a method of put
+//and a route of markComplete
 app.put('/markComplete', (request, response) => {
+    //itemFromJS was sent from main.js and refers to the text of the
+    //item that was clicked in the dom
     db.collection('todos').updateOne({thing: request.body.itemFromJS},{
+        //this will set completed to true for the clicked item
         $set: {
             completed: true
           }
     },{
         sort: {_id: -1},
+        //If upsert is true and you try to update something that is not there
+        //it will create the document for you
         upsert: false
     })
     .then(result => {
         console.log('Marked Complete')
+        //THis response is sent to main.js 
         response.json('Marked Complete')
     })
     .catch(error => console.error(error))
 
 })
+
 
 app.put('/markUnComplete', (request, response) => {
     db.collection('todos').updateOne({thing: request.body.itemFromJS},{
@@ -78,16 +124,22 @@ app.put('/markUnComplete', (request, response) => {
 
 })
 
+//our fetch in main.js had a method of delete
+//and a route of deleteItem
 app.delete('/deleteItem', (request, response) => {
+    //We go to our collection 'todos' and delete the document with
+    //the 'thing' that matches the inner text of the span 
     db.collection('todos').deleteOne({thing: request.body.itemFromJS})
     .then(result => {
         console.log('Todo Deleted')
+        //this responds back to client side (main.js )
         response.json('Todo Deleted')
     })
     .catch(error => console.error(error))
 
 })
-
+//were using our port, or once were on horoku we will use the environment variables
+ 
 app.listen(process.env.PORT || PORT, ()=>{
     console.log(`Server running on port ${PORT}`)
 })
