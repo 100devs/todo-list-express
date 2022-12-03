d26d72503082d02e2999f16ed544f16d94e0108a
Dominik Jones <dominikjo22@gmail.com>
2022-12-02 21:33:35 -0500

    comments

--- server.js
+++ server.js
@@ -14,16 +14,17 @@ let db,
     dbName = 'todo'
 //declaring a variable and assigning our database connection string to it
 
-MongoClient.connect(dbConnectionStr, { useUnifiedTopology: true })
-    .then(client => {
+MongoClient.connect(dbConnectionStr, { useUnifiedTopology: true }) //create a connection to mongodb and passing in our connection string.
+    .then(client => { //waiting for the connection and proceeeding if successful and passing in all the client information
         console.log(`Connected to ${dbName} Database`)
         db = client.db(dbName)
+        // assigning a value to previously declared db variable that contains a db client factory method
     })
     
-app.set('view engine', 'ejs')
-app.use(express.static('public'))
-app.use(express.urlencoded({ extended: true }))
-app.use(express.json())
+app.set('view engine', 'ejs') //sets ejs as the default render method
+app.use(express.static('public')) //sets the location for static assets
+app.use(express.urlencoded({ extended: true })) //tells express to decode and encode urls where the header matches the content. Supports arrays and objects
+app.use(express.json())// parses JSON content from incoming requests
 
 
 app.get('/',async (request, response)=>{//starts a GET method when the root route is passed in, sets up req and res parameters
@@ -42,56 +43,62 @@ app.get('/',async (request, response)=>{//starts a GET method when the root rout
 })
 
 app.post('/addTodo', (request, response) => {
-    db.collection('todos').insertOne({thing: request.body.todoItem, completed: false})
+    // starts a POST method when the add route is passed in
+    db.collection('todos').insertOne({thing: request.body.todoItem, completed: false})//inserts a new item into todos collection, gives it a completed value of false by default
     .then(result => {
         console.log('Todo Added')
-        response.redirect('/')
+        response.redirect('/') // gets rid of the /addTodo route, and redirects back to the homepage
     })
-    .catch(error => console.error(error))
+    .catch(error => console.error(error)) // catches error
 })
 
 app.put('/markComplete', (request, response) => {
-    db.collection('todos').updateOne({thing: request.body.itemFromJS},{
+    //starts a PUT method when the markComplete route is passed in
+    db.collection('todos').updateOne({thing: request.body.itemFromJS},{//look in the db for one item matching the name of the item passed in
         $set: {
-            completed: true
+            completed: true // set completed status to true
           }
     },{
-        sort: {_id: -1},
-        upsert: false
+        sort: {_id: -1}, //sorts item in desceding order
+        upsert: false //prevents insertion if item does not exist
     })
     .then(result => {
         console.log('Marked Complete')
+        //logging successful completion
         response.json('Marked Complete')
+        //sending a response back to the sender
     })
-    .catch(error => console.error(error))
+    .catch(error => console.error(error)) //catch errors
 
 })
 
-app.put('/markUnComplete', (request, response) => {
-    db.collection('todos').updateOne({thing: request.body.itemFromJS},{
+app.put('/markUnComplete', (request, response) => { 
+    // starts a PUT method when the markUnComplete route is passed in
+    db.collection('todos').updateOne({thing: request.body.itemFromJS}, //look in the db for one item matching the name // 
+    {
         $set: {
-            completed: false
+            completed: false // changes completed status to false
           }
     },{
-        sort: {_id: -1},
+        sort: {_id: -1}, //moves item to the bottom of the list
         upsert: false
     })
     .then(result => {
-        console.log('Marked Complete')
-        response.json('Marked Complete')
+        console.log('Marked Complete') //starts a then if update was successful
+        response.json('Marked Complete')//sending a response back to the sender
     })
     .catch(error => console.error(error))
-
+//catch errors
 })
 
 app.delete('/deleteItem', (request, response) => { //starts a delete method when the delete route is passed
     db.collection('todos').deleteOne({thing: request.body.itemFromJS})// look inside the todos collection for the one item that has a matching name
     .then(result => {
-        console.log('Todo Deleted')
-        response.json('Todo Deleted')
+        console.log('Todo Deleted')//logging successful completion
+        response.json('Todo Deleted')//sending a response back to the sender
     })
     .catch(error => console.error(error))
-
+//catch errors
 })
 
 //controls the enviornment and allows it to host
