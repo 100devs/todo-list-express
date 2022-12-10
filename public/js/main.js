d9c4fe307e0adeaae2fcb99e02cb95e67192fba7
Steven Ong <stevenong1619@gmail.com>
2022-12-09 17:45:16 -0800

    finish updating comments

--- public/js/main.js
+++ public/js/main.js
@@ -1,51 +1,80 @@
-const deleteBtn = document.querySelectorAll('.fa-trash')
+// get all classes with .fa-trash to add a trash icon to
+const deleteBtn = document.querySelectorAll('.fa-trash') 
+// get all spans inside .item class to use for db searches
 const item = document.querySelectorAll('.item span')
+// get all spans that have .completed to set the class in the html for the css styling to strike through
 const itemCompleted = document.querySelectorAll('.item span.completed')
 
-Array.from(deleteBtn).forEach((element)=>{
+
+// creates an array from the nodelist and add deleteItem event listeners to each
+Array.from(deleteBtn).forEach((element)=>{ // Array.from 
     element.addEventListener('click', deleteItem)
 })
 
+// creates an array from the nodelist and add markComplete event listeners to each
 Array.from(item).forEach((element)=>{
     element.addEventListener('click', markComplete)
 })
 
+// creates an array from the nodelist and add markUnComplete event listeners to each
 Array.from(itemCompleted).forEach((element)=>{
     element.addEventListener('click', markUnComplete)
 })
 
+// 
 async function deleteItem(){
-    const itemText = this.parentNode.childNodes[1].innerText
+    // this - object that was tied to event listener, 
+	// parent node - parent of that object, childnode [1] - the span text
+	const itemText = this.parentNode.childNodes[1].innerText 
+
+	// error handling through a try catch
     try{
-        const response = await fetch('deleteItem', {
-            method: 'delete',
-            headers: {'Content-Type': 'application/json'},
-            body: JSON.stringify({
+		//delete item is a route for the backend in the server.js and runs the deleteItem function
+        const response = await fetch('deleteItem', { 
+			// one of the four requests to the server and in this case is a delete
+            method: 'delete', 
+			// shows what type is being returned and in this case it is Json
+            headers: {'Content-Type': 'application/json'}, 
+            // wait for data to get returned from the backend
+			body: JSON.stringify({ 
               'itemFromJS': itemText
             })
           })
+		  
         const data = await response.json()
+		// log the data we receive
         console.log(data)
+		// reload the page and get request to populate new data
         location.reload()
-
+		// if there was an error sending the delete request log to console the error
     }catch(err){
         console.log(err)
     }
 }
 
 async function markComplete(){
+	// this - object that was tied to event listener, 
+	// parent node - parent of that object, childnode [1] - the span text
     const itemText = this.parentNode.childNodes[1].innerText
+	// error handling through a try catch
     try{
+		//delete item is a route for the backend in the server.js and runs the deleteItem function
         const response = await fetch('markComplete', {
+			// one of the four requests to the server and in this case is a put which is updating
             method: 'put',
+			// shows what type is being returned and in this case it is Json
             headers: {'Content-Type': 'application/json'},
+			// wait for data to get returned from the backend
             body: JSON.stringify({
                 'itemFromJS': itemText
             })
           })
+
         const data = await response.json()
         console.log(data)
+		//log the data we receive 
         location.reload()
+		// if there was an error sending the put request log to console the error
 
     }catch(err){
         console.log(err)
--- server.js
+++ server.js
@@ -1,93 +1,141 @@
-const express = require('express') // express is used 
-const app = express() // first two lines required to create an express application
-const MongoClient = require('mongodb').MongoClient // mongoDB is used
-const PORT = 2121 // 2121 is the specified port the server will be using
-require('dotenv').config() // zero-dependency module that loads environment variables from a .env file to process.env
+// express is used for our server
+const express = require('express') 
+// initiate app using the express constructor
+const app = express() 
+// mongoDB is used
+const MongoClient = require('mongodb').MongoClient 
+// fallback port
+const PORT = 2121 
+// using dotenv for our config file. Keep private info off of GIT
+require('dotenv').config() 
 
-
+// create some variables to use for our database
 let db,
     dbConnectionStr = process.env.DB_STRING,
-    dbName = 'todo'
+    dbName = 'todo' //name of the collection for our database
 
+// connect to the database
 MongoClient.connect(dbConnectionStr, { useUnifiedTopology: true })
     .then(client => {
+		// once connected log connection status
         console.log(`Connected to ${dbName} Database`)
+		// assign the reference to the connected DB
         db = client.db(dbName)
     })
-    
-app.set('view engine', 'ejs') // this line states that we will be using ejs to generate our HTML
-app.use(express.static('public')) // any files you put into the public folder will be served up from this line
+
+// set the view engine to ejs
+app.set('view engine', 'ejs') 
+// use express' built in public directory for default client side css/html/js
+app.use(express.static('public'))
+// urlencoded parses incoming requests
 app.use(express.urlencoded({ extended: true }))
+// parses incoming requests for json type requests
 app.use(express.json())
 
 
-app.get('/',async (request, response)=>{ // exactly like a click event // a git request to the database
-    const todoItems = await db.collection('todos').find().toArray() // go to the database // find all the documents in the collect // and turn it into an array
-    const itemsLeft = await db.collection('todos').countDocuments({completed: false}) // go to the database // find all the items that's not completed // turn it into an array
-    response.render('index.ejs', { items: todoItems, left: itemsLeft }) // renders the HTML // responds with the HTML file // items is the toDoItems array // left is the itemsLeft array
-    // db.collection('todos').find().toArray()
-    // .then(data => {
-    //     db.collection('todos').countDocuments({completed: false})
-    //     .then(itemsLeft => {
-    //         response.render('index.ejs', { items: data, left: itemsLeft })
-    //     })
-    // })
-    // .catch(error => console.error(error))
+// exactly like a click event // a git request to the database
+app.get('/',async (request, response)=>{ 
+	// go to the database // find all the documents in the collection // and turn it into an array
+    const todoItems = await db.collection('todos').find().toArray() 
+	// go to the database // find all the items that's not completed // turn it into an array
+    const itemsLeft = await db.collection('todos').countDocuments({completed: false}) 
+	// renders the view index.ejs and send the ejs the following items todoItems and itemLeft
+    response.render('index.ejs', { items: todoItems, left: itemsLeft }) 
+
+	// alternative method using .then instead of async/await
+		// db.collection('todos').find().toArray()
+		// .then(data => {
+		//     db.collection('todos').countDocuments({completed: false})
+		//     .then(itemsLeft => {
+		//         response.render('index.ejs', { items: data, left: itemsLeft })
+		//     })
+		// })
+		// .catch(error => console.error(error))
 })
 
-app.post('/addTodo', (request, response) => { // a git request to the database specifically a post request // '/addTodo' has to be the exact as the form on ejs
-    db.collection('todos').insertOne({thing: request.body.todoItem, completed: false}) // insert a new document into the database // a new todoItem // set as not completed 
-    .then(result => { // after the request this is the result
-        console.log('Todo Added') // logs 'Todo Added' to the console
-        response.redirect('/') // redirects back to the main route and triggers a refresh 
+// a git request to the database specifically a post request // '/addTodo' has to be the exact as the form on ejs
+app.post('/addTodo', (request, response) => { 
+	// insert a new document into the database // a new todoItem // set as not completed 
+    db.collection('todos').insertOne({thing: request.body.todoItem, completed: false})
+	// after the request this is the result
+    .then(result => { 
+		// logs 'Todo Added' to the console
+        console.log('Todo Added') 
+		// redirects back to the main route and triggers a refresh 
+        response.redirect('/') 
     })
-    .catch(error => console.error(error)) // an error if request doesn't go through
+	// an error if request doesn't go through
+    .catch(error => console.error(error)) 
 })
 
-app.put('/markComplete', (request, response) => { // a git request to the database to mark the item complete
-    db.collection('todos').updateOne({thing: request.body.itemFromJS},{ // updates the document in the database
+// a git request to the database to mark the item complete
+app.put('/markComplete', (request, response) => { 
+	// updates the document in the database
+    db.collection('todos').updateOne({thing: request.body.itemFromJS},{ 
         $set: {
-            completed: true // changed completed state to true
+			// set the document completed property to true
+            completed: true 
           }
     },{
+		// sorts the collection in ascending order by their id that was created on mongodb
         sort: {_id: -1}, 
+		// creates the item if it doesn't exist when set to true
         upsert: false
     })
-    .then(result => { // after the request this is the result
+	// after the request this is the result
+    .then(result => { 
+		// console logs 'Marked Complete'
         console.log('Marked Complete')
         response.json('Marked Complete')
     })
-    .catch(error => console.error(error)) // an error if request doesn't go through
+	// an error if request doesn't go through
+    .catch(error => console.error(error)) 
 
 })
 
+// a git request to the database to mark the item UnComplete
 app.put('/markUnComplete', (request, response) => {
+	// updates the document in the database
     db.collection('todos').updateOne({thing: request.body.itemFromJS},{
         $set: {
-            completed: false // changed completed state to false
+			// changed completed state to false
+            completed: false 
           }
     },{
-        sort: {_id: -1},
+		// sorts the collection in ascending order by their id that was created on mongodb
+        sort: {_id: -1}, 
+		// creates the item if it doesn't exist when set to true
         upsert: false
     })
-    .then(result => { // after the request this is the result
-        console.log('Marked Complete') // logs 'Marked Complete' in the console
-        response.json('Marked Complete') // marked complete on the json
+	// after the request this is the result
+    .then(result => { 
+		// logs 'Marked Complete' in the console
+        console.log('Marked Complete') 
+		// marked complete on the json
+        response.json('Marked Complete') 
     })
-    .catch(error => console.error(error)) // an error if request doesn't go through
+	// an error if request doesn't go through
+    .catch(error => console.error(error)) 
+
+})
+// delete request to the db 
+app.delete('/deleteItem', (request, response) => { 
+	// removes a document from the body
+    db.collection('todos').deleteOne({thing: request.body.itemFromJS}) 
+	// after the request this is the result
+    .then(result => { 
+		// logs 'Todo Deleted' to the console
+        console.log('Todo Deleted') 
+		// deleted json
+        response.json('Todo Deleted') 
+    })
+	// an error if request doesn't go through
+    .catch(error => console.error(error)) 
 
 })
 
-app.delete('/deleteItem', (request, response) => { // git request to delete an item
-    db.collection('todos').deleteOne({thing: request.body.itemFromJS}) // removes a document from the body
-    .then(result => { // after the request this is the result
-        console.log('Todo Deleted') // logs 'Todo Deleted' to the console
-        response.json('Todo Deleted') // deleted json
-    })
-    .catch(error => console.error(error)) // an error if request doesn't go through
-
-})
-
-app.listen(process.env.PORT || PORT, ()=>{ // either use the local port or the port the server uses
-    console.log(`Server running on port ${PORT}`) // console logs which port is running on 
+// either use the local port or the port the server uses
+app.listen(process.env.PORT || PORT, ()=>{ 
+	// console logs which port is running on 
+    console.log(`Server running on port ${PORT}`) 
 })
