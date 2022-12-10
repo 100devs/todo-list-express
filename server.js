f52f1cfdcf4b4d9cd1a3a35b3bba398b8ca3b703
Nicole Dicochea <dicocheanicole@gmail.com>
2022-12-10 11:01:30 -0800

    DB_string

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const MongoClient = require('mongodb').MongoClient
 const PORT = 2121
-require('dotenv').config()
+require('dotenv').config() // .env file 
 
 
 let db,
