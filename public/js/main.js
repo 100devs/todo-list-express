c7ecba56e72b3c4be7f4aee59e3901cf122d758b
Eleanor Latus <eleanor@latus.co.uk>
2022-12-30 11:30:12 +0000

    comments added line by line

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,9 @@
+// selecting all trash can icons and item spans
 const deleteBtn = document.querySelectorAll('.fa-trash')
 const item = document.querySelectorAll('.item span')
 const itemCompleted = document.querySelectorAll('.item span.completed')
+
+// looping through and adding smurf to each trash icon/span
 
 Array.from(deleteBtn).forEach((element)=>{
     element.addEventListener('click', deleteItem)
@@ -15,17 +18,24 @@ Array.from(itemCompleted).forEach((element)=>{
 })
 
 async function deleteItem(){
+    // grabbing the text from the corresponding todo e.g. walk mickey
+    //parentNode == li, childNode == span
     const itemText = this.parentNode.childNodes[1].innerText
     try{
+        // making a fetch/request to the server with a route of /deleteItem
         const response = await fetch('deleteItem', {
+            // making a delete request
             method: 'delete',
             headers: {'Content-Type': 'application/json'},
+            //sending request body to tell our API which item to delete
             body: JSON.stringify({
               'itemFromJS': itemText
             })
           })
+        //waiting for response
         const data = await response.json()
         console.log(data)
+        //getting the page to refresh and a new get request
         location.reload()
 
     }catch(err){
@@ -34,17 +44,24 @@ async function deleteItem(){
 }
 
 async function markComplete(){
+    // grabbing the text from the todo e.g. walk mickey
+    //parentNode == li, childNode == span
     const itemText = this.parentNode.childNodes[1].innerText
     try{
+        //making a fetch/request to the server with a route of /markComplete
         const response = await fetch('markComplete', {
+            // making a put request to the server
             method: 'put',
             headers: {'Content-Type': 'application/json'},
+            //sends the item text with the request body to tell our API which item to update
             body: JSON.stringify({
                 'itemFromJS': itemText
             })
           })
+        //await response from server.js and console log said response
         const data = await response.json()
         console.log(data)
+        //refresh page and trigger get request
         location.reload()
 
     }catch(err){
@@ -53,17 +70,24 @@ async function markComplete(){
 }
 
 async function markUnComplete(){
+    // grabbing text from the span to send with the request
     const itemText = this.parentNode.childNodes[1].innerText
     try{
+        // sending request to server
         const response = await fetch('markUnComplete', {
+            // making put request
             method: 'put',
             headers: {'Content-Type': 'application/json'},
+            //sending text within the request body
             body: JSON.stringify({
                 'itemFromJS': itemText
             })
           })
+        //await response
         const data = await response.json()
+        //console log response
         console.log(data)
+        // reload page to send new get request
         location.reload()
 
     }catch(err){
