2174c936a7615fe6537df26a23b84027c3f00f74
Rai_Froh <raifroh@gmail.com>
2023-01-31 11:29:41 -0800

    mongodb reshuffle on server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
+const MongoClient = require('mongodb').MongoClient
 const express = require('express')
 const app = express()
-const MongoClient = require('mongodb').MongoClient
 const PORT = 2121
 require('dotenv').config()
 
