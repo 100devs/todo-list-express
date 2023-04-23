2c8e1e56c0dc59e638a00a3675556055ff25b75a
Jonathan Guzman <85004006+jonathang2560@users.noreply.github.com>
2023-04-23 10:04:31 -0500

    Update main.js

--- public/js/main.js
+++ public/js/main.js
@@ -1,22 +1,28 @@
+//identifies .fa-trash as deleteBtn
 const deleteBtn = document.querySelectorAll('.fa-trash')
+//identifies .item span as item
 const item = document.querySelectorAll('.item span')
+// identifies .item span.completed as itemCompleted
 const itemCompleted = document.querySelectorAll('.item span.completed')
 
+//function that goes through each of the elements in order identify the choosen element for it to be deleted
 Array.from(deleteBtn).forEach((element)=>{
     element.addEventListener('click', deleteItem)
 })
-
+//function that on click calls the function markComplete
 Array.from(item).forEach((element)=>{
     element.addEventListener('click', markComplete)
 })
-
+//function that on click calls the function markUnComplete
 Array.from(itemCompleted).forEach((element)=>{
     element.addEventListener('click', markUnComplete)
 })
-
+//function deleteItem()
 async function deleteItem(){
+//goes to up tp thiss' parentNode then down to childNodes first childs' innertext
     const itemText = this.parentNode.childNodes[1].innerText
     try{
+//waits for delete item
         const response = await fetch('deleteItem', {
             method: 'delete',
             headers: {'Content-Type': 'application/json'},
