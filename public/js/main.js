a07fc86bc3bb470409f22bc8cb5404b9ff35c552
wSpaceByte <107953410+wSpaceByte@users.noreply.github.com>
2023-04-25 21:42:31 -0700

    Completed CSS commenting, started JS

--- public/js/main.js
+++ public/js/main.js
@@ -1,21 +1,21 @@
-const deleteBtn = document.querySelectorAll('.fa-trash')
-const item = document.querySelectorAll('.item span')
-const itemCompleted = document.querySelectorAll('.item span.completed')
+const deleteBtn = document.querySelectorAll('.fa-trash') //declares variable "deleteBtn" which will contains all '.fa-trash' classes
+const item = document.querySelectorAll('.item span') //declares variable "item" which will contains all '.item span' classes
+const itemCompleted = document.querySelectorAll('.item span.completed') //declares variable "itemCompleted" which will contains all '.item span.completed' classes
 
-Array.from(deleteBtn).forEach((element)=>{
-    element.addEventListener('click', deleteItem)
-})
+Array.from(deleteBtn).forEach((element)=>{  //organizes all elements within variable, 'deleteBtn', into array through a loop through whole array, then adds an event listener to each - specifically a click
+    element.addEventListener('click', deleteItem) //when element from array is clicked, invoke 'deleteItem'
+}) //closes loop
 
-Array.from(item).forEach((element)=>{
-    element.addEventListener('click', markComplete)
-})
+Array.from(item).forEach((element)=>{ //organizes all elements within variable, 'item', into array through a loop through whole array, then adds an event listener to each - specifically a click
+    element.addEventListener('click', markComplete) //when element from array is clicked, invoke 'markComplete'
+}) //closes loop
 
-Array.from(itemCompleted).forEach((element)=>{
-    element.addEventListener('click', markUnComplete)
-})
+Array.from(itemCompleted).forEach((element)=>{ //organizes all elements within variable, 'itemCompleted', into array through a loop through whole array, then adds an event listener to each - specifically a click
+    element.addEventListener('click', markUnComplete) //when element from array is clicked, invoke 'markUnComplete'
+}) //closes loop
 
-async function deleteItem(){
-    const itemText = this.parentNode.childNodes[1].innerText
+async function deleteItem(){ //starts function block, with async/await syntax
+    const itemText = this.parentNode.childNodes[1].innerText  //'this' refers to the element clicked on. Refer to parent element, then drop down to the child's innerText. Store this inner text in the variable 'itemText'
     try{
         const response = await fetch('deleteItem', {
             method: 'delete',
