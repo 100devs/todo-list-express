dea64f4107b49cb1f33657df27b57bec63ea1934
Borja Martí <borjamarti@outlook.com>
2023-05-07 19:35:08 +0200

    Commented the code to explain its functioning

--- public/js/main.js
+++ public/js/main.js
@@ -1,7 +1,9 @@
+// Query selectors of our HTML element by class and type
 const deleteBtn = document.querySelectorAll('.fa-trash')
 const item = document.querySelectorAll('.item span')
 const itemCompleted = document.querySelectorAll('.item span.completed')
 
+// Since we used querySelectorAll for multiple elements, we are returned a NodeList, which we'll have to transform into an array to operate. We use a forEach loop on the array to add event listeners to all the elements contaiend in the NodeList
 Array.from(deleteBtn).forEach((element)=>{
     element.addEventListener('click', deleteItem)
 })
@@ -14,9 +16,13 @@ Array.from(itemCompleted).forEach((element)=>{
     element.addEventListener('click', markUnComplete)
 })
 
+// Since we are going to make fetch requests, which involve promises, we are going to use async functions, allowing us to wait for the promise to complete before operating on it.
 async function deleteItem(){
+    // 'this' references the element from which the event listener was fired, allowing us, in this case, to bind its text content
     const itemText = this.parentNode.childNodes[1].innerText
+    // We use try to group the operations that will be done to the promise if it's fulfilled, and catch in case there is an error in any of the steps
     try{
+        // Fetch request to the 'deleteItem' route allows us to delete items from the list
         const response = await fetch('deleteItem', {
             method: 'delete',
             headers: {'Content-Type': 'application/json'},
@@ -24,6 +30,7 @@ async function deleteItem(){
               'itemFromJS': itemText
             })
           })
+        // If the fetch is succesful, the response is logged
         const data = await response.json()
         console.log(data)
         location.reload()
@@ -33,9 +40,11 @@ async function deleteItem(){
     }
 }
 
+// Same process, but for marking a task as completed
 async function markComplete(){
     const itemText = this.parentNode.childNodes[1].innerText
     try{
+        // In this case, we use 'put' method to update the task
         const response = await fetch('markComplete', {
             method: 'put',
             headers: {'Content-Type': 'application/json'},
@@ -52,6 +61,7 @@ async function markComplete(){
     }
 }
 
+// Same as completed, but for marking uncompleted
 async function markUnComplete(){
     const itemText = this.parentNode.childNodes[1].innerText
     try{
