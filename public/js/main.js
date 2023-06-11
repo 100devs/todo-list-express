8027442a7ed906f150adc6f040516f661c7e13f7
Kay <khulood.hassan@hotmail.com>
2023-06-11 21:53:07 +0400

    Commented

--- public/js/main.js
+++ public/js/main.js
@@ -1,72 +1,117 @@
+//- returns a node list of all trash icons
 const deleteBtn = document.querySelectorAll('.fa-trash')
+//- returns a node list of all the todo items
 const item = document.querySelectorAll('.item span')
+//- returns a node list of all the iems with a class of completed
 const itemCompleted = document.querySelectorAll('.item span.completed')
 
-Array.from(deleteBtn).forEach((element)=>{
-    element.addEventListener('click', deleteItem)
+//- Create an array from the node list and loop through
+Array.from(deleteBtn).forEach(e => {
+    //- add an event listener that listens on a click and calls the deleteItem function when clicked
+    e.addEventListener('click', deleteItem)
 })
 
-Array.from(item).forEach((element)=>{
-    element.addEventListener('click', markComplete)
+//- Create an array from the node list and loop through
+Array.from(item).forEach(e => {
+    //- add an event listener that listens on a click and calls the markComplete function when clicked
+    e.addEventListener('click', markComplete)
 })
 
-Array.from(itemCompleted).forEach((element)=>{
-    element.addEventListener('click', markUnComplete)
+//- Create an array from the node list and loop through
+Array.from(itemCompleted).forEach(e => {
+    //- add an event listener that listens on a click and calls the markUncomplete function when clicked
+    e.addEventListener('click', markUnComplete)
 })
 
-async function deleteItem(){
+//- async function for deleting an item
+async function deleteItem() {
+    //- it refers to the inner text content of the span that contains the todo item
     const itemText = this.parentNode.childNodes[1].innerText
-    try{
+    //- try catch block
+    try {
+        //- stores the fetch data in repsonse variable
         const response = await fetch('deleteItem', {
+            //- defines the fetch method
             method: 'delete',
+            //- relays information about the content being sent to a server
             headers: {'Content-Type': 'application/json'},
+            //- stringifies the content being sent so the items can be read
             body: JSON.stringify({
+                //- the object being sent
               'itemFromJS': itemText
             })
-          })
+        })
+        //- stores the json fetch response in the data variable
         const data = await response.json()
+        //- logs the data to the console
         console.log(data)
+        //- reloads the current document
         location.reload()
-
-    }catch(err){
+    //- catch an error
+    } catch (err) {
+        //- console logs the error
         console.log(err)
     }
 }
 
-async function markComplete(){
+//- async function for marking an item as complete
+async function markComplete() {
+    //- it refers to the inner text content of the span that contains the todo item
     const itemText = this.parentNode.childNodes[1].innerText
-    try{
+    //- try catch block
+    try {
+        //- stores the fetch data in repsonse variable
         const response = await fetch('markComplete', {
+            //- defines the fetch method
             method: 'put',
+            //- relays information about the content being sent to a server
             headers: {'Content-Type': 'application/json'},
+            //- stringifies the content being sent so the items can be read
             body: JSON.stringify({
+                //- the object being sent
                 'itemFromJS': itemText
             })
-          })
+        })
+        //- stores the json fetch response in the data variable
         const data = await response.json()
+        //- logs the data to the console
         console.log(data)
+        //- reloads the current document
         location.reload()
-
-    }catch(err){
+    //- catch an error
+    } catch (err) {
+        //- console logs the error
         console.log(err)
     }
 }
 
-async function markUnComplete(){
+//- async function for marking an item as incomplete
+async function markUnComplete() {
+    //- it refers to the inner text content of the span that contains the todo item
     const itemText = this.parentNode.childNodes[1].innerText
-    try{
+    //- try catch block
+    try {
+        //- stores the fetch data in repsonse variable
         const response = await fetch('markUnComplete', {
+            //- defines the fetch method
             method: 'put',
+            //- relays information about the content being sent to a server
             headers: {'Content-Type': 'application/json'},
+            //- stringifies the content being sent so the items can be read
             body: JSON.stringify({
+                //- the object being sent
                 'itemFromJS': itemText
             })
-          })
+        })
+        //- stores the json fetch response in the data variable
         const data = await response.json()
+        //- logs the data to the console
         console.log(data)
+        //- reloads the current document
         location.reload()
-
-    }catch(err){
+    //- catch an error
+    } catch (err) {
+        //- console logs the error
         console.log(err)
     }
 }
--- server.js
+++ server.js
@@ -1,93 +1,157 @@
+//- Add express moduke
 const express = require('express')
+//- Initialize express
 const app = express()
-const MongoClient = require('mongodb').MongoClient
+//- Add mongodb module
+const { MongoClient } = require('mongodb')
+//- Define listen port
 const PORT = 2121
+//- Import .env file that contains densitive information
 require('dotenv').config()
 
+//- Declare db variable to store database
+let db
+//- Initialize dbConnectionStr with the value from .env file that contains the mongodb uri
+let dbConnectionStr = process.env.DB_STRING
+//- initialize dbName with a string of todo
+let dbName = 'todo'
 
-let db,
-    dbConnectionStr = process.env.DB_STRING,
-    dbName = 'todo'
+//- Instantiate a new client
+const client = new MongoClient(dbConnectionStr, { useUnifiedTopology: true })
 
-MongoClient.connect(dbConnectionStr, { useUnifiedTopology: true })
-    .then(client => {
-        console.log(`Connected to ${dbName} Database`)
+//- async connect function to connect to mongo
+async function connect(){
+    //- try catch block
+    try {
+        //- await a client connect
+        await client.connect();
+        //- set database
         db = client.db(dbName)
-    })
-    
+        //- console log the connection confirmation
+        console.log("Connected to MongoDB");
+    }
+    //- catch error
+    catch (error) {
+        //- console log the error
+        console.error(error);
+    }
+}
+
+//- call the connect function
+connect();
+
+//- used to set configurations; view engine in this case
 app.set('view engine', 'ejs')
+//- tells the app to use the public folder for static files
 app.use(express.static('public'))
+//- middleway that parses requests from forms
 app.use(express.urlencoded({ extended: true }))
+//- parses requests to allow server to handle JSON data
 app.use(express.json())
 
-
-app.get('/',async (request, response)=>{
-    const todoItems = await db.collection('todos').find().toArray()
-    const itemsLeft = await db.collection('todos').countDocuments({completed: false})
-    response.render('index.ejs', { items: todoItems, left: itemsLeft })
-    // db.collection('todos').find().toArray()
-    // .then(data => {
-    //     db.collection('todos').countDocuments({completed: false})
-    //     .then(itemsLeft => {
-    //         response.render('index.ejs', { items: data, left: itemsLeft })
-    //     })
-    // })
-    // .catch(error => console.error(error))
+//- makes a read request to the root
+app.get('/', async (req, res) => {
+    //- try catch block
+    try {
+        //- stores all the todo items in an array in the todoItems variable
+        const todoItems = await db.collection('todos').find().toArray()
+        //- stores the count of incomplete todos and stores the value in itemsLeft
+        const itemsLeft = await db.collection('todos').countDocuments({completed: false})
+        //- renders the ejs file and passes the items and left queries
+        res.render('index.ejs', { items: todoItems, left: itemsLeft })
+    //- catch the error
+    } catch (error) {
+        //- display error on console
+        console.error(error)
+    }
 })
 
-app.post('/addTodo', (request, response) => {
-    db.collection('todos').insertOne({thing: request.body.todoItem, completed: false})
-    .then(result => {
+//- this is a create request. the path is the form from the ejs file.
+app.post('/addTodo', async (req, res) => {
+    //- try catch block
+    try {
+        //- adds the item inserted into the form into the todos collection
+        await db.collection('todos').insertOne({thing: req.body.todoItem, completed: false})
+        //- logs that todo was added
         console.log('Todo Added')
-        response.redirect('/')
-    })
-    .catch(error => console.error(error))
+        //- redirects to the root again aka refresh
+        res.redirect('/')
+    //- catch the error
+    } catch (error) {
+        //- display error on console
+        console.error(error)
+    }
 })
 
-app.put('/markComplete', (request, response) => {
-    db.collection('todos').updateOne({thing: request.body.itemFromJS},{
-        $set: {
-            completed: true
-          }
-    },{
-        sort: {_id: -1},
-        upsert: false
-    })
-    .then(result => {
+//- update request to mark a todo as complete
+app.put('/markComplete', async (req, res) => {
+    //- try catch block
+    try {
+        //- update one value in the database. In this case, the update is to set the completed field to true
+        await db.collection('todos').updateOne({thing: req.body.itemFromJS}, {
+            //- set completed field to true
+            $set: {completed: true}
+        },{
+            //- sorts opposite (last to first)
+            sort: {_id: -1},
+            //- upsert is a combination of update + insert. It is used when using update or updateOne. When false, if no combination is found, it doesn't update anything. When true, if no combination is found, a new field/document is added
+            upsert: false
+        })
+        //- logs that mark was complete
         console.log('Marked Complete')
-        response.json('Marked Complete')
-    })
-    .catch(error => console.error(error))
-
+        //- used to send a json response to the client. The purposed of this repsonse is just to assure to the client that the todo is marked as complete
+        res.json('Marked Complete')
+    //- catch the error
+    } catch (error) {
+        //- display error on console
+        console.error(error)
+    }
 })
 
-app.put('/markUnComplete', (request, response) => {
-    db.collection('todos').updateOne({thing: request.body.itemFromJS},{
-        $set: {
-            completed: false
-          }
-    },{
-        sort: {_id: -1},
-        upsert: false
-    })
-    .then(result => {
-        console.log('Marked Complete')
-        response.json('Marked Complete')
-    })
-    .catch(error => console.error(error))
-
+//- update to remove the mark as complete from the todo
+app.put('/markUnComplete', async (req, res) => {
+    //- try catch block
+    try {
+        //- update one value in the database. In this case, the update is to set the completed field to false
+        await db.collection('todos').updateOne({thing: req.body.itemFromJS}, {
+            //- set completed field to true
+            $set: {completed: false}
+        },{
+            //- sorts opposite (last to first)
+            sort: {_id: -1},
+            //- upsert is a combination of update + insert. It is used when using update or updateOne. When false, if no combination is found, it doesn't update anything. When true, if no combination is found, a new field/document is added
+            upsert: false
+        })
+        //- logs that mark was incomplete
+        console.log('Marked Incomplete')
+        //- used to send a json response to the client. The purposed of this repsonse is just to assure to the client that the todo is marked as incomplete
+        res.json('Marked Incomplete')
+    //- catch the error
+    } catch (error) {
+        //- display error on console
+        console.error(error)
+    }
 })
 
-app.delete('/deleteItem', (request, response) => {
-    db.collection('todos').deleteOne({thing: request.body.itemFromJS})
-    .then(result => {
+//- delete method to delete an item from the list of todos
+app.delete('/deleteItem', async (req, res) => {
+    //- try catch block
+    try {
+        //- delete one value in the database
+        await db.collection('todos').deleteOne({thing: req.body.itemFromJS})
+        //- logs that dodo was deleted
         console.log('Todo Deleted')
-        response.json('Todo Deleted')
-    })
-    .catch(error => console.error(error))
-
+        //- used to send a json response to the client. The purposed of this repsonse is just to assure to the client that the todo is deleted
+        res.json('Todo Deleted')
+    //- catch the error
+    } catch (error) {
+        //- display error on console
+        console.error(error)
+    }
 })
 
-app.listen(process.env.PORT || PORT, ()=>{
+//- listens to the server on the port set
+app.listen(process.env.PORT || PORT, () => {
+    //- console logs that the server is running
     console.log(`Server running on port ${PORT}`)
 })
