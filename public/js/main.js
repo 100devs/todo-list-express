68d2a5d4ea3eaeba3accb271150d78529f57a669
Bryan King Pecho <bkpecho05@gmail.com>
2023-06-16 10:45:56 +0800

    Update main.js

--- public/js/main.js
+++ public/js/main.js
@@ -1,7 +1,12 @@
+// variables
+
+// assigns variables to some classes
 const deleteBtn = document.querySelectorAll('.fa-trash')
 const item = document.querySelectorAll('.item span')
 const itemCompleted = document.querySelectorAll('.item span.completed')
 
+
+// assigns an click addEventListener to the declared variables
 Array.from(deleteBtn).forEach((element)=>{
     element.addEventListener('click', deleteItem)
 })
@@ -14,9 +19,14 @@ Array.from(itemCompleted).forEach((element)=>{
     element.addEventListener('click', markUnComplete)
 })
 
+// an async function that deletes an item
 async function deleteItem(){
+    
+    //retrieves the text content of the current element
     const itemText = this.parentNode.childNodes[1].innerText
     try{
+        
+        // initiates a delete request to the deleteItem endpoint using the fetch API 
         const response = await fetch('deleteItem', {
             method: 'delete',
             headers: {'Content-Type': 'application/json'},
@@ -24,8 +34,14 @@ async function deleteItem(){
               'itemFromJS': itemText
             })
           })
+        
+//         parses the response as json
         const data = await response.json()
+        
+//         logs the response data to the console
         console.log(data)
+        
+//         reloads the page
         location.reload()
 
     }catch(err){
@@ -33,9 +49,14 @@ async function deleteItem(){
     }
 }
 
+// an async function to mark an element as completed
 async function markComplete(){
+    
+    //retrieves the inner text content of the current element
     const itemText = this.parentNode.childNodes[1].innerText
     try{
+        
+        //initiates a put request to the markComplete endpoint using the FetchAPI
         const response = await fetch('markComplete', {
             method: 'put',
             headers: {'Content-Type': 'application/json'},
@@ -43,18 +64,31 @@ async function markComplete(){
                 'itemFromJS': itemText
             })
           })
+        
+        // parses the response to json
         const data = await response.json()
+        
+        // logs the response to the console
         console.log(data)
+        
+        // reloads the page
         location.reload()
 
     }catch(err){
+        
+        // logs the error to the console
         console.log(err)
     }
 }
 
+// an async function that marks the current element as incomplete
 async function markUnComplete(){
+    
+    // retrieves the inner text content of the current element
     const itemText = this.parentNode.childNodes[1].innerText
     try{
+        
+        // initiates a put request to the markUncomplete endpoint using fetchAPI
         const response = await fetch('markUnComplete', {
             method: 'put',
             headers: {'Content-Type': 'application/json'},
@@ -62,11 +96,19 @@ async function markUnComplete(){
                 'itemFromJS': itemText
             })
           })
+        
+        // parses the response into json
         const data = await response.json()
+        
+        // logs the data (response) in the console
         console.log(data)
+        
+        // reloads the page
         location.reload()
 
     }catch(err){
+        
+        // logs the error in the console
         console.log(err)
     }
 }
