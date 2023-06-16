79d7bfed5d44a0a95fbe9ad2e1608e73c20e2cfe
Bryan King Pecho <bkpecho05@gmail.com>
2023-06-16 13:12:14 +0800

    Update server.js

--- server.js
+++ server.js
@@ -65,60 +65,88 @@ app.get('/',async (request, response)=>{
     // .catch(error => console.error(error))
 })
 
-
+// a post request in the /addTodo route
 app.post('/addTodo', (request, response) => {
+   // inserts a new todo in the todos collection with the provided data
     db.collection('todos').insertOne({thing: request.body.todoItem, completed: false})
     .then(result => {
+        
+        // logs in the console that the todo are added
         console.log('Todo Added')
+        
+        //redirect to root url
         response.redirect('/')
     })
-    .catch(error => console.error(error))
+    
+    .catch(error => console.error(error)) //catches the error, console logs the error
 })
 
+// a put request at /markComplete route
 app.put('/markComplete', (request, response) => {
+    
+    // updates the todos document witht he provided itemFromJS
     db.collection('todos').updateOne({thing: request.body.itemFromJS},{
         $set: {
+            //sets completed as true
             completed: true
           }
     },{
+        
+        //sorts all the items
         sort: {_id: -1},
         upsert: false
     })
     .then(result => {
+        // executes this block of code when the update is successful
         console.log('Marked Complete')
         response.json('Marked Complete')
     })
+    
+    //logs error in the console
     .catch(error => console.error(error))
 
 })
 
+// put request in markUnComplete route
 app.put('/markUnComplete', (request, response) => {
+    // updates the this in the collection todos
     db.collection('todos').updateOne({thing: request.body.itemFromJS},{
         $set: {
+           //sets completed as false
             completed: false
           }
     },{
+        //sorts all the items
         sort: {_id: -1},
         upsert: false
     })
     .then(result => {
-        console.log('Marked Complete')
-        response.json('Marked Complete')
+        // logs in the console that it is marked incomplete
+        console.log('Marked Incomplete')
+        response.json('Marked Incomplete')
     })
+    //logs the error in the console
     .catch(error => console.error(error))
 
 })
 
+// a delete request at /deleteItem route
 app.delete('/deleteItem', (request, response) => {
+    
+    // deletes the thing that is inside the todos collection
     db.collection('todos').deleteOne({thing: request.body.itemFromJS})
     .then(result => {
+        //logs in the console that the todo is deleted
         console.log('Todo Deleted')
         response.json('Todo Deleted')
     })
+    //logs in the error if deletion is unsuccessful
     .catch(error => console.error(error))
 
 })
 
+// app.listen starts the express application, tells it to start listening to the provided port in the environment variable (when deployed) or the local PORT when on development
 app.listen(process.env.PORT || PORT, ()=>{
+    // logs in the console when app.listen is successful
     console.log(`Server running on port ${PORT}`)
 })
