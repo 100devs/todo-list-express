b244276dd82e88a0ad1c9cc1feaf12f2662ef06a
Marko Vasic <122864019+Vastradamus@users.noreply.github.com>
2023-07-03 10:26:19 +0200

    added comments for main.js EJS and Server.js

--- public/js/main.js
+++ public/js/main.js
@@ -1,29 +1,42 @@
+// get elements from the html and store them in the variables 
 const deleteBtn = document.querySelectorAll('.fa-trash')
 const item = document.querySelectorAll('.item span')
 const itemCompleted = document.querySelectorAll('.item span.completed')
 
+// iterate through deleteBtn and add click event for each of them, that will fire a deleteItem function
 Array.from(deleteBtn).forEach((element)=>{
     element.addEventListener('click', deleteItem)
 })
+// iterate through item and add click event for each of them, that will fire a markComplete function
 
 Array.from(item).forEach((element)=>{
     element.addEventListener('click', markComplete)
 })
 
+// iterate through itemCompleted and add click event for each of them, that will fire a markUnComplete function
+
 Array.from(itemCompleted).forEach((element)=>{
     element.addEventListener('click', markUnComplete)
 })
 
+// async fucntio means that you don't have to wait for it to be finished, the goblin can came back and grab the resaults 
+
 async function deleteItem(){
+    // go to the parent element of the element cliced and grab the innerText of first child 
     const itemText = this.parentNode.childNodes[1].innerText
+
+    // start a try-catch block in case of errors
     try{
+        // await meangs you should wait until the promise is solved 
         const response = await fetch('deleteItem', {
+            // declaring a method we want, and type of data we're using
             method: 'delete',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
               'itemFromJS': itemText
             })
           })
+          // we need to await for everyitnh to be complited so we can res with someting, in this case json file
         const data = await response.json()
         console.log(data)
         location.reload()
@@ -70,3 +83,4 @@ async function markUnComplete(){
         console.log(err)
     }
 }
+
--- server.js
+++ server.js
@@ -1,30 +1,34 @@
-const express = require('express')
+// dependencies 
+const express = require('express') 
 const app = express()
 const MongoClient = require('mongodb').MongoClient
+
+// port ( can go to .env as well)
 const PORT = 2121
-require('dotenv').config()
+require('dotenv').config() // enable .env 
 
 
 let db,
-    dbConnectionStr = process.env.DB_STRING,
+    dbConnectionStr = process.env.DB_STRING, // referencing db_string from .env file for security 
     dbName = 'todo'
 
-MongoClient.connect(dbConnectionStr, { useUnifiedTopology: true })
-    .then(client => {
+MongoClient.connect(dbConnectionStr, { useUnifiedTopology: true }) // use new MongoDb engine
+    .then(client => { // after you're connected console log name of database 
         console.log(`Connected to ${dbName} Database`)
-        db = client.db(dbName)
+        db = client.db(dbName) // connect to db, so you dont have to type clinet.db('todo') over again
     })
     
-app.set('view engine', 'ejs')
-app.use(express.static('public'))
-app.use(express.urlencoded({ extended: true }))
+    
+app.set('view engine', 'ejs') // render the page using ejs
+app.use(express.static('public')) //middleware for serving static files via HTTP
+app.use(express.urlencoded({ extended: true })) // not 100% but i think it allows us to parse trough files we got from user in req.body
 app.use(express.json())
 
 
 app.get('/',async (request, response)=>{
-    const todoItems = await db.collection('todos').find().toArray()
-    const itemsLeft = await db.collection('todos').countDocuments({completed: false})
-    response.render('index.ejs', { items: todoItems, left: itemsLeft })
+    const todoItems = await db.collection('todos').find().toArray() // wait for it to find all todos from db and put it in the arary
+    const itemsLeft = await db.collection('todos').countDocuments({completed: false}) // count how many documents have complited: false 
+    response.render('index.ejs', { items: todoItems, left: itemsLeft }) // render data with ejs template
     // db.collection('todos').find().toArray()
     // .then(data => {
     //     db.collection('todos').countDocuments({completed: false})
@@ -36,15 +40,19 @@ app.get('/',async (request, response)=>{
 })
 
 app.post('/addTodo', (request, response) => {
+    // go to the db collenction an insert one, thing is the naem in db collection, req.body.todoItem gets whatever they wrote. It takes req goes to bdoy and todoItem is name of the fild
     db.collection('todos').insertOne({thing: request.body.todoItem, completed: false})
     .then(result => {
         console.log('Todo Added')
-        response.redirect('/')
+        response.redirect('/') // get back to the main page
     })
-    .catch(error => console.error(error))
+    .catch(error => console.error(error)) // console log the error
 })
 
+// if you click on the item, it should be marked complite 
 app.put('/markComplete', (request, response) => {
+
+    // updateOne looks for thing that has the same name in Db as itemFromJS( item being clicked)
     db.collection('todos').updateOne({thing: request.body.itemFromJS},{
         $set: {
             completed: true
@@ -87,7 +95,7 @@ app.delete('/deleteItem', (request, response) => {
     .catch(error => console.error(error))
 
 })
-
+// Defining PORT to run on, ether local or hosted
 app.listen(process.env.PORT || PORT, ()=>{
     console.log(`Server running on port ${PORT}`)
 })
