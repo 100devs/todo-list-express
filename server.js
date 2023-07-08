de5745fcdcac385370825cfc12b33aca4ba43d91
Miguel Gomez <122931039+MiguelGGithub@users.noreply.github.com>
2023-07-08 14:59:53 -0400

    Update server.js

--- server.js
+++ server.js
@@ -2,29 +2,31 @@ const express = require('express') //Express is installed and initialized
 const app = express() //express is stored to use.
 const MongoClient = require('mongodb').MongoClient //MongoDB is installed and initialized
 const PORT = 2121 //Port # created (this server is listening on localhost:2121
-require('dotenv').config() //dotenv file is read from. This contains sensitive information, most likely about the MongoDB client like password and user. 
+require('dotenv').config() //dotenv file is read from. This contains sensitive information, most likely about the MongoDB client like password and user to link MongoDB database to the program
 
 
 let db,
-    dbConnectionStr = process.env.DB_STRING,
+    dbConnectionStr = process.env.DB_STRING, //Explained above., just declared here. 
     dbName = 'todo'
 
-MongoClient.connect(dbConnectionStr, { useUnifiedTopology: true })
+MongoClient.connect(dbConnectionStr, { useUnifiedTopology: true }) //the server is connected to the MongoDB database using the hidden ENV files and variables stored there (dbConnectionStr and dbName).
     .then(client => {
         console.log(`Connected to ${dbName} Database`)
         db = client.db(dbName)
     })
-    
-app.set('view engine', 'ejs')
-app.use(express.static('public'))
-app.use(express.urlencoded({ extended: true }))
-app.use(express.json())
+
+//all of this is middleware
+app.set('view engine', 'ejs') //EJS view engine set to write content from server to DOM.
+app.use(express.static('public')) //serves a static file to the server from 'public' (folder) to use.
+app.use(express.urlencoded({ extended: true })) //urelencoded allows for the grabbing of the URL to find what it's looking for. Ex. https://blahblah.com/images. I only need to hear the endpoint of /images, not the
+                                                // full URL, this is what urlencoded does, it grabs the "code" from the URL.
+app.use(express.json()) // allows for parsing of JSON data to the request body. 
 
 
 app.get('/',async (request, response)=>{
-    const todoItems = await db.collection('todos').find().toArray()
-    const itemsLeft = await db.collection('todos').countDocuments({completed: false})
-    response.render('index.ejs', { items: todoItems, left: itemsLeft })
+    const todoItems = await db.collection('todos').find().toArray() //This sets the todoItems ul in index.ejs to an array
+    const itemsLeft = await db.collection('todos').countDocuments({completed: false}) //This sets the items in todoItems which are not completed to an array of itemsLeft
+    response.render('index.ejs', { items: todoItems, left: itemsLeft }) // renders the todoItems and itemsLeft to the DOM in index.ejs. Changes todoItems to items and itemsLeft to left. (all are arrays)
     // db.collection('todos').find().toArray()
     // .then(data => {
     //     db.collection('todos').countDocuments({completed: false})
@@ -35,7 +37,7 @@ app.get('/',async (request, response)=>{
     // .catch(error => console.error(error))
 })
 
-app.post('/addTodo', (request, response) => {
+app.post('/addTodo', (request, response) => { //Explained in main.js, adds a to-do item which is NOT completed to the todoItems ul.
     db.collection('todos').insertOne({thing: request.body.todoItem, completed: false})
     .then(result => {
         console.log('Todo Added')
@@ -44,7 +46,7 @@ app.post('/addTodo', (request, response) => {
     .catch(error => console.error(error))
 })
 
-app.put('/markComplete', (request, response) => {
+app.put('/markComplete', (request, response) => { //Explained in main.js, adds class of "completed" to an item in the todoItems ul in index.ejs.
     db.collection('todos').updateOne({thing: request.body.itemFromJS},{
         $set: {
             completed: true
@@ -61,7 +63,7 @@ app.put('/markComplete', (request, response) => {
 
 })
 
-app.put('/markUnComplete', (request, response) => {
+app.put('/markUnComplete', (request, response) => { //Explained in main.js, removes class of "completed" from to-do list item in todoItems ul in index.ejs
     db.collection('todos').updateOne({thing: request.body.itemFromJS},{
         $set: {
             completed: false
@@ -78,7 +80,7 @@ app.put('/markUnComplete', (request, response) => {
 
 })
 
-app.delete('/deleteItem', (request, response) => {
+app.delete('/deleteItem', (request, response) => { //Explained in main.js, removes to-do list item from to-do list (todoItems ul in index.ejs).
     db.collection('todos').deleteOne({thing: request.body.itemFromJS})
     .then(result => {
         console.log('Todo Deleted')
@@ -88,6 +90,6 @@ app.delete('/deleteItem', (request, response) => {
 
 })
 
-app.listen(process.env.PORT || PORT, ()=>{
+app.listen(process.env.PORT || PORT, ()=>{ //creates the server and makes it listen on port 2121
     console.log(`Server running on port ${PORT}`)
 })
