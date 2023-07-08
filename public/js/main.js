fa276edb43f1d5aa476350c9dc5d1ee4d8bb7d5b
Miguel Gomez <122931039+MiguelGGithub@users.noreply.github.com>
2023-07-08 14:14:52 -0400

    Update main.js

--- public/js/main.js
+++ public/js/main.js
@@ -1,23 +1,23 @@
-const deleteBtn = document.querySelectorAll('.fa-trash')
-const item = document.querySelectorAll('.item span')
-const itemCompleted = document.querySelectorAll('.item span.completed')
+const deleteBtn = document.querySelectorAll('.fa-trash') //Delete button (span) from index.ejs is stored in a variable
+const item = document.querySelectorAll('.item span') //Each item in the to-do list from index.ejs is stored in a variable
+const itemCompleted = document.querySelectorAll('.item span.completed') //Each item that is marked as completed with the class of "completed" in the index.ejs file is stored in a variable
 
 Array.from(deleteBtn).forEach((element)=>{
-    element.addEventListener('click', deleteItem)
+    element.addEventListener('click', deleteItem) //For each delete button per item, the function deleteItem() is added to it on a click.
 })
 
 Array.from(item).forEach((element)=>{
-    element.addEventListener('click', markComplete)
+    element.addEventListener('click', markComplete) //For each item in the to-do list, the function markComplete() is added to it on a click
 })
 
 Array.from(itemCompleted).forEach((element)=>{
-    element.addEventListener('click', markUnComplete)
+    element.addEventListener('click', markUnComplete) //For each item which has the class of "completed" (to show its completed), the markUncomplete() method is added to it on a click.
 })
 
-async function deleteItem(){
-    const itemText = this.parentNode.childNodes[1].innerText
+async function deleteItem(){ //this function is called on every delete button on the to-do list.
+    const itemText = this.parentNode.childNodes[1].innerText //The innerText (content) of the item at hand is stored in a variable.
     try{
-        const response = await fetch('deleteItem', {
+        const response = await fetch('deleteItem', { 
             method: 'delete',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
