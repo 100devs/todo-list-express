0f41b8952be4f289eebfe49104175a5873844ca8
Mayur Rathod <rmayur777@gmail.com>
2023-07-22 14:05:05 +0530

    added comments to server and mainjs

--- public/js/main.js
+++ public/js/main.js
@@ -1,32 +1,32 @@
-const deleteBtn = document.querySelectorAll('.fa-trash')
-const item = document.querySelectorAll('.item span')
-const itemCompleted = document.querySelectorAll('.item span.completed')
+const deleteBtn = document.querySelectorAll('.fa-trash') //variables for easy reading takes all items with class fa trash
+const item = document.querySelectorAll('.item span') // all spans inside elements with class item
+const itemCompleted = document.querySelectorAll('.item span.completed') // all spans with class completed in the elements with class item
 
-Array.from(deleteBtn).forEach((element)=>{
-    element.addEventListener('click', deleteItem)
+Array.from(deleteBtn).forEach((element)=>{ // all the elements with class fa trash are now in an array and looping
+    element.addEventListener('click', deleteItem) // and now have an event listener and calliing deleteitem
 })
 
-Array.from(item).forEach((element)=>{
-    element.addEventListener('click', markComplete)
+Array.from(item).forEach((element)=>{ // creating an array from item and looping 
+    element.addEventListener('click', markComplete) // event listener calling for markcomoplete
 })
 
-Array.from(itemCompleted).forEach((element)=>{
+Array.from(itemCompleted).forEach((element)=>{ // same as above
     element.addEventListener('click', markUnComplete)
 })
 
 async function deleteItem(){
-    const itemText = this.parentNode.childNodes[1].innerText
+    const itemText = this.parentNode.childNodes[1].innerText //assignin  a variable to the text from the choldnode that is span
     try{
-        const response = await fetch('deleteItem', {
-            method: 'delete',
-            headers: {'Content-Type': 'application/json'},
-            body: JSON.stringify({
-              'itemFromJS': itemText
+        const response = await fetch('deleteItem', { //awaiting response of feych of the deleteitem route
+            method: 'delete', // crud method 
+            headers: {'Content-Type': 'application/json'}, // speicifies the type of content as json which will be returned
+            body: JSON.stringify({ // convert converts a javascipt value to json string
+              'itemFromJS': itemText // text retruned is set as itemtext
             })
           })
-        const data = await response.json()
+        const data = await response.json() // waiting for json to be coverted
         console.log(data)
-        location.reload()
+        location.reload() // reload the page to show updated dom
 
     }catch(err){
         console.log(err)
@@ -34,18 +34,18 @@ async function deleteItem(){
 }
 
 async function markComplete(){
-    const itemText = this.parentNode.childNodes[1].innerText
+    const itemText = this.parentNode.childNodes[1].innerText// assign a variable to the text taken from the span of li
     try{
-        const response = await fetch('markComplete', {
-            method: 'put',
+        const response = await fetch('markComplete', { // awatign response of fetch fn of markcomplete
+            method: 'put', // crud method
             headers: {'Content-Type': 'application/json'},
-            body: JSON.stringify({
-                'itemFromJS': itemText
+            body: JSON.stringify({// converts json to string
+                'itemFromJS': itemText//text retruned is set as itemtext
             })
           })
-        const data = await response.json()
+        const data = await response.json()// waiting for json to convert
         console.log(data)
-        location.reload()
+        location.reload() //refresh
 
     }catch(err){
         console.log(err)
@@ -53,18 +53,18 @@ async function markComplete(){
 }
 
 async function markUnComplete(){
-    const itemText = this.parentNode.childNodes[1].innerText
+    const itemText = this.parentNode.childNodes[1].innerText // extract text from span of li
     try{
         const response = await fetch('markUnComplete', {
-            method: 'put',
-            headers: {'Content-Type': 'application/json'},
-            body: JSON.stringify({
-                'itemFromJS': itemText
+            method: 'put', // crud method
+            headers: {'Content-Type': 'application/json'}, // type of response expected
+            body: JSON.stringify({// converting js value to json
+                'itemFromJS': itemText // setting text receieved as itemfromjs
             })
           })
-        const data = await response.json()
+        const data = await response.json() // waiting for json to convert
         console.log(data)
-        location.reload()
+        location.reload()// refresh
 
     }catch(err){
         console.log(err)
