5200914df5f54ddc552eda655e65b3100291424c
gachanjaprince <gachanjaprince@gmail.com>
2023-07-28 00:48:11 -0600

    Added comments

--- public/js/main.js
+++ public/js/main.js
@@ -2,31 +2,31 @@ const deleteBtn = document.querySelectorAll('.fa-trash')
 const item = document.querySelectorAll('.item span')
 const itemCompleted = document.querySelectorAll('.item span.completed')
 
-Array.from(deleteBtn).forEach((element)=>{
+Array.from(deleteBtn).forEach((element)=>{ // Delete buttons call deleteItem
     element.addEventListener('click', deleteItem)
 })
 
-Array.from(item).forEach((element)=>{
+Array.from(item).forEach((element)=>{ // Incomplete list items call markComplete
     element.addEventListener('click', markComplete)
 })
 
-Array.from(itemCompleted).forEach((element)=>{
+Array.from(itemCompleted).forEach((element)=>{ // Complete list items call markUncomplete
     element.addEventListener('click', markUnComplete)
 })
 
 async function deleteItem(){
-    const itemText = this.parentNode.childNodes[1].innerText
+    const itemText = this.parentNode.childNodes[1].innerText  // Obtain task name typed in
     try{
-        const response = await fetch('deleteItem', {
+        const response = await fetch('deleteItem', { // Sends delete request to server
             method: 'delete',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
-              'itemFromJS': itemText
+              'itemFromJS': itemText // Text (task name) added to req body sent to the server - Identifies doc to be deleted
             })
           })
         const data = await response.json()
         console.log(data)
-        location.reload()
+        location.reload() // Page refresh after update
 
     }catch(err){
         console.log(err)
@@ -34,18 +34,18 @@ async function deleteItem(){
 }
 
 async function markComplete(){
-    const itemText = this.parentNode.childNodes[1].innerText
+    const itemText = this.parentNode.childNodes[1].innerText // Obtain task name typed in
     try{
-        const response = await fetch('markComplete', {
+        const response = await fetch('markComplete', { // sends /markComplete put request to server
             method: 'put',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
-                'itemFromJS': itemText
+                'itemFromJS': itemText  // Text (task name) added to req body sent to the server → Identifies doc to be updated
             })
           })
         const data = await response.json()
         console.log(data)
-        location.reload()
+        location.reload()  // Page refresh after update
 
     }catch(err){
         console.log(err)
@@ -53,18 +53,18 @@ async function markComplete(){
 }
 
 async function markUnComplete(){
-    const itemText = this.parentNode.childNodes[1].innerText
+    const itemText = this.parentNode.childNodes[1].innerText // Obtain task name typed in
     try{
-        const response = await fetch('markUnComplete', {
+        const response = await fetch('markUnComplete', { // sends /markUncomplete put request to server
             method: 'put',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
-                'itemFromJS': itemText
+                'itemFromJS': itemText // Text (task name) added to req body sent to the server → Identifies doc to be updated
             })
           })
         const data = await response.json()
         console.log(data)
-        location.reload()
+        location.reload() // Page refresh after update
 
     }catch(err){
         console.log(err)
--- server.js
+++ server.js
@@ -22,9 +22,9 @@ app.use(express.json())
 
 
 app.get('/',async (request, response)=>{
-    const todoItems = await db.collection('todos').find().toArray()
-    const itemsLeft = await db.collection('todos').countDocuments({completed: false})
-    response.render('index.ejs', { items: todoItems, left: itemsLeft })
+    const todoItems = await db.collection('todos').find().toArray() // Obtain to-do documents/objects (list items) from database
+    const itemsLeft = await db.collection('todos').countDocuments({completed: false}) // Obtains no. of incomplete tasks
+    response.render('index.ejs', { items: todoItems, left: itemsLeft }) // Data objects (list items) passed into ejs template
     // db.collection('todos').find().toArray()
     // .then(data => {
     //     db.collection('todos').countDocuments({completed: false})
@@ -35,22 +35,24 @@ app.get('/',async (request, response)=>{
     // .catch(error => console.error(error))
 })
 
+
 app.post('/addTodo', (request, response) => {
-    db.collection('todos').insertOne({thing: request.body.todoItem, completed: false})
+    db.collection('todos').insertOne({thing: request.body.todoItem, completed: false}) // todoItem from request is added to database
     .then(result => {
         console.log('Todo Added')
-        response.redirect('/')
+        response.redirect('/') // triggers refresh after document creation
     })
     .catch(error => console.error(error))
 })
+
 
 app.put('/markComplete', (request, response) => {
-    db.collection('todos').updateOne({thing: request.body.itemFromJS},{
+    db.collection('todos').updateOne({thing: request.body.itemFromJS},{ // Update first document with 'thing' property from inputted text
         $set: {
-            completed: true
+            completed: true // Change document's 'completed' property to true
           }
     },{
-        sort: {_id: -1},
+        sort: {_id: -1}, // Allows us to grab first item matching condition
         upsert: false
     })
     .then(result => {
@@ -60,26 +62,28 @@ app.put('/markComplete', (request, response) => {
     .catch(error => console.error(error))
 
 })
+
 
 app.put('/markUnComplete', (request, response) => {
-    db.collection('todos').updateOne({thing: request.body.itemFromJS},{
+    db.collection('todos').updateOne({thing: request.body.itemFromJS},{ // Update first document with 'thing' property from inputted text
         $set: {
-            completed: false
+            completed: false // Change document's 'completed' property to false
           }
     },{
-        sort: {_id: -1},
+        sort: {_id: -1}, // Allows us to grab first item matching condition
         upsert: false
     })
     .then(result => {
-        console.log('Marked Complete')
-        response.json('Marked Complete')
+        console.log('Marked Uncomplete')
+        response.json('Marked Uncomplete')
     })
     .catch(error => console.error(error))
 
 })
 
+
 app.delete('/deleteItem', (request, response) => {
-    db.collection('todos').deleteOne({thing: request.body.itemFromJS})
+    db.collection('todos').deleteOne({thing: request.body.itemFromJS}) // Delete document from database
     .then(result => {
         console.log('Todo Deleted')
         response.json('Todo Deleted')
@@ -87,6 +91,7 @@ app.delete('/deleteItem', (request, response) => {
     .catch(error => console.error(error))
 
 })
+
 
 app.listen(process.env.PORT || PORT, ()=>{
     console.log(`Server running on port ${PORT}`)
