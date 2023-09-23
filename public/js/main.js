c513775262762709419383642847b3cea7fcea52
Saba Azeem <sabaazeem025@gmail.com>
2023-09-23 17:35:02 +0500

    add comments

--- public/js/main.js
+++ public/js/main.js
@@ -1,22 +1,31 @@
+//Select all elements with class 'fa-trash' and store them in 'deleteBtn'
 const deleteBtn = document.querySelectorAll('.fa-trash')
+//Select all elements with class 'item span' and store them in 'item'
 const item = document.querySelectorAll('.item span')
+//Select all elements with class 'item span.completed' and store them in 'itemCompleted'
 const itemCompleted = document.querySelectorAll('.item span.completed')
 
+//Add a click event listener to each element with class 'fa-trash'
 Array.from(deleteBtn).forEach((element)=>{
     element.addEventListener('click', deleteItem)
 })
 
+//add a click event listener to each element with class 'item span'
 Array.from(item).forEach((element)=>{
     element.addEventListener('click', markComplete)
 })
 
+//Add a click event listener to each element with class 'item span.complted'
 Array.from(itemCompleted).forEach((element)=>{
     element.addEventListener('click', markUnComplete)
 })
 
+//Function to handle the deletion of an item
 async function deleteItem(){
+    //Get the text of the item to be deleted
     const itemText = this.parentNode.childNodes[1].innerText
     try{
+        //Send a DELETE request to 'deleteItem' route with the item text as JSON
         const response = await fetch('deleteItem', {
             method: 'delete',
             headers: {'Content-Type': 'application/json'},
@@ -24,7 +33,10 @@ async function deleteItem(){
               'itemFromJS': itemText
             })
           })
+        //Parse the response JSON
         const data = await response.json()
+
+        //log the response data and reload the page
         console.log(data)
         location.reload()
 
@@ -33,9 +45,12 @@ async function deleteItem(){
     }
 }
 
+//Function to handle marking an item as complete
 async function markComplete(){
+    //Get the text of the item to be marked as complete
     const itemText = this.parentNode.childNodes[1].innerText
     try{
+        //Send a PUT request to 'markComplete' route with the item text as JSON
         const response = await fetch('markComplete', {
             method: 'put',
             headers: {'Content-Type': 'application/json'},
@@ -43,7 +58,9 @@ async function markComplete(){
                 'itemFromJS': itemText
             })
           })
+        //Parse the response JSON
         const data = await response.json()
+        //Log the response data and reload the page
         console.log(data)
         location.reload()
 
@@ -52,9 +69,12 @@ async function markComplete(){
     }
 }
 
+//Function to handle marking an item as uncomplete
 async function markUnComplete(){
+    //Get the text of the item to be marked as uncomplete
     const itemText = this.parentNode.childNodes[1].innerText
     try{
+        //Send a PUT request to 'markUnComplete' route with the item text as JSON
         const response = await fetch('markUnComplete', {
             method: 'put',
             headers: {'Content-Type': 'application/json'},
@@ -62,7 +82,9 @@ async function markUnComplete(){
                 'itemFromJS': itemText
             })
           })
+        //Parse the response JSON
         const data = await response.json()
+        //Log the response data and reload the page
         console.log(data)
         location.reload()
 
