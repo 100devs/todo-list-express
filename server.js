4e68a14f7f02b838a436c49c06c8eabca4ead90d
vmalaviya <varunmalaviya7@gmail.com>
2023-10-09 17:34:33 -0400

    added comments to better explain code execution and flow

--- server.js
+++ server.js
@@ -1,29 +1,37 @@
+/* import all required dependencies */
 const express = require('express')
 const app = express()
 const MongoClient = require('mongodb').MongoClient
 const PORT = 2121
 require('dotenv').config()
 
-
+/* get connection string from .env file */
 let db,
     dbConnectionStr = process.env.DB_STRING,
     dbName = 'todo'
 
+    /* connect to database */
 MongoClient.connect(dbConnectionStr, { useUnifiedTopology: true })
     .then(client => {
         console.log(`Connected to ${dbName} Database`)
         db = client.db(dbName)
     })
-    
+
+/* set view engine */
 app.set('view engine', 'ejs')
+/* set static folder for static assets */
 app.use(express.static('public'))
+/* parse URL-encoded and JSON request bodies */
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 
-
+/* define route handler for '/' route  */
 app.get('/',async (request, response)=>{
+    /* retrieve todo items from collection and conver them to an array */
     const todoItems = await db.collection('todos').find().toArray()
+    /* count all incomplete items */
     const itemsLeft = await db.collection('todos').countDocuments({completed: false})
+    /* render ejs template for views */
     response.render('index.ejs', { items: todoItems, left: itemsLeft })
     // db.collection('todos').find().toArray()
     // .then(data => {
@@ -35,7 +43,9 @@ app.get('/',async (request, response)=>{
     // .catch(error => console.error(error))
 })
 
+/* define route handler for /addTodo route, handling POST requests */
 app.post('/addTodo', (request, response) => {
+    /* add new todo to collection */
     db.collection('todos').insertOne({thing: request.body.todoItem, completed: false})
     .then(result => {
         console.log('Todo Added')
@@ -44,7 +54,9 @@ app.post('/addTodo', (request, response) => {
     .catch(error => console.error(error))
 })
 
+/* define route handler for handling PUT requests */
 app.put('/markComplete', (request, response) => {
+    /* update completed status of selected todo */
     db.collection('todos').updateOne({thing: request.body.itemFromJS},{
         $set: {
             completed: true
@@ -61,7 +73,9 @@ app.put('/markComplete', (request, response) => {
 
 })
 
+/* define route handler for marking a todo as incomplete */
 app.put('/markUnComplete', (request, response) => {
+    /* mark selected item as incomplete */
     db.collection('todos').updateOne({thing: request.body.itemFromJS},{
         $set: {
             completed: false
@@ -78,7 +92,9 @@ app.put('/markUnComplete', (request, response) => {
 
 })
 
+/* define route handler for deleting todos */
 app.delete('/deleteItem', (request, response) => {
+    /* delete selected todo from  database */
     db.collection('todos').deleteOne({thing: request.body.itemFromJS})
     .then(result => {
         console.log('Todo Deleted')
@@ -88,6 +104,7 @@ app.delete('/deleteItem', (request, response) => {
 
 })
 
+/* start server on dynamic port */
 app.listen(process.env.PORT || PORT, ()=>{
     console.log(`Server running on port ${PORT}`)
 })
