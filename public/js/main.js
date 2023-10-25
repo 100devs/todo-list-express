a4191f440bbadd1af2565d88cc041ca341dd7607
1x55 <k4i@pm.me>
2023-10-25 13:48:19 +0900

    Completed adding comments

--- public/js/main.js
+++ public/js/main.js
@@ -1,48 +1,72 @@
+// selects all HTML elements with the class 'fa-trash' and stores them in the 'deleteBtn" variable
 const deleteBtn = document.querySelectorAll('.fa-trash')
+
+//all HTML elements with the class of item that contain '<span>' elements are selected and stored inthe 'item' variable.
 const item = document.querySelectorAll('.item span')
+
+//selects elemts that meet 2 criterias:
+//elements with class "item" and among these 'item' elements, specifically wants those contianing a '<span>' element with the class 'completed' Result is stored in varivble 'itemCompleted'
 const itemCompleted = document.querySelectorAll('.item span.completed')
 
+// Array.from() method converts the NodeList stored in the deleteBtn variable into a JavaScript array. After converting the NodeList into an array, the forEach method is called on the array. Attaching a click event listener to each element in the deleteBtn array. When any of these elements is clicked, it will call the deleteItem function
 Array.from(deleteBtn).forEach((element)=>{
     element.addEventListener('click', deleteItem)
 })
 
+//Similar to the previous line, this code attaches a click event listener to each element in the item array, calling the markComplete function when clicked.
 Array.from(item).forEach((element)=>{
     element.addEventListener('click', markComplete)
 })
 
+//attaches a click event listener to wach element in the 'itemCompleted' array calling the 'markCompleted' function when clicked
 Array.from(itemCompleted).forEach((element)=>{
     element.addEventListener('click', markUnComplete)
 })
 
+// 'deleteItem' function is executed when an element with the 'fa-trash' class is clicked. A async request is sent to a server to delete an item and upon success, reloads the pade
 async function deleteItem(){
+    //retrieves text content of the second child node of the parent node of the current element and stores it in the variable itemText
     const itemText = this.parentNode.childNodes[1].innerText
+    //try catch block, used for error handling. code inside 'try'block is executed, and if an error occurs,it can be caught and handled by the 'catch' block
     try{
+        //The await keyword is used because fetch returns a promise, and it makes the code wait until the promise is resolved. The response from the server will be stored in the response variable.
         const response = await fetch('deleteItem', {
+            // specifies that a DELETE request should be sent to the server.
             method: 'delete',
+            // sets the request headers to indicate that the request's body is in JSON format. This is important when sending data to the server because it tells the server how to interpret the data.
             headers: {'Content-Type': 'application/json'},
+            // This is the data being sent in the request body. It's converted to a JSON string using JSON.stringify. In this case, it's an object with a property 'itemFromJS' containing the value of itemText.
             body: JSON.stringify({
               'itemFromJS': itemText
             })
           })
+        //the response from the server is parsed as JSON, and the resulting data is stored in the data variable.
         const data = await response.json()
+        //The received data is logged to the console, allowing you to see the response from the server
         console.log(data)
+        //This line forces the browser to reload the current page. This might be used to reflect changes made on the server, such as deleting an item.
         location.reload()
-
+    //marks the beginning of the catch block. Any errors are logged to the console for debugging and error tracking. 
     }catch(err){
         console.log(err)
     }
 }
-
+//defines an asynchronous function called markComplete
 async function markComplete(){
     const itemText = this.parentNode.childNodes[1].innerText
     try{
+        //a network request is initiated using the fetch function. It sends a PUT request to the 'markComplete' URL. The await keyword is used because fetch returns a promise, and it makes the code wait until the promise is resolved. The response from the server will be stored in the response variable.
         const response = await fetch('markComplete', {
+            //This option specifies that a PUT request should be sent to the server. PUT requests are often used to update or modify existing resources on the server.
             method: 'put',
+            //Headers provide additional information about the request, such as the content type. In this case, it specifies that the request body will be in JSON format, and the content type is set to 'application/json.' This is important because it tells the server that it should expect JSON data in the request body.
             headers: {'Content-Type': 'application/json'},
+            //sending a JSON object with a single property 'itemFromJS' containing the itemText variable. This JSON data will be sent to the server as part of the 'PUT' request. The server will then use this data to identify and mark the corresponding item as complete.
             body: JSON.stringify({
                 'itemFromJS': itemText
             })
           })
+        //waits for the response from the server and then parses it as JSON. The response data is stored in the data variable.
         const data = await response.json()
         console.log(data)
         location.reload()
@@ -51,10 +75,11 @@ async function markComplete(){
         console.log(err)
     }
 }
-
+//handles marking an item as uncompleted
 async function markUnComplete(){
+    // extract the text content of the second child node of the current element.
     const itemText = this.parentNode.childNodes[1].innerText
-    try{
+    try{ //sends a 'PUT' request to the 'markUnComplete' endpoint using the fetch API
         const response = await fetch('markUnComplete', {
             method: 'put',
             headers: {'Content-Type': 'application/json'},
--- server.js
+++ server.js
@@ -16,47 +16,58 @@ let db,
     // initialise dbName with a string of todo
     dbName = 'todo'
 
+// an object provided by the MongoDB Node.js driver that allows you to connect to and interact with MongoDB databases. 
 MongoClient.connect(dbConnectionStr, { useUnifiedTopology: true })
+    
+    // .then() is a method commonly used with Promises in JavaScript. It is used to specify what should happen once a Promise is resolved or fulfilled. When a Promise is resolved successfully, the code within the .then() block is executed. 'client' is a variable that represents the result or value that is passed when the Promise is fulfilled. Here, client represents the MongoDB client that is successfully connected to the database.
     .then(client => {
         console.log(`Connected to ${dbName} Database`)
+        // establish a connection to the MongoDB database and make it accessible for your Express application. This allows you to perform operations on the database using the db variable, such as inserting, updating, and retrieving data from the 'todo' database within your Express routes and handlers.
         db = client.db(dbName)
     })
-    
+// configure Express app to use EJS template engine for rendering views   
 app.set('view engine', 'ejs')``
+// serves static files form the 'public' folder
 app.use(express.static('public'))
+//configures middleware to handle incoming data from HTML forms
 app.use(express.urlencoded({ extended: true }))
+//configures middleware to handle incoming JSON data
 app.use(express.json())
 
-
+//set up route for handling HTTP GET requests to the root path ('/')
 app.get('/',async (request, response)=>{
+    //inside root handler, it retrieves data from a MondoDB collection called 'todos'
+    //'.find()' with no arguments provided, this searches for all documents in the 'todos' collection. 'toArray' converts the result of the 'find' operation into an array of JS objects. Each object represents a document from the collection
     const todoItems = await db.collection('todos').find().toArray()
+    //queries the 'todos' collection to count the number of documents that have the 'completed' field set to 'false' 
     const itemsLeft = await db.collection('todos').countDocuments({completed: false})
+    //generate an HTML response using an EJS template (index.ejs). Sends the retrieved data 'todoItems' and 'itemsLeft' to the template to be rendered dynamically.
+    //Note: items and left are keys in an object that is passed as the second argument to the response.render function. So, when your EJS template is rendered, you can access these variables as 'items' and 'left' within the template to dynamically populate content. For example, you might use <%= items %> and <%= left %> in your EJS template to display the to-do items and the number of items left to complete.
     response.render('index.ejs', { items: todoItems, left: itemsLeft })
-    // db.collection('todos').find().toArray()
-    // .then(data => {
-    //     db.collection('todos').countDocuments({completed: false})
-    //     .then(itemsLeft => {
-    //         response.render('index.ejs', { items: data, left: itemsLeft })
-    //     })
-    // })
-    // .catch(error => console.error(error))
+  
 })
 
+//sets up a route for handling POST requests to the '/addTodo' URL path
 app.post('/addTodo', (request, response) => {
+    //interacting with a MongoDB database specifically with the 'todos' collection. It inserts a new document into the collection. 'thing': The value of this field is taken from the request.body.todoItem
     db.collection('todos').insertOne({thing: request.body.todoItem, completed: false})
     .then(result => {
         console.log('Todo Added')
+        //redirects the user to the root URL ('/')
         response.redirect('/')
     })
     .catch(error => console.error(error))
 })
 
+//set up route to handle HTTP PUT requests to the 'markComplete' URL path
 app.put('/markComplete', (request, response) => {
+    //This line interacts with a MongoDB database collection named 'todos.' It performs an update operation using the updateOne method. 'thing...' this query identifies the document to be updated: in this example it looks for the document where the 'thing' field matches the value received in the HTTP request body as 'itemFormJS' 
     db.collection('todos').updateOne({thing: request.body.itemFromJS},{
+        //sets the 'completed' field of the matched document to true, indicating that the to-do item is marked as complete.
         $set: {
             completed: true
           }
-    },{
+    },{ //specify that the documents should be sorted by the '_id' field in descending order (-1), and upsert is set to 'false,' which means a new document won't be created if no match is found.
         sort: {_id: -1},
         upsert: false
     })
@@ -69,14 +80,16 @@ app.put('/markComplete', (request, response) => {
 })
 
 app.put('/markUnComplete', (request, response) => {
+    //accessing the 'todos' collection within the MongoDB database. Then, it calls updateOne() on that collection.Inside the updateOne() method, there's a filter object {thing: request.body.itemFromJS} that specifies which document(s) to update. It looks for a document where the 'thing' field matches the value received in the HTTP request body as 'itemFromJS'. 
     db.collection('todos').updateOne({thing: request.body.itemFromJS},{
+        //$set is used in MongoDB to update or add fields within a document. Here it updates the 'completed' field of the matched document to 'false'
         $set: {
             completed: false
           }
-    },{
+    },{//sort docs in descending order
         sort: {_id: -1},
         upsert: false
-    })
+    }) // after update operation is completed, '.then()' block to handle result
     .then(result => {
         console.log('Marked Complete')
         response.json('Marked Complete')
@@ -84,7 +97,7 @@ app.put('/markUnComplete', (request, response) => {
     .catch(error => console.error(error))
 
 })
-
+//code sets up a route for handling HTTP DELETE requests to the path '/deleteItem'.
 app.delete('/deleteItem', (request, response) => {
     db.collection('todos').deleteOne({thing: request.body.itemFromJS})
     .then(result => {
@@ -94,7 +107,7 @@ app.delete('/deleteItem', (request, response) => {
     .catch(error => console.error(error))
 
 })
-
+// responsible for starting your Express server and listening on a specific port, which can be dynamically determined from the process.env.PORT environment variable or fallback to the default port (2121 in this case). 
 app.listen(process.env.PORT || PORT, ()=>{
     console.log(`Server running on port ${PORT}`)
 })
