919798b86d2712f329c6d5852fcb0ec7bfcf3512
Claudio Lespier <clespier@gmail.com>
2024-02-01 13:03:40 -0500

    Comments were added.

--- public/js/main.js
+++ public/js/main.js
@@ -1,31 +1,43 @@
+//Selecting the spans needed for search on mongodb
 const deleteBtn = document.querySelectorAll('.fa-trash')
 const item = document.querySelectorAll('.item span')
 const itemCompleted = document.querySelectorAll('.item span.completed')
 
+//Looping through each span and adding the event listener on click then calling function
 Array.from(deleteBtn).forEach((element)=>{
     element.addEventListener('click', deleteItem)
 })
 
+//Looping through each span and adding the event listener on click then calling function
 Array.from(item).forEach((element)=>{
     element.addEventListener('click', markComplete)
 })
 
+//Looping through each span and adding the event listener on click then calling function
 Array.from(itemCompleted).forEach((element)=>{
     element.addEventListener('click', markUnComplete)
 })
 
+
+//function to fetch data 
 async function deleteItem(){
+    //goes into the span, searches for the parent and grabs the first span's text
     const itemText = this.parentNode.childNodes[1].innerText
     try{
+        //fetch server side data, calling the delete item path
         const response = await fetch('deleteItem', {
+            //Fetch method
             method: 'delete',
             headers: {'Content-Type': 'application/json'},
+            //Turning itemText into Json format for the request
             body: JSON.stringify({
               'itemFromJS': itemText
             })
           })
+        //Saving response in the data variable
         const data = await response.json()
         console.log(data)
+        //Refresh page
         location.reload()
 
     }catch(err){
@@ -34,15 +46,20 @@ async function deleteItem(){
 }
 
 async function markComplete(){
+    //goes into the span, searches for the parent and grabs the first span's text
     const itemText = this.parentNode.childNodes[1].innerText
     try{
+        //fetch server side data, calling the markcomplete path
         const response = await fetch('markComplete', {
+            //Fetch method
             method: 'put',
             headers: {'Content-Type': 'application/json'},
+            //Turning itemText into Json format for the request
             body: JSON.stringify({
                 'itemFromJS': itemText
             })
           })
+          //Saving response in the data variable
         const data = await response.json()
         console.log(data)
         location.reload()
@@ -52,6 +69,7 @@ async function markComplete(){
     }
 }
 
+//See other methods for comments.
 async function markUnComplete(){
     const itemText = this.parentNode.childNodes[1].innerText
     try{
--- server.js
+++ server.js
@@ -1,29 +1,38 @@
+//Variable required for express
 const express = require('express')
 const app = express()
-const MongoClient = require('mongodb').MongoClient
+//Used to access mongodb
+const MongoClient = require('mongodb').MongoClient\
+
+//Local server port
 const PORT = 2121
 require('dotenv').config()
 
-
+//Used to access mongodb
 let db,
     dbConnectionStr = process.env.DB_STRING,
     dbName = 'todo'
-
+//Used to access mongodb
 MongoClient.connect(dbConnectionStr, { useUnifiedTopology: true })
     .then(client => {
         console.log(`Connected to ${dbName} Database`)
         db = client.db(dbName)
     })
-    
+   
+//Variable required for express
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 
-
+//path that handles get requests sserver side
 app.get('/',async (request, response)=>{
+    //Find the document we are looking for and place them in an array and then in a variable
     const todoItems = await db.collection('todos').find().toArray()
+    //Counts documents and stores in a variable
     const itemsLeft = await db.collection('todos').countDocuments({completed: false})
+
+    //Responds with index.ejs, document and how many are completed false.
     response.render('index.ejs', { items: todoItems, left: itemsLeft })
     // db.collection('todos').find().toArray()
     // .then(data => {
@@ -35,25 +44,35 @@ app.get('/',async (request, response)=>{
     // .catch(error => console.error(error))
 })
 
+//path that handles post requests server side
 app.post('/addTodo', (request, response) => {
+    //Inserts document into database
     db.collection('todos').insertOne({thing: request.body.todoItem, completed: false})
     .then(result => {
+        //Console logs confirmation
         console.log('Todo Added')
+        //Refreash the page making a get request
         response.redirect('/')
     })
     .catch(error => console.error(error))
 })
 
+//path that handles post requests server side
 app.put('/markComplete', (request, response) => {
+    //Update document that matches the request text
     db.collection('todos').updateOne({thing: request.body.itemFromJS},{
         $set: {
+            //Once document is found change completed to true
             completed: true
           }
     },{
+        //Sorty documents that match text
         sort: {_id: -1},
+        //If document does not exist do not create a new one with the thing text we are searching
         upsert: false
     })
     .then(result => {
+        //Console log complete
         console.log('Marked Complete')
         response.json('Marked Complete')
     })
@@ -61,26 +80,33 @@ app.put('/markComplete', (request, response) => {
 
 })
 
+//path that handles post requests server side
 app.put('/markUnComplete', (request, response) => {
+    //Update document that matches the request text
     db.collection('todos').updateOne({thing: request.body.itemFromJS},{
         $set: {
+            //Once document is found change completed to false
             completed: false
           }
-    },{
+    },{ //Sorty documents that match text 
         sort: {_id: -1},
+        //If document does not exist do not create a new one with the thing text we are searching
         upsert: false
     })
     .then(result => {
+        //Console log complete
         console.log('Marked Complete')
         response.json('Marked Complete')
     })
     .catch(error => console.error(error))
 
 })
-
+//path that handles delete on mmongodb
 app.delete('/deleteItem', (request, response) => {
+    //finds text in the todo database and deletes it
     db.collection('todos').deleteOne({thing: request.body.itemFromJS})
     .then(result => {
+        //Console log confirmation
         console.log('Todo Deleted')
         response.json('Todo Deleted')
     })
@@ -88,6 +114,7 @@ app.delete('/deleteItem', (request, response) => {
 
 })
 
+//Listening on port for requests
 app.listen(process.env.PORT || PORT, ()=>{
     console.log(`Server running on port ${PORT}`)
 })
