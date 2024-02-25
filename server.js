3372158ace4de31b9bb084e45f4bc044a269ce5d
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2024-02-25 15:37:52 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,93 +1,134 @@
-const express = require('express')
-const app = express()
-const MongoClient = require('mongodb').MongoClient
-const PORT = 2121
-require('dotenv').config()
-
+// import express module into the project.
+const express = require("express");
+// initialize express.
+const app = express();
+// import mongoclient from mongodb.
+const MongoClient = require("mongodb").MongoClient;
+// specifies port to run server on.
+const PORT = 2121;
+// import variables storing key information in the .env file.
+require("dotenv").config();
 
 let db,
-    dbConnectionStr = process.env.DB_STRING,
-    dbName = 'todo'
+  dbConnectionStr = process.env.DB_STRING,
+  dbName = "todo";
 
-MongoClient.connect(dbConnectionStr, { useUnifiedTopology: true })
-    .then(client => {
-        console.log(`Connected to ${dbName} Database`)
-        db = client.db(dbName)
+// connects to database using the connection string from the .env file and logs the a mesage if connections is successful. it also creates a db collection.
+MongoClient.connect(dbConnectionStr, { useUnifiedTopology: true }).then(
+  (client) => {
+    console.log(`Connected to ${dbName} Database`);
+    db = client.db(dbName);
+  }
+);
+
+// sets the view engine to ejs
+app.set("view engine", "ejs");
+// handle the static files for us.
+app.use(express.static("public"));
+// body parser.
+app.use(express.urlencoded({ extended: true }));
+// enables json parsing.
+app.use(express.json());
+
+// creates a request handler for the home page and runs an anonymous async function;
+app.get("/", async (request, response) => {
+  const todoItems = await db.collection("todos").find().toArray();
+  const itemsLeft = await db
+    .collection("todos")
+    .countDocuments({ completed: false });
+  response.render("index.ejs", { items: todoItems, left: itemsLeft });
+  // db.collection('todos').find().toArray()
+  // .then(data => {
+  //     db.collection('todos').countDocuments({completed: false})
+  //     .then(itemsLeft => {
+  //         response.render('index.ejs', { items: data, left: itemsLeft })
+  //     })
+  // })
+  // .catch(error => console.error(error))
+});
+
+// creates a request handler to handle form submission and runs an anonymous async function;
+app.post("/addTodo", (request, response) => {
+  db.collection("todos")
+    //   insert a new document into the collection
+    .insertOne({ thing: request.body.todoItem, completed: false })
+    .then((result) => {
+      console.log("Todo Added");
+      // redirects back to the home page. a new get request
+      response.redirect("/");
     })
-    
-app.set('view engine', 'ejs')
-app.use(express.static('public'))
-app.use(express.urlencoded({ extended: true }))
-app.use(express.json())
+    // error handler
+    .catch((error) => console.error(error));
+});
 
-
-app.get('/',async (request, response)=>{
-    const todoItems = await db.collection('todos').find().toArray()
-    const itemsLeft = await db.collection('todos').countDocuments({completed: false})
-    response.render('index.ejs', { items: todoItems, left: itemsLeft })
-    // db.collection('todos').find().toArray()
-    // .then(data => {
-    //     db.collection('todos').countDocuments({completed: false})
-    //     .then(itemsLeft => {
-    //         response.render('index.ejs', { items: data, left: itemsLeft })
-    //     })
-    // })
-    // .catch(error => console.error(error))
-})
-
-app.post('/addTodo', (request, response) => {
-    db.collection('todos').insertOne({thing: request.body.todoItem, completed: false})
-    .then(result => {
-        console.log('Todo Added')
-        response.redirect('/')
-    })
-    .catch(error => console.error(error))
-})
-
-app.put('/markComplete', (request, response) => {
-    db.collection('todos').updateOne({thing: request.body.itemFromJS},{
+// creates a request handler to handle completed tasks and runs an anonymous async function;
+app.put("/markComplete", (request, response) => {
+  db.collection("todos")
+    .updateOne(
+      // this is the document to be updated. it is the item on which the event listener was triggered on.
+      { thing: request.body.itemFromJS },
+      {
+        // sets the value of the completed key to true.
         $set: {
-            completed: true
-          }
-    },{
-        sort: {_id: -1},
-        upsert: false
+          completed: true,
+        },
+      },
+      {
+        sort: { _id: -1 },
+        // if set to true, it would create a new document in the absense of a document to be updated.
+        upsert: false,
+      }
+    )
+    .then((result) => {
+      console.log("Marked Complete");
+      response.json("Marked Complete");
     })
-    .then(result => {
-        console.log('Marked Complete')
-        response.json('Marked Complete')
-    })
-    .catch(error => console.error(error))
+    // error handler
+    .catch((error) => console.error(error));
+});
 
-})
+// creates a request handler t handle the user request to remove the completed class from a task and runs an anonymous async function;
+app.put("/markUnComplete", (request, response) => {
+  db.collection("todos")
+    //   updates the document
+    .updateOne(
+      // this is the document to be updated. it is the item on which the event listener was triggered on
+      { thing: request.body.itemFromJS },
 
-app.put('/markUnComplete', (request, response) => {
-    db.collection('todos').updateOne({thing: request.body.itemFromJS},{
+      {
+        // sets the value of the completed key to true.
         $set: {
-            completed: false
-          }
-    },{
-        sort: {_id: -1},
-        upsert: false
+          completed: false,
+        },
+      },
+      {
+        sort: { _id: -1 },
+        // if set to true, it would create a new document in the absense of a document to be updated.
+        upsert: false,
+      }
+    )
+    .then((result) => {
+      console.log("Marked Complete");
+      response.json("Marked Complete");
     })
-    .then(result => {
-        console.log('Marked Complete')
-        response.json('Marked Complete')
+    // error handler
+    .catch((error) => console.error(error));
+});
+
+// creates a request handler to delete documents in the collection and runs an anonymous async function;
+app.delete("/deleteItem", (request, response) => {
+  db.collection("todos")
+    //   deleteOne method deletes an item from the collection.  // thing is the document to be deleted. it is the item on which the event listener was triggered on
+    .deleteOne({ thing: request.body.itemFromJS })
+    .then((result) => {
+      console.log("Todo Deleted");
+      response.json("Todo Deleted");
     })
-    .catch(error => console.error(error))
+    // error handler
+    .catch((error) => console.error(error));
+});
 
-})
-
-app.delete('/deleteItem', (request, response) => {
-    db.collection('todos').deleteOne({thing: request.body.itemFromJS})
-    .then(result => {
-        console.log('Todo Deleted')
-        response.json('Todo Deleted')
-    })
-    .catch(error => console.error(error))
-
-})
-
-app.listen(process.env.PORT || PORT, ()=>{
-    console.log(`Server running on port ${PORT}`)
-})
+// runs the server on a port
+app.listen(process.env.PORT || PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
